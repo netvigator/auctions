c3185b1c4d7701e4e0643552d5d28ee2d1ab23ac
Rick Graves <gravesricharde@yahoo.com>
2017-12-17 12:11:36 +0700

    add bhascategories, clarify long names

--- markets/models.py
+++ markets/models.py
@@ -1,5 +1,5 @@
 from django.db.models           import PositiveSmallIntegerField as SmallInt
-from django.db.models           import Model, CharField
+from django.db.models           import Model, CharField, NullBooleanField
 from django_countries.fields    import CountryField
 
 # Create your models here.
@@ -14,14 +14,29 @@ from django_countries.fields    import CountryField
 # http://developer.ebay.com/devzone/finding/Concepts/SiteIDToGlobalID.html
 # http://developer.ebay.com/DevZone/half-finding/CallRef/Enums/GlobalIdList.html
 
+# not all markets have their own categories
+# https://developer.ebay.com/api-docs/commerce/taxonomy/static/supportedmarketplaces.html
+# also A given eBay marketplace might use multiple category trees
+# https://developer.ebay.com/api-docs/commerce/taxonomy/resources/category_tree/methods/getDefaultCategoryTreeId
+
+#### NOTE Russia is on the list of markets with own category list but d/n have id for the market!
+# https://developer.ebay.com/api-docs/commerce/taxonomy/static/supportedmarketplaces.html
+# ebay.com/sch/Russia is website, but that is a category on USA site, forget Russia
+
+# currencies:
+# https://developer.ebay.com/devzone/finding/callref/Enums/currencyIdList.html
+
 class Market(Model):
-    cmarket     = CharField(    'market',    max_length = 14, unique=True )
-    ccountry    = CountryField( 'country' )
-    clanguage   = CharField(    'language',  max_length = 8 )
-    iebaysiteid = SmallInt(     'global ID', unique=True )
-    icategoryver= SmallInt(     'most recent category version', null = True )
-    ccurrencydef= CharField(    'currency default',
-                                max_length = 3, null = True )
+    cmarket         = CharField(    'ebay Global ID',    max_length = 14,
+                                unique = True )
+    ccountry        = CountryField( 'country' )
+    clanguage       = CharField(    'language',  max_length = 8 )
+    iebaysiteid     = SmallInt(     'site ID', unique=True )
+    bhascategories  = NullBooleanField( 'has own categories?', null = True )
+    icategoryver    = SmallInt(     'most recent category version',
+                               null = True )
+    ccurrencydef    = CharField(    'currency default',
+                               max_length = 3, null = True )
     
     def __str__(self):
         return self.cmarket
