18bd7f36d3fda52ee92ae4110acc824198863006
Rick Graves <gravesricharde@yahoo.com>
2017-12-18 15:38:52 +0700

    correct userOne name, should be aardvigator not netvigator

--- core/tests.py
+++ core/tests.py
@@ -11,7 +11,7 @@ class CoreUserTests(TestCase):
 
     def test_get_user(self):
         
-        self.assertEquals( oUserOne.username, 'netvigator')
+        self.assertEquals( oUserOne.username, 'aardvigator')
 
 
 class EbayWrapperTests(TestCase):
