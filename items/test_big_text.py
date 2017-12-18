809eaf40aaec803d5f39ff8327bca8d3b44c9de1
Rick Graves <gravesricharde@yahoo.com>
2017-12-18 15:43:30 +0700

    store big content of example search result json file

---
+++ items/test_big_text.py
@@ -0,0 +1,174 @@
+
+sExampleResponse = \
+'''{
+  "findItemsAdvancedResponse": [
+    {
+      "searchResult": [
+        {
+          "@count": "1",
+          "item": [
+            {
+              "itemId": [
+                "253295991282"
+              ],
+              "title": [
+                "Simpson 360 Multimeter"
+              ],
+              "listingInfo": [
+                {
+                  "bestOfferEnabled": [
+                    "false"
+                  ],
+                  "endTime": [
+                    "2018-01-04T14:01:02.000Z"
+                  ],
+                  "gift": [
+                    "false"
+                  ],
+                  "listingType": [
+                    "StoreInventory"
+                  ],
+                  "buyItNowAvailable": [
+                    "false"
+                  ],
+                  "startTime": [
+                    "2017-12-05T14:01:02.000Z"
+                  ]
+                }
+              ],
+              "primaryCategory": [
+                {
+                  "categoryId": [
+                    "58277"
+                  ],
+                  "categoryName": [
+                    "Multimeters"
+                  ]
+                }
+              ],
+              "globalId": [
+                "EBAY-US"
+              ],
+              "isMultiVariationListing": [
+                "false"
+              ],
+              "location": [
+                "Piedmont,SC,USA"
+              ],
+              "topRatedListing": [
+                "false"
+              ],
+              "shippingInfo": [
+                {
+                  "shipToLocations": [
+                    "Worldwide"
+                  ],
+                  "oneDayShippingAvailable": [
+                    "false"
+                  ],
+                  "shippingServiceCost": [
+                    {
+                      "@currencyId": "USD",
+                      "__value__": "10.0"
+                    }
+                  ],
+                  "expeditedShipping": [
+                    "false"
+                  ],
+                  "handlingTime": [
+                    "2"
+                  ],
+                  "shippingType": [
+                    "Flat"
+                  ]
+                }
+              ],
+              "autoPay": [
+                "false"
+              ],
+              "viewItemURL": [
+                "http://www.ebay.com/itm/Simpson-360-Multimeter-/253295991282"
+              ],
+              "postalCode": [
+                "29673"
+              ],
+              "condition": [
+                {
+                  "conditionDisplayName": [
+                    "For parts or not working"
+                  ],
+                  "conditionId": [
+                    "7000"
+                  ]
+                }
+              ],
+              "country": [
+                "US"
+              ],
+              "sellingStatus": [
+                {
+                  "sellingState": [
+                    "Active"
+                  ],
+                  "currentPrice": [
+                    {
+                      "@currencyId": "USD",
+                      "__value__": "39.0"
+                    }
+                  ],
+                  "convertedCurrentPrice": [
+                    {
+                      "@currencyId": "USD",
+                      "__value__": "39.0"
+                    }
+                  ],
+                  "timeLeft": [
+                    "P19DT23H42M8S"
+                  ]
+                }
+              ],
+              "galleryURL": [
+                "http://thumbs3.ebaystatic.com/m/mDzuc_hBdbr66ce2oqz2yxA/140.jpg"
+              ],
+              "paymentMethod": [
+                "PayPal"
+              ],
+              "returnsAccepted": [
+                "false"
+              ]
+            }
+          ]
+        }
+      ],
+      "itemSearchURL": [
+        "http://www.ebay.com/sch/58277/i.html?_nkw=Simpson+360&_ddo=1&_ipg=100&_pgn=1"
+      ],
+      "timestamp": [
+        "2017-12-15T14:18:54.955Z"
+      ],
+      "ack": [
+        "Success"
+      ],
+      "version": [
+        "1.13.0"
+      ],
+      "paginationOutput": [
+        {
+          "pageNumber": [
+            "1"
+          ],
+          "totalEntries": [
+            "1"
+          ],
+          "entriesPerPage": [
+            "100"
+          ],
+          "totalPages": [
+            "1"
+          ]
+        }
+      ]
+    }
+  ]
+}
+'''
