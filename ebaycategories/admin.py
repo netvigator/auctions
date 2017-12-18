48fc07a5dd1491c243c0d7b68b45be07d1ebb872
Rick Graves <gravesricharde@yahoo.com>
2017-12-18 18:03:41 +0700

    add EbayCategory & Admin, needs troubleshooting still

--- ebaycategories/admin.py
+++ ebaycategories/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import EbayCategory
+from .models import EbayCategory, Condition
 
 
 class EbayCategoryAdmin(admin.ModelAdmin):
-    list_display = ( "ebay_id", "ctitle", "ilevel", "iparent_id", "bleaf_category", "iTreeVersion" ) 
+    list_display = ( "ebay_id", "ctitle", "ilevel", "iparent_id",
+                     "bleaf_category", "iTreeVersion" )
 
+
+class ConditionAdmin(admin.ModelAdmin):
+    list_display = ( "ebay_id", "ctitle" )
 
 admin.site.register(EbayCategory, EbayCategoryAdmin)
+
