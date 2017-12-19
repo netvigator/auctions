a62ba4ee42125a6269ba7a2894a4266f618bc9b1
Rick Graves <gravesricharde@yahoo.com>
2017-12-19 17:14:40 +0700

    update itemHits, conform to what ebay gives via the finding API

--- itemhits/models.py
+++ itemhits/models.py
@@ -1,5 +1,7 @@
 from django.db              import models
 from djmoney.models.fields  import MoneyField
+from django_countries.fields \
+                            import CountryField
 
 # Create your models here.
 
@@ -15,27 +17,47 @@ class ItemHit(models.Model):
                         'ebay item number', primary_key = True )
     cTitle          = models.CharField(
                         'auction headline', max_length = 48, db_index = True )
-    mLastBid        = MoneyField( 'most recent bid',
+    cLocation       = models.CharField( 'location',
+                        max_length = 48, null = True )
+    cCountry        = CountryField( "country", null = True )
+    cMarket         = models.CharField( 'market Global ID',
+                        max_length = 14, null = True )
+    cGalleryURL     = models.CharField( 'gallery pic URL',
+                        max_length = 88, null = True )
+    cEbayItemURL    = models.CharField( 'ebay item URL',
+                        max_length =188, null = True )
+    tTimeBeg        = models.DateTimeField( 'beginning date/time',null = True )
+    tTimeEnd        = models.DateTimeField( 'ending date/time',   null = True )
+    bBestOfferable  = models.BooleanField( 'best offer enabled?', default = False )
+    bBuyItNowable   = models.BooleanField( 'buy it now enabled?', default = False )
+    mCurrentPrice   = MoneyField( 'current price (local currency)',
                         max_digits = 10, decimal_places = 2,
                         default_currency='USD', null = True )
-    clastbid        = models.CharField(
-                        'most recent bid (text)', max_length = 18,
+    nCurrentPrice   = models.DecimalField(
+                        'current price (converted to USD)',
+                        max_digits=10, decimal_places=2,
                         db_index = False, null = True )
-    mBuyItNow       = MoneyField( 'buy it now price',
-                        max_digits = 10, decimal_places = 2,
-                        default_currency='USD', null = True )
-    cbuyitnow       = models.CharField(
-                        'buy it now price (text)', max_length = 18,
-                        db_index = False, null = True )
-    iBidCount       = models.PositiveSmallIntegerField( 'number of bids', default = 0 )
-    tAuctionEnd     = models.DateTimeField( 'auction ending date/time' )
-    tAuctionBeg     = models.DateTimeField( 'auction beginning date/time' )
+    iCategoryID     = models.IntegerField( 'primary category ID', null = True )
+    cCategory       = models.CharField( 'primary category',
+                        max_length = 48, null = True )
+    
+    iConditionID    = models.IntegerField( 'condition ID', null = True )
+    cCondition      = models.CharField( 'condition display name',
+                        max_length = 28, null = True )
+    
+    cSellingState   = models.CharField( 'selling state',
+                        max_length = 18, null = True )
+    
+    iBidCount       = models.PositiveSmallIntegerField( 'number of bids',
+                        default = 0 )
+    
     dhitstars       = models.DecimalField(
                         'hit stars', max_digits = 3, decimal_places = 2 )
     bitemhit        = models.BooleanField( 'item of interest?', default = False )
+    
     tlook4hits      = models.DateTimeField(
                         'assessed interest date/time', null = True )
-    imodel          = models.ForeignKey( Model,     null = True )
+    iModel          = models.ForeignKey( Model,     null = True )
     iBrand          = models.ForeignKey( Brand,     null = True )
     iCategory       = models.ForeignKey( Category,  null = True )
     iUser           = models.ForeignKey( User, verbose_name = 'Owner' )
