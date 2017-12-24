bad37043f441002d3e5273afc567bf449e8de5b4
Rick Graves <gravesricharde@yahoo.com>
2017-12-24 19:35:24 +0800

    experimenting with class based views

--- brands/views.py
+++ brands/views.py
@@ -1,23 +1,29 @@
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
-from django.views.generic import CreateView
+from django.views.generic.edit  import CreateView, UpdateView, DeleteView
+from django.views.generic       import DetailView, ListView
+from django.urls                import reverse_lazy
+from django.views               import generic
+
+from .models                    import Brand
+from categories.models          import Category
+from models.models              import Model
+
 
 # Create your views here but keep them thin.
 
-from .models import Brand
 
-class IndexView(generic.ListView):  
-    template_name = 'brands/index.html'
-    context_object_name = 'index'
-    model = Brand
-    
 '''
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views.generic import CreateView
+
+
+
+
     def get_queryset(self):
         """Return the last five published questions."""
         return Brand.objects.order_by('cTitle')
-'''
+
 
 class DetailView(generic.DetailView):
     model = Brand
@@ -33,3 +39,62 @@ class CreateBrandView(CreateView):
 
     def get_success_url(self):
         return reverse('brands-list')
+'''
+tModelFields = (
+    'cTitle',
+    'bWanted',
+    'bAllOfInterest',
+    'cLookFor',
+    'iStars',
+    'cComment',
+    'cNationality',
+    'cExcludeIf' )
+'''
+    'iUser',
+    'tCreate',
+    'tModify'
+'''
+
+lMoreModelFields = list( tModelFields )
+lMoreModelFields.extend( [ 'iUser_id', 'tCreate', 'tModify' ] )
+tMoreModelFields = tuple( lMoreModelFields )
+
+class BrandCreate(CreateView):
+    model   = Brand
+    fields  = tModelFields
+    template_name = 'brands/brand_add_form.html'
+
+class BrandDelete(DeleteView):
+    model   = Brand
+    template_name = 'brands/brand_confirm_delete.html'
+    success_url = reverse_lazy('brand-list')
+
+class BrandDetail(DetailView):
+    model   = Brand
+    fields  = tModelFields
+    template_name = 'brands/brand_detail_form.html'
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(BrandDetail, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the categories
+        context['fields_list'] = Brand.getFieldsForView(
+                                        Brand, tMoreModelFields )
+        #
+        context['categories_list'] = Category.objects.all()
+        # Add in a QuerySet of all the models
+        context['models_list'    ] = Model.objects.all()
+        return context
+
+class BrandUpdate(UpdateView):
+    model   = Brand
+    fields  = tModelFields
+    template_name = 'brands/brand_edit_form.html'
+
+class IndexView(ListView):  
+    template_name = 'brands/brand_index.html'
+    # context_object_name = 'brand_list' # default
+    model = Brand
+    
+    def get_queryset(self):
+        return Brand.objects.filter(iUser=self.request.user)
