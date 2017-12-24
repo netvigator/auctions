677428d6c8b6f088bbda82269acb9e8151ef3685
Rick Graves <gravesricharde@yahoo.com>
2017-12-24 19:34:45 +0800

    change to using app prefix for template names

--- brands/urls.py
+++ brands/urls.py
@@ -2,12 +2,24 @@ from django.conf.urls import url
 
 from . import views
 
+
+
 app_name = 'brands'
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$',      views.IndexView.as_view(),  name='index'),
+    url(r'^add$',   views.BrandCreate.as_view(),name='brand-add'),  # works
+    url(r'^(?P<pk>[0-9]+)/$',
+                    views.BrandDetail.as_view(),name='brand-detail'),
+    url(r'^(?P<pk>[0-9]+)/delete/$',
+                    views.BrandDelete.as_view(),name='brand-delete'),
+    url(r'^(?P<pk>[0-9]+)/edit/$',
+                    views.BrandUpdate.as_view(),name='brand-edit'),
+    
+]
+
+'''    
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^new$', views.CreateBrandView.as_view(),
         name='brands-new',),
-]
-
+'''
