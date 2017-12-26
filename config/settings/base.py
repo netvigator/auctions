e3c24004237fe370791e0295e4194ac980643e2f
Rick Graves <gravesricharde@yahoo.com>
2017-12-26 19:56:12 +0700

    add django_countries to list of installed 3rd party apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -98,6 +98,7 @@ THIRD_PARTY_APPS = [
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'django_countries',
 ]
 
 # Apps specific for this project go here.
