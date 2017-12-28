60d61d93a23110373cfaf4fe1d60c5b906deae6f
Rick Graves <gravesricharde@yahoo.com>
2017-12-28 18:19:50 +0700

    implement some brand tests, more to come

--- brands/tests.py
+++ brands/tests.py
@@ -1,6 +1,11 @@
-from django.test import TestCase
+from django.test        import TestCase
 from django.test.client import Client
 from django.test.client import RequestFactory
+from django.urls        import reverse
+from django.contrib.auth import get_user_model
+
+from pprint import pprint
+from six    import print_ as print3
 
 
 # Create your tests here.
@@ -16,6 +21,97 @@ class ModelModelTest(TestCase):
         sBrand = "My brand name"
         oBrand = Brand(cTitle= sBrand )
         self.assertEqual(str(sBrand), oBrand.cTitle)
+
+
+def createBrand( sName, oUser ):
+    #
+    oBrand = Brand(cTitle = sName, iUser = oUser )
+    oBrand.save()
+    #
+    return oBrand.id
+
+    
+class BrandViewsTests(TestCase):
+    """Brand views tests."""
+    
+    def setUp(self):
+        
+        oUser = get_user_model()
+
+        self.user1 = oUser.objects.create_user( 'username1', 'email@ymail.com' )
+        self.user1.set_password( 'mypassword')
+        self.user1.first_name = 'John'
+        self.user1.last_name = 'Citizen'
+        self.user1.save()
+        
+        # print3( 'user1.id:', user1.id )
+        
+        self.user2 = oUser.objects.create_user( 'username2', 'email@gmail.com' )
+        self.user2.set_password( 'mypassword')
+        self.user2.first_name = 'Joe'
+        self.user2.last_name = 'Blow'
+        self.user2.save()
+        
+        
+        '''
+        sBrand = "Proctor & Gamble"
+        oBrand = Brand(cTitle= sBrand, iUser = user1 )
+        
+        sBrand = "Lever Brothers"
+        oBrand = Brand(cTitle= sBrand, iUser = user2 )
+        '''
+        
+    def test_no_brands_yet(self):
+        #
+        """
+        If no brands exist, an appropriate message is displayed.
+        """
+        self.client = Client()
+        
+        self.client.login(username='username1', password='mypassword')
+        #
+        response = self.client.get(reverse('brands:index'))
+        #response = self.client.get('/brands/')
+        
+        #pprint( 'printing response:')
+        #pprint( response )
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['brand_list'], [])
+        self.assertContains(response, "No brands are available.")
+    
+    
+        
+    def test_got_brands(self):
+        #
+        """
+        If no brands exist, an appropriate message is displayed.
+        """
+        self.client = Client()
+        
+        self.client.login(username='username1', password='mypassword')
+        #
+        sBrand = "Proctor & Gamble"
+        oBrand = Brand( cTitle= sBrand, iUser = self.user1 )
+        oBrand.save()
+        
+        sBrand = "Lever Brothers"
+        oBrand = Brand( cTitle= sBrand, iUser = self.user1 )
+        oBrand.save()
+
+        response = self.client.get(reverse('brands:index'))
+        #response = self.client.get('/brands/')
+        
+        #pprint( 'printing response:')
+        #pprint( response )
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['brand_list'],
+                ['<Brand: Lever Brothers>', '<Brand: Proctor & Gamble>'] )
+        self.assertContains(response, "Lever Brothers")
+    
+    
+
 
 '''
 class BrandListViewTests(TestCase):
