2e5bd78c57daf250db7c33628192e6729c7005cd
Rick Graves <gravesricharde@yahoo.com>
2017-12-29 15:00:30 +0700

    for ebay categories, add to views.py to show category list via mptt

--- ebaycategories/views.py
+++ ebaycategories/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts   import render
+
+from .models            import EbayCategory
 
 # Create your views here.
+
+def show_ebay_categories(request):
+    return render_to_response("ebay_categories.html",
+        {'nodes':EbayCategory.objects.all()},
+        context_instance=RequestContext(request))
