f005ba5c5cdf2af6298027b83d8f24945ed1e058
Rick Graves <gravesricharde@yahoo.com>
2017-12-30 12:30:19 +0700

    use mptt built in admin class for admin

--- ebaycategories/admin.py
+++ ebaycategories/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 
+from mptt.admin import MPTTModelAdmin
+
 # Register your models here.
 
 from .models import EbayCategory, Condition
 
-
-class EbayCategoryAdmin(admin.ModelAdmin):
+'''
+class EbayCategoryAdmin(MPTTModelAdmin):
     list_display = ( "iMarket", "iCategoryID", "cTitle", "iLevel",
                      "iParentID", "bLeafCategory", "iTreeVersion" )
+'''
 
 class ConditionAdmin(admin.ModelAdmin):
     list_display = ( "iConditionID", "cTitle" )
 
-admin.site.register(EbayCategory, EbayCategoryAdmin)
+admin.site.register(EbayCategory, MPTTModelAdmin)
 
 admin.site.register( Condition )
