0076c6dbebd4b647b130f949cfe133861e41e4fb
Rick Graves <gravesricharde@yahoo.com>
2017-12-30 18:05:28 +0700

    mptt wants "name" as the column name, must use default django admin for ebay categories because table will contain multiple trees

--- ebaycategories/admin.py
+++ ebaycategories/admin.py
@@ -6,15 +6,14 @@ from mptt.admin import MPTTModelAdmin
 
 from .models import EbayCategory, Condition
 
-'''
-class EbayCategoryAdmin(MPTTModelAdmin):
-    list_display = ( "iMarket", "iCategoryID", "cTitle", "iLevel",
+class EbayCategoryAdmin(admin.ModelAdmin):
+    list_display = ( "iMarket", "iCategoryID", "name", "iLevel",
                      "iParentID", "bLeafCategory", "iTreeVersion" )
-'''
 
 class ConditionAdmin(admin.ModelAdmin):
     list_display = ( "iConditionID", "cTitle" )
 
-admin.site.register(EbayCategory, MPTTModelAdmin)
+# admin.site.register(EbayCategory, MPTTModelAdmin)
+admin.site.register(EbayCategory, EbayCategoryAdmin)
 
 admin.site.register( Condition )
