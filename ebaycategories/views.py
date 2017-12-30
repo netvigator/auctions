130e595eb087dadebbdff9dec005c53d6e42dfc9
Rick Graves <gravesricharde@yahoo.com>
2017-12-30 10:14:38 +0700

    implement mptt for ebay categories, needs tweaking tho

--- ebaycategories/views.py
+++ ebaycategories/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts   import render
 
+from django.shortcuts   import render_to_response
+from django.template    import RequestContext
+
 from .models            import EbayCategory
 
 # Create your views here.
 
 def show_ebay_categories(request):
-    return render_to_response("ebay_categories.html",
-        {'nodes':EbayCategory.objects.all()},
-        context_instance=RequestContext(request))
+    context = {'nodes':EbayCategory.objects.all()}
+    return render(request, 'ebaycategories/ebay_categories.html', context)
