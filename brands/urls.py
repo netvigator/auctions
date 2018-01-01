51e44a681035b4d3bfc7f3eabed90f697744dd79
Rick Graves <gravesricharde@yahoo.com>
2018-01-01 07:16:54 +0700

    implement cookiecutter great idea: explicitly name vars in urls.py urls

--- brands/urls.py
+++ brands/urls.py
@@ -7,14 +7,26 @@ from . import views
 app_name = "brands"
 
 urlpatterns = [
-    url(r'^$',      views.IndexView.as_view(),  name='index'),
-    url(r'^add$',   views.BrandCreate.as_view(),name='add'  ),
-    url(r'^(?P<pk>[0-9]+)/$',
-                    views.BrandDetail.as_view(),name='detail'),
-    url(r'^(?P<pk>[0-9]+)/delete/$',
-                    views.BrandDelete.as_view(),name='delete'),
-    url(r'^(?P<pk>[0-9]+)/edit/$',
-                    views.BrandUpdate.as_view(),name='edit' ),
+    url(
+        regex   = r'^$',
+        view    = views.IndexView.as_view(),
+        name    = 'index' ),
+    url(
+        regex   = r'^add$',
+        view    = views.BrandCreate.as_view(),
+        name    = 'add'  ),
+    url(
+        regex   = r'^(?P<pk>[0-9]+)/$',
+        view    = views.BrandDetail.as_view(),
+        name    = 'detail'),
+    url(
+        regex   = r'^(?P<pk>[0-9]+)/delete/$',
+        view    = views.BrandDelete.as_view(),
+        name    = 'delete'),
+    url(
+        regex   = r'^(?P<pk>[0-9]+)/edit/$',
+        view    = views.BrandUpdate.as_view(),
+        name    = 'edit' ),
     
 ]
 
