aa4840c80798a58fe9eac8ab13d5a1139f37e633
Rick Graves <gravesricharde@yahoo.com>
2018-01-02 07:20:11 +0700

    now allow either upper or lower case to access ebay category listing

--- ebaycategories/views.py
+++ ebaycategories/views.py
@@ -9,7 +9,7 @@ from markets.models     import Market
 # Create your views here.
 
 def show_ebay_categories(request, sMarket):
-    iMarket = Market.objects.get( cMarket = sMarket )
+    iMarket = Market.objects.get( cMarket = sMarket.upper() )
     context = { 'nodes':EbayCategory.objects.filter(
         iMarket = iMarket, iTreeVersion = iMarket.iCategoryVer ) }
     return render(request, 'ebaycategories/ebay_categories.html', context)
