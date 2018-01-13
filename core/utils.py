e231655b3621a3fdaba878e1bca3cecb9a2b0b45
Rick Graves <gravesricharde@yahoo.com>
2018-01-13 11:23:04 -0800

    move creating user_one to separate file to simplify testing

--- core/utils.py
+++ core/utils.py
@@ -1,35 +1,9 @@
 # misc utils can go here
-from django.contrib.auth        import get_user_model
 from django.db.models           import ForeignKey
 
-oUser = get_user_model()
-
-oUserOne = oUser.objects.filter( id = 1 ).first()
 
 #                "2017-12-15T05:22:47.000Z"
 EBAY_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.000Z'
-
-if not oUserOne:
-    #
-    '''
-    oUser = User()
-    #
-    oUser.first_name    = 'Rick'
-    oUser.last_name     = 'Graves'
-    oUser.email         = 'gravesricharde@yahoo.com'
-    oUser.username      = 'netvigator'
-    oUser.password      = 'tba'
-    #
-    oUser.save()
-    #
-    oUserOne = oUser
-    '''
-    oUserOne = get_user_model().objects.create_user(
-        'netvigator',
-        email           = 'gravesricharde@yahoo.com',
-        password        = None,
-        first_name      = 'Rick',
-        last_name       = 'Graves', )
 
 
 def getNamerSpacer( sRootTag, sXmlNameSpace = 'urn:ebay:apis:eBLBaseComponents' ):
