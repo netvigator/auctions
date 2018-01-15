f41b8e751f19b9f867d2bb467bf0271306761769
Rick Graves <gravesricharde@yahoo.com>
2018-01-15 10:19:40 -0800

    accomodate experimental drill down tree for ebay categories (not working)

--- ebaycategories/urls.py
+++ ebaycategories/urls.py
@@ -8,7 +8,13 @@ app_name = "ebaycategories"
 
 urlpatterns = [
     url(
-        regex   = r'^(?P<sMarket>(EBAY|ebay)-[A-Za-z]{2,5})/$',
+        # regex   = r'^(?P<sMarket>(EBAY|ebay)-[A-Za-z]{2,5})/$',
+        regex   = r'^(?P<sMarket>ebay-[a-z]{2,5})/$',
         view    = views.show_ebay_categories,
-        name    = 'marketindex' ),
+        name    = 'ebay_categories_index' ),
+    url(
+        regex   = r'^(?P<sMarket>EBAY-[A-Z]{2,5})/$',
+        view    = views.show_ebay_tree,
+        name    = 'ebay_categories_tree' ),
     ]
+    
