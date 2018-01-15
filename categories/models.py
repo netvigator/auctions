c0f1ed2720d82b33df48c32bdcaad6c5e5f8d0c3
Rick Graves <gravesricharde@yahoo.com>
2018-01-15 10:18:21 -0800

    add help_text for most columns

--- categories/models.py
+++ categories/models.py
@@ -16,34 +16,46 @@ class Category(models.Model):
     cTitle          = models.CharField(
                         'category description', max_length = 48, db_index = True)
     cKeyWords       = models.CharField( 'category key words', max_length = 88,
-                                        null = True, blank = True )
+                                        null = True, blank = True,
+        help_text = 'Bot will look for this text in the item description' )
     bKeyWordRequired= models.BooleanField(
-                        'key word required?', default = False )
+                        'key word required?', default = False,
+        help_text = 'Bot will know this model is for sale only '
+                    'if these key words are in the description' )
     cLookFor        = models.TextField(
-                        'Considered a hit if this text is found '
-                        '(each line evaluated separately, '
-                        'put different look for tests on different lines)',
-                        null=True, blank = True )
+                        'Considered a hit if this text is found',
+                        null=True, blank = True,
+        help_text = 'Considered a hit if this text is found -- '
+                    'each line evaluated separately, '
+                    'put different look for tests on different lines'  )
     iStars          = IntegerRangeField(
                         'desireability, 10 star category is most desireable',
                             min_value = 0, max_value = 10, default = 5 )
     bAllOfInterest  = models.BooleanField(
-                        'want everything of this category?', default = False )
-    bWantPair       = models.BooleanField('only want pairs?', default = False)
+                        'want everything of this category?', default = False,
+        help_text = 'Definitely set to True for desireable & rare categories'  )
+    bWantPair       = models.BooleanField('only want pairs?', default = False,
+        help_text = 'are you hoping to find these in paris?' )
     bAccessory      = models.BooleanField('accessory?', default = False)
     bComponent      = models.BooleanField('component?', default = False)
     iFamily         = models.ForeignKey( 'self',
-                        verbose_name = 'category family', null = True, blank = True )
+                        verbose_name = 'category family',
+                        null = True, blank = True,
+        help_text = 'you can group some categories into families, '
+                    'choose a category to be the lead' )
     cExcludeIf      = models.TextField(
-                        'Not a hit if this text is found '
-                        '(each line evaluated separately, '
-                        'put different exclude tests on different lines)',
-                        null = True, blank = True )
+                        'Not a hit if this text is found',
+                        null = True, blank = True,
+        help_text = 'Not a hit if this text is found '
+                    '(each line evaluated separately, '
+                    'put different exclude tests on different lines)' )
     iLegacyKey      = models.PositiveIntegerField( 'legacy key', unique=True )
     iLegacyFamily   = models.PositiveIntegerField( 'legacy family',
                                                     null = True )
     bModelsShared   = models.BooleanField(  'brands share model numbers',
-                                                    default = False)
+                                                    default = False,
+        help_text = 'Set to True if different brands use the same model '
+                    'names or numbers in this category' )
     tLegacyCreate   = models.DateTimeField( 'legacy row created on' )
     tLegacyModify   = models.DateTimeField( 'legacy row updated on',
                                             null = True )
