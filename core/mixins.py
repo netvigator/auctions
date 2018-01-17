7a361025f8afaa353903a640209781df276e6a40
Rick Graves <gravesricharde@yahoo.com>
2018-01-17 11:25:52 -0800

    add more verbose docstring about the DoesLoggedInUserOwnThisRowMixin

--- core/mixins.py
+++ core/mixins.py
@@ -2,7 +2,14 @@
 from django.core.exceptions import PermissionDenied
 
 class DoesLoggedInUserOwnThisRowMixin(object):
-
+    
+    '''
+    For some tables (Brands, Categories, Models, Searches, Items, and more),
+    users should ONLY be able to their own records (rows).
+    So we will overried get_object() and test there.
+    This is tested in the Brands app!
+    '''
+    
     def get_object(self):
         '''only allow owner (or superuser) to access the table row'''
         obj = super(DoesLoggedInUserOwnThisRowMixin, self).get_object()
@@ -13,4 +20,4 @@ class DoesLoggedInUserOwnThisRowMixin(object):
                 "Permission Denied -- that's not your record!")
         return obj
 
-    
+
