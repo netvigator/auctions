fe0fa428f0e21b03028e3691d61c5dfa91a84694
Rick Graves <gravesricharde@yahoo.com>
2018-01-17 19:06:31 -0800

    implement several searching tests

--- searching/tests.py
+++ searching/tests.py
@@ -1,23 +1,25 @@
-from django.test            import TestCase
-from django.contrib.auth    import get_user_model
+from django.test                import TestCase, RequestFactory
+from django.contrib.auth        import get_user_model
+from django.core.urlresolvers   import reverse, resolve
 
-from .forms                 import AddOrUpdateForm
-from .models                import Search
+from .forms                     import AddOrUpdateForm
+from .models                    import Search
+from .views                     import (
+        SearchCreate, IndexView, SearchDetail, SearchDelete, SearchUpdate )
 
-from core.tests             import getDefaultMarket
-from markets.models         import Market
-from ebaycategories.models  import EbayCategory
+
+from core.tests                 import getDefaultMarket
+from markets.models             import Market
+from ebaycategories.models      import EbayCategory
 
 # Create your tests here.
 
-
-
-
-
-class SearchModelTest(TestCase):
+class BaseUserTestCase(TestCase):
 
     def setUp(self):
 
+        self.factory = RequestFactory()
+        
         getDefaultMarket( self )
 
         oUser = get_user_model()
@@ -48,16 +50,93 @@ class SearchModelTest(TestCase):
             iMarket     = self.market,
             bLeafCategory = False )
         self.ebc.save()
+
+
+class TestSearchCreateView(BaseUserTestCase):
+    
+    def setUp(self):
+
+        # call BaseUserTestCase.setUp()
+        super(TestSearchCreateView, self).setUp()
+        # Instantiate the view directly. Never do this outside a test!
+        self.view = SearchCreate()
+        # Generate a fake request
+        request = self.factory.get('/fake-url')
+        # Attach the user to the request
+        request.user = self.user1
+        # Attach the request to the view
+        self.view.request = request
+
+'''
+    def test_get_success_url(self):
+        # Expect: '/users/testuser/', as that is the default username for
+        #   self.make_user()
+        self.assertEqual(
+            self.view.get_success_url(),
+            '/searching/index/'
+        )
+    def test_get_object(self):
+        # Expect: self.user, as that is the request's user object
+        self.assertEqual(
+            self.view.get_object(),
+            self.user
+        )
+
+'''
+class TestURLs(BaseUserTestCase):
+
+    def test_get_absolute_url(self):
+        oSearch = Search(
+            cTitle          = "My clever search",
+            cKeyWords       = "Blah bleh blih",
+            cPriority       = "A",
+            iUser           = self.user1,
+            id              = 1 )
+        oSearch.save()
+            
+        self.assertEqual(
+            oSearch.get_absolute_url(),
+            '/searching/1/'
+        )
+
+    def test_list_reverse(self):
+        """searches:index should reverse to /searching/."""
+        self.assertEqual(reverse('searching:index'), '/searching/')
+
+
+    def test_list_resolve(self):
+        """/searching/ should resolve to searching:index."""
+        self.assertEqual(resolve('/searching/').view_name, 'searching:index')
+
+    def test_detail_reverse(self):
+        """searching:detail should reverse to /searching/<pk>/."""
+        self.assertEqual(
+            reverse('searching:detail', kwargs={ 'pk': 1 }),
+            '/searching/1/'
+        )
+
+    def test_edit_reverse(self):
+        """searching:edit should reverse to /searching/edit/."""
+        self.assertEqual(reverse('searching:edit', kwargs={ 'pk': 1 }),
+                         '/searching/1/edit/')
+
+    def test_update_resolve(self):
+        """/searching/~update/ should resolve to searching:update."""
+        self.assertEqual(
+            resolve('/searching/1/edit/').view_name,
+            'searching:edit'
+        )
+
+
+
+class SearchModelTest(BaseUserTestCase):
         
     def test_string_representation(self):
         sSearch = "My clever search"
         oSearch = Search( cTitle = sSearch )
         self.assertEqual( str(oSearch), oSearch.cTitle )
-
-
     
-    def AddNewSearchTest( TescCase ):
-        
+    def AddNewSearchTest( TescCase ):        
         form = AddOrUpdateForm(
                 cTitle          = "My clever search",
                 cKeyWords       = "Blah bleh blih",
