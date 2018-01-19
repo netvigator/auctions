62c02e5e35e59ecc5a327b997f43b09b5dff48cb
Rick Graves <gravesricharde@yahoo.com>
2018-01-19 08:33:51 -0800

    implement ModelViewsTests

--- models/tests.py
+++ models/tests.py
@@ -1,7 +1,10 @@
 from django.core.urlresolvers   import reverse, resolve
 from django.test                import TestCase
+from django.test.client         import Client
 
 from core.tests                 import getDefaultMarket, BaseUserTestCase
+from core.utils                 import getExceptionMessageFromResponse
+
 from categories.models          import Category
 
 from .models                    import Model
@@ -21,18 +24,15 @@ class TestURLs(BaseUserTestCase):
 
     def test_get_absolute_url(self):
         
-        oCategory           = Category.objects.get( pk = 1 )
-        
         oModel = Model(
             cTitle          = "My stellar model",
             iUser           = self.user1,
-            iCategory       = oCategory,
-            id              = 1 )
+            iCategory       = self.oCategory )
         oModel.save()
-            
+        
         self.assertEqual(
             oModel.get_absolute_url(),
-            '/models/1/'
+            '/models/%s/' % oModel.id
         )
 
     def test_list_reverse(self):
@@ -70,3 +70,88 @@ class ModelModelTest(TestCase):
         sModel = "My model name/number"
         oModel = Model(cTitle= sModel )
         self.assertEqual(str(sModel), oModel.cTitle)
+
+
+
+
+class ModelViewsTests(BaseUserTestCase):
+    """Model views tests."""
+    
+        
+    def test_no_models_yet(self):
+        #
+        """
+        If no models exist, an appropriate message is displayed.
+        """
+        self.client = Client()
+        
+        self.client.login(username='username1', password='mypassword')
+        #
+        response = self.client.get(reverse('models:index'))
+        #response = self.client.get('/models/')
+        
+        #pprint( 'printing response:')
+        #pprint( response )
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['model_list'], [])
+        self.assertContains(response, "No models are available.")
+    
+    
+        
+    def test_got_models(self):
+        #
+        """
+        If models exist, an appropriate message is displayed.
+        """
+        self.client = Client()
+        
+        self.client.login(username='username1', password='mypassword')
+        #
+        
+        oModel      = Model(
+            cTitle = "Crest", iUser = self.user1, iCategory = self.oCategory )
+        oModel.save()
+        
+        oModel      = Model(
+            cTitle = "Colgate", iUser = self.user1, iCategory = self.oCategory )
+        oModel.save()
+        iLeverID    = oModel.id
+
+        response = self.client.get(reverse('models:index'))
+        #response = self.client.get('/models/')
+        
+        #pprint( 'printing response:')
+        #pprint( response )
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['model_list'],
+                ['<Model: Colgate>', '<Model: Crest>'] )
+        self.assertContains(response, "Colgate")
+        #
+        response = self.client.get(
+                reverse( 'models:detail', kwargs={ 'pk': iLeverID } ) )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Colgate")
+        
+        self.client.logout()
+        self.client.login(username='username2', password='mypassword')
+        
+        response = self.client.get(
+                reverse( 'models:detail', kwargs={ 'pk': iLeverID } ) )
+
+        self.assertEqual(response.status_code, 403) # forbidden
+        self.assertEqual(
+            getExceptionMessageFromResponse( response ),
+            "Permission Denied -- that's not your record!" )
+
+        self.client.logout()
+        self.client.login(username='username3', password='mypassword')
+        
+        response = self.client.get(
+                reverse( 'models:detail', kwargs={ 'pk': iLeverID } ) )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Colgate")
+
+
+
