8322a37301d5f60896dfd920baada704c222764a
Rick Graves <gravesricharde@yahoo.com>
2018-01-25 21:39:39 -0800

    implement WereAnyReleventColsChangedMixin, add tRegExRelevantCols

--- models/views.py
+++ models/views.py
@@ -4,13 +4,15 @@ from django.contrib.auth.mixins     import LoginRequiredMixin
 from django.http                    import HttpResponseRedirect
 from django.urls                    import reverse_lazy
 
-from core.mixins                    import DoesLoggedInUserOwnThisRowMixin
-
-from .models                        import Model
+from core.mixins                    import ( DoesLoggedInUserOwnThisRowMixin,
+                                             WereAnyReleventColsChangedMixin )
 
 from core.views                     import (
                     CreateViewGotCrispy, DeleteViewGotModel,
                     DetailViewGotModel, ListViewGotModel, UpdateViewGotCrispy )
+
+from .models                        import Model
+
 
 # Create your views here but keep them thin.
 
@@ -76,11 +78,19 @@ class ModelCreate( LoginRequiredMixin, CreateViewGotCrispy ):
 
 class ModelUpdate(
         LoginRequiredMixin, DoesLoggedInUserOwnThisRowMixin,
-        UpdateViewGotCrispy):
+        WereAnyReleventColsChangedMixin, UpdateViewGotCrispy):
 
     fields = tModelFields
     model = Model
     template_name = 'models/edit.html'
+
+    tRegExRelevantCols = (
+        'cTitle',
+        'cKeyWords',
+        'bKeyWordRequired',
+        'bsplitdigitsok',
+        'cLookFor',
+        'bSubModelsOK' )
 
     def post(self, request, *args, **kwargs):
         if "cancel" in request.POST:
