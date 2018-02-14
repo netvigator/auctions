884dd67c3b93d7c9aa28051ffe7b1921ba93a438
Rick Graves <gravesricharde@yahoo.com>
2018-02-14 17:42:27 +0700

    remove ItemHit model from ItemHits app, move here as model ItemFound

--- searching/models.py
+++ searching/models.py
@@ -1,5 +1,12 @@
 from django.db                  import models
+from django_countries.fields    import CountryField
 from djmoney.models.fields      import MoneyField
+
+# Create your models here.
+
+from models.models          import Model
+from brands.models          import Brand
+from categories.models      import Category
 
 # Create your models here.
 
@@ -64,6 +71,71 @@ class Search(models.Model):
         #
         return getReverseWithQueryUTC( 'searching:detail',
                     kwargs={ 'pk': self.pk } )
+
+
+class ItemFound(models.Model):
+    iItemNumb       = models.BigIntegerField(
+                        'ebay item number', primary_key = True )
+    cTitle          = models.CharField(
+                        'auction headline', max_length = 48, db_index = True )
+    cLocation       = models.CharField( 'location',
+                        max_length = 48, null = True )
+    cCountry        = CountryField( "country", null = True )
+    cMarket         = models.CharField( 'market Global ID',
+                        max_length = 14, null = True )
+    cGalleryURL     = models.CharField( 'gallery pic URL',
+                        max_length = 88, null = True )
+    cEbayItemURL    = models.CharField( 'ebay item URL',
+                        max_length =188, null = True )
+    tTimeBeg        = models.DateTimeField( 'beginning date/time',null=True )
+    tTimeEnd        = models.DateTimeField( 'ending date/time',   null=True )
+    bBestOfferable  = models.BooleanField( 'best offer enabled?',
+                                            default = False )
+    bBuyItNowable   = models.BooleanField( 'buy it now enabled?',
+                                            default = False )
+    cListingType    = models.CharField( 'listing type', max_length = 15,
+                                            null = True )
+    mCurrentPrice   = MoneyField( 'current price (local currency)',
+                        max_digits = 10, decimal_places = 2,
+                        default_currency='USD', null = True )
+    nCurrentPrice   = models.DecimalField(
+                        'current price (converted to USD)',
+                        max_digits=10, decimal_places=2,
+                        db_index = False, null = True )
+    iCategoryID     = models.IntegerField( 'primary category ID', null=True )
+    cCategory       = models.CharField( 'primary category',
+                        max_length = 48, null = True )
+    
+    iConditionID    = models.IntegerField( 'condition ID', null = True )
+    cCondition      = models.CharField( 'condition display name',
+                        max_length = 28, null = True )
+    
+    cSellingState   = models.CharField( 'selling state',
+                        max_length = 18, null = True )
+    
+    dhitstars       = models.DecimalField(
+                        'hit stars', max_digits = 3, decimal_places = 2,
+                        null = True )
+    bitemhit        = models.BooleanField( 'item of interest?',
+                        default = False )
+    
+    tlook4hits      = models.DateTimeField(
+                        'assessed interest date/time', null = True )
+    iModel          = models.ForeignKey( Model,     null = True )
+    iBrand          = models.ForeignKey( Brand,     null = True )
+    iCategory       = models.ForeignKey( Category,  null = True )
+    iUser           = models.ForeignKey( User, verbose_name = 'Owner' )
+    tCreate         = models.DateTimeField( 'created on', auto_now_add=True )
+    tModify         = models.DateTimeField(
+                        'retrieved info date/time', auto_now = True )
+    
+    def __str__(self):
+        return self.cTitle
+        
+    class Meta:
+        verbose_name_plural = 'itemsfound'
+        db_table            = verbose_name_plural
+
 
 '''
 mistake! this is info obtained by looing up the listing, not from the finding API!
