a4a05a415dac5560cb1f8c82fa540a870f0714ea
Rick Graves <gravesricharde@yahoo.com>
2018-02-17 03:48:22 +0700

    code previously here under test_AddNewSearch now in test_views as test_form_valid

--- searching/tests/test_models.py
+++ searching/tests/test_models.py
@@ -1,9 +1,9 @@
 from core.tests                 import BaseUserTestCase
 
-from .forms                     import SearchAddOrUpdateForm
-from .models                    import Search
-from .test_big_text             import sExampleResponse
-from .utils                     import getSearchResultGenerator
+from ..forms                     import SearchAddOrUpdateForm
+from ..models                    import Search
+from ..test_big_text             import sExampleResponse
+from ..utils                     import getSearchResultGenerator
 
 
 
@@ -14,50 +14,5 @@ class SearchModelTest(BaseUserTestCase):
         sSearch = "My clever search 2"
         oSearch = Search( cTitle = sSearch )
         self.assertEqual( str(oSearch), oSearch.cTitle )
-
-    def test_AddNewSearch(self):
-
-        # has key words
-        dData = dict(
-                cTitle          = "My clever search 3",
-                cKeyWords       = "Blah bleh blih",
-                cPriority       = "A",
-                which           = 'Create',
-                iUser           = self.user1 )
-        form = SearchAddOrUpdateForm( data = dData )
-        form.request = self.request
-        self.assertTrue( form.is_valid() )
-
-        # has a category
-        dData = dict(
-                cTitle          = "My clever search 4",
-                iDummyCategory  = 10,
-                cPriority       = "A",
-                which           = 'Create',
-                iUser           = self.user1 )
-        form = SearchAddOrUpdateForm( data = dData )
-        form.request = self.request
-
-        self.assertTrue( form.is_valid() )
-        # no key words, no category
-        dData = dict(
-                cTitle          = "My clever search 5",
-                cPriority       = "A",
-                which           = 'Create',
-                iUser           = self.user1 )
-        form = SearchAddOrUpdateForm( data = dData ) 
-        form.request = self.request
-        self.assertFalse( form.is_valid() )
-
-        # has an invalid category
-        dData = dict(
-                cTitle          = "My clever search 6",
-                iDummyCategory  = 'abc',
-                cPriority       = "A",
-                which           = 'Create',
-                iUser           = self.user1 )
-        form = SearchAddOrUpdateForm( data = dData )
-        form.request = self.request
-        self.assertFalse( form.is_valid() )
 
 
