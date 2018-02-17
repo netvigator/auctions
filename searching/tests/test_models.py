8933de1f7a9f7b3221dc164ce8d0a4acf627058b
Rick Graves <gravesricharde@yahoo.com>
2018-02-17 07:00:47 +0700

    add test_get_absolute_url

--- searching/tests/test_models.py
+++ searching/tests/test_models.py
@@ -1,9 +1,12 @@
-from core.tests                 import BaseUserTestCase
+from django.test.client import Client
 
-from ..forms                     import SearchAddOrUpdateForm
-from ..models                    import Search
-from ..test_big_text             import sExampleResponse
-from ..utils                     import getSearchResultGenerator
+from core.tests         import (
+                        BaseUserTestCase, getUrlQueryStringOff, queryGotUTC )
+
+from ..forms            import SearchAddOrUpdateForm
+from ..models           import Search
+from ..test_big_text    import sExampleResponse
+from ..utils            import getSearchResultGenerator
 
 
 
@@ -15,4 +18,19 @@ class SearchModelTest(BaseUserTestCase):
         oSearch = Search( cTitle = sSearch )
         self.assertEqual( str(oSearch), oSearch.cTitle )
 
+    def test_get_absolute_url(self):
+        #
+        self.client = Client()
+        #
+        self.client.login(username='username1', password='mypassword')
+        #
+        sSearch     = "Great Widgets"
+        oSearch     = Search( cTitle= sSearch, iUser = self.user1 )
+        oSearch.save()
+        #
+        tUrlParts   = getUrlQueryStringOff( oSearch.get_absolute_url() )
+        #
+        self.assertEqual( tUrlParts[0], '/searching/%s/' % oSearch.id )
+        #
+        self.assertTrue( queryGotUTC( tUrlParts[1] ) )
 
