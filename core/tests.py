ceb3bc05ee436c8e11b7175e3bc59c92cfa9ca85
Rick Graves <gravesricharde@yahoo.com>
2018-02-17 09:53:26 +0700

    implenent setup_view_for_tests(), hope now views and forms are fully testable

--- core/tests.py
+++ core/tests.py
@@ -233,6 +233,19 @@ class NbspTests(TestCase):
                            "how&nbsp;now&nbsp;brown&nbsp;cow" )
 
 
+def setup_view_for_tests( view, request, *args, **kwargs ):
+    """
+    Mimic as_view() returned callable, but returns view instance.
+    args and kwargs are the same you would pass to ``reverse()``
+    """
+    #
+    view.request= request
+    view.args   = args
+    view.kwargs = kwargs
+    #
+    return view
+
+
 '''
 oTest = {
     'cNationality': ('Nationality', 'C'),
