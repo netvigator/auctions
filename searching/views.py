5be8eb2ef60df6934e683fc2200fcf87b708b8f3
Rick Graves <gravesricharde@yahoo.com>
2018-02-17 07:03:38 +0700

    rename EbayCategoryFormValidMixin to SearchFormValidSuccessPostMixin, move post method to mixin

--- searching/views.py
+++ searching/views.py
@@ -1,19 +1,12 @@
-from django.contrib.auth    import get_user_model
-from django.urls            import reverse_lazy
-from django.http            import HttpResponseRedirect
-
-from .forms                 import SearchAddOrUpdateForm
-from .mixins                import EbayCategoryFormValidMixin
-from .models                import Search
-
 from core.views             import (
                                 CreateViewGotCrispy, DeleteViewGotModel,
                                 DetailViewGotModel,  ListViewGotModel,
                                 UpdateViewGotCrispy )
 
-# Create your views here.
+from .mixins                import SearchFormValidSuccessPostMixin
+from .models                import Search
 
-User = get_user_model()
+# Create your views here.
 
 tModelFields = (
     'cTitle',
@@ -22,14 +15,9 @@ tModelFields = (
     'cPriority', )
 
 
-class SearchCreate( EbayCategoryFormValidMixin, CreateViewGotCrispy ):
+class SearchCreate( SearchFormValidSuccessPostMixin, CreateViewGotCrispy ):
 
-    form_class      = SearchAddOrUpdateForm
-    model           = Search
     template_name   = 'searching/add.html'
-    success_url     = reverse_lazy('searching:index')
-    #success_url = reverse_lazy('searching:detail', kwargs={'pk': self.object.id})
-
     success_message = 'New Search record successfully saved!!!!'
 
     def get_form(self, form_class=None):
@@ -38,19 +26,12 @@ class SearchCreate( EbayCategoryFormValidMixin, CreateViewGotCrispy ):
         form.which = 'Create'
         return form
 
-    def post(self, request, *args, **kwargs):
-        if "cancel" in request.POST:
-            url = reverse_lazy('searching:index')
-            return HttpResponseRedirect(url)
-        else:
-            return super(SearchCreate, self).post(request, *args, **kwargs)
 
 
 class IndexView( ListViewGotModel ):  
-    template_name   = 'searching/index.html'
-    # context_object_name = 'searching_list' # default
-    model           = Search
     
+    template_name   = 'searching/index.html'
+    model           = Search
 
 
 class SearchDetail( DetailViewGotModel ):
@@ -61,27 +42,16 @@ class SearchDetail( DetailViewGotModel ):
 
 
 class SearchDelete( DeleteViewGotModel ):
-    model           = Search
-    template_name   = 'confirm_delete.html'
-    success_url     = reverse_lazy('searching:index')
 
+    template_name   = 'confirm_delete.html'
     success_message = 'Search record successfully deleted!!!!'
 
-    def post(self, request, *args, **kwargs):
-        if "cancel" in request.POST:
-            self.object = self.get_object()
-            url = reverse_lazy('searching:detail', kwargs={'pk': self.object.id})
-            return HttpResponseRedirect(url)
-        else:
-            return super(SearchDelete, self).post(request, *args, **kwargs)
 
 
 
-class SearchUpdate(  EbayCategoryFormValidMixin, UpdateViewGotCrispy ):
-    model           = Search
-    form_class      = SearchAddOrUpdateForm
+class SearchUpdate(  SearchFormValidSuccessPostMixin, UpdateViewGotCrispy ):
+
     template_name   = 'searching/edit.html'
-
     success_message = 'Search record update successfully saved!!!!'
 
     def get_form(self, form_class=None):
@@ -91,11 +61,4 @@ class SearchUpdate(  EbayCategoryFormValidMixin, UpdateViewGotCrispy ):
         return form
 
 
-    def post(self, request, *args, **kwargs):
-        if "cancel" in request.POST:
-            self.object = self.get_object()
-            url = reverse_lazy('searching:detail', kwargs={'pk': self.object.id})
-            return HttpResponseRedirect(url)
-        else:
-            return super(SearchUpdate, self).post(request, *args, **kwargs)
 
