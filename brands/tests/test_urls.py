f9853c4dbe86c6b76bffab283203d4694a02f50b
Rick Graves <gravesricharde@yahoo.com>
2018-02-18 20:48:57 +0700

    class TestURLs moved from tests.py to here

---
+++ brands/tests/test_urls.py
@@ -0,0 +1,58 @@
+from django.core.urlresolvers   import reverse, resolve
+
+from core.test_utils            import ( BaseUserTestCase,
+                                         getUrlQueryStringOff, queryGotUTC )
+
+# Create your tests here.
+
+from ..models                   import Brand
+
+
+
+class TestURLs(BaseUserTestCase):
+
+    def test_get_absolute_url(self):
+        oBrand = Brand(
+            cTitle          = "My premium brand",
+            iUser           = self.user1,
+            id              = 1 )
+        oBrand.save()
+        #
+        tParts = getUrlQueryStringOff( oBrand.get_absolute_url() )
+        #
+        self.assertEqual( tParts[0], '/brands/%s/' % oBrand.id )
+        #
+        self.assertTrue( queryGotUTC( tParts[1] ) )
+        #
+        self.assertFalse( queryGotUTC( tParts[0] ) )
+        
+
+    def test_list_reverse(self):
+        """searches:index should reverse to /brands/."""
+        self.assertEqual(reverse('brands:index'), '/brands/')
+
+
+    def test_list_resolve(self):
+        """/brands/ should resolve to brands:index."""
+        self.assertEqual(resolve('/brands/').view_name, 'brands:index')
+
+    def test_detail_reverse(self):
+        """brands:detail should reverse to /brands/<pk>/."""
+        self.assertEqual(
+            reverse('brands:detail', kwargs={ 'pk': 1 }),
+            '/brands/1/'
+        )
+
+    def test_edit_reverse(self):
+        """brands:edit should reverse to /brands/edit/."""
+        self.assertEqual(reverse('brands:edit', kwargs={ 'pk': 1 }),
+                         '/brands/1/edit/')
+
+    def test_update_resolve(self):
+        """/brands/~update/ should resolve to brands:update."""
+        self.assertEqual(
+            resolve('/brands/1/edit/').view_name,
+            'brands:edit'
+        )
+
+
