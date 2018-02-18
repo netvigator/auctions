3907fad2672f1fa74c989ead24de75256d58a2c0
Rick Graves <gravesricharde@yahoo.com>
2018-02-18 20:43:57 +0700

    put back foreign keys to user, django can now run

--- auctionbot/users/models.py
+++ auctionbot/users/models.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.urlresolvers import reverse
-from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers   import reverse
+from django.db                  import models
+from django.utils.encoding      import python_2_unicode_compatible
+from django.utils.translation   import ugettext_lazy as _
 
-from markets.models     import Market
+from ebayinfo.models            import Market
 
 iEbayUSA = Market.objects.get( cMarket = 'EBAY-US' ).id or 1
 
@@ -12,6 +12,7 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name        = models.CharField(_('Name of User'), blank=True, max_length=255)
+    #models.ForeignKey( Market, models.PositiveIntegerField( 
     iMarket     = models.ForeignKey( Market,
                     verbose_name = 'ebay market (default)',
                     default = iEbayUSA )
