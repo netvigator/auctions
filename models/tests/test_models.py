1b0e4ac4eb49a95659a1e6d07388bb8d66f75632
Rick Graves <gravesricharde@yahoo.com>
2018-02-18 21:01:11 +0700

    class ModelModelTest moved from tests.py to here

---
+++ models/tests/test_models.py
@@ -0,0 +1,11 @@
+from django.test                import TestCase
+
+from ..models                   import Model
+
+class ModelModelTest(TestCase):
+
+    def test_string_representation(self):
+        sModel = "My model name/number"
+        oModel = Model(cTitle= sModel )
+        self.assertEqual(str(sModel), oModel.cTitle)
+
