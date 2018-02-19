b7551e37f0b46a198220e20c6e6bc0e8700116c3
Rick Graves <gravesricharde@yahoo.com>
2018-02-20 06:38:10 +0700

    start testing core/utils.py

---
+++ core/tests/test_utils.py
@@ -0,0 +1,46 @@
+from django.test        import TestCase
+from django.utils       import timezone
+
+from ..utils            import _getIsoDateTimeOffDateTimeCol, getReverseWithUpdatedQuery
+from ..utils_testing    import getUrlQueryStringOff, queryGotUpdated
+
+from Time               import sFormatISOdateTimeNoColon
+from Time.Test          import isISOdatetime
+
+
+class DateTimeTests(TestCase):
+    '''date time function tests'''
+
+    def test_getIsoDateTimeOffDateTimeCol(self):
+        #
+        '''test _getIsoDateTimeOffDateTimeCol()'''
+        #
+        oNow = timezone.now()
+        #
+        sNow = _getIsoDateTimeOffDateTimeCol( oNow )
+        #
+        self.assertTrue( isISOdatetime( sNow, sFormatISOdateTimeNoColon ) )
+        
+    def test_getReverseWithUpdatedQuery(self):
+        #
+        self.pk         = 1
+        self.tModify    = timezone.now()
+        #
+        kwargs = { 'pk': self.pk, 'tModify': self.tModify }
+        #
+        sURL = getReverseWithUpdatedQuery( 'models:detail', kwargs = kwargs )
+        #
+        tParts = getUrlQueryStringOff( sURL )
+        #
+        self.assertEqual( tParts[0], '/models/%s/' % self.pk )
+        #
+        self.assertTrue( queryGotUpdated( tParts[1] ) )
+        #
+        self.assertFalse( queryGotUpdated( tParts[0] ) )
+
+        
+
+
+
+
+        
