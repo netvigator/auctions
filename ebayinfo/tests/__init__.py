859ae9a6bf81acb4a6a3833efef6d8e88613295e
Rick Graves <gravesricharde@yahoo.com>
2018-02-20 14:58:48 +0700

    put big text for testing here, sExampleCategoryVersion & sExampleCategoryList

---
+++ ebayinfo/tests/__init__.py
@@ -0,0 +1,102 @@
+
+# actually for 'EBAY-US' as of 2017-12
+sExampleCategoryVersion = \
+  '''<?xml version="1.0" encoding="UTF-8"?>
+    <GetCategoriesResponse xmlns="urn:ebay:apis:eBLBaseComponents">
+        <Timestamp>2017-12-12T04:45:28.766Z</Timestamp>
+        <Ack>Success</Ack>
+        <Version>1041</Version>
+        <Build>E1041_CORE_APICATALOG_18587827_R1</Build>
+        <UpdateTime>2017-06-13T02:06:57.000Z</UpdateTime>
+        <CategoryVersion>117</CategoryVersion>
+        <ReservePriceAllowed>true</ReservePriceAllowed>
+        <MinimumReservePrice>0.0</MinimumReservePrice>
+    </GetCategoriesResponse>'''
+
+sExampleCategoryList = \
+ '''<?xml version="1.0" encoding="UTF-8"?>
+<GetCategoriesResponse xmlns="urn:ebay:apis:eBLBaseComponents">
+    <Timestamp>2017-12-27T21:26:23.376Z</Timestamp>
+    <Ack>Success</Ack>
+    <Version>1041</Version>
+    <Build>E1041_CORE_APICATALOG_18587827_R1</Build>
+    <CategoryArray>
+        <Category>
+            <BestOfferEnabled>true</BestOfferEnabled>
+            <AutoPayEnabled>true</AutoPayEnabled>
+            <CategoryID>20081</CategoryID>
+            <CategoryLevel>1</CategoryLevel>
+            <CategoryName>Antiques</CategoryName>
+            <CategoryParentID>20081</CategoryParentID>
+        </Category>
+        <Category>
+            <BestOfferEnabled>true</BestOfferEnabled>
+            <AutoPayEnabled>true</AutoPayEnabled>
+            <CategoryID>37903</CategoryID>
+            <CategoryLevel>2</CategoryLevel>
+            <CategoryName>Antiquities</CategoryName>
+            <CategoryParentID>20081</CategoryParentID>
+        </Category>
+        <Category>
+            <BestOfferEnabled>true</BestOfferEnabled>
+            <AutoPayEnabled>true</AutoPayEnabled>
+            <CategoryID>37908</CategoryID>
+            <CategoryLevel>3</CategoryLevel>
+            <CategoryName>The Americas</CategoryName>
+            <CategoryParentID>37903</CategoryParentID>
+            <LeafCategory>true</LeafCategory>
+        </Category>
+        <Category>
+            <BestOfferEnabled>true</BestOfferEnabled>
+            <AutoPayEnabled>true</AutoPayEnabled>
+            <CategoryID>162922</CategoryID>
+            <CategoryLevel>3</CategoryLevel>
+            <CategoryName>Byzantine</CategoryName>
+            <CategoryParentID>37903</CategoryParentID>
+            <LeafCategory>true</LeafCategory>
+        </Category>
+        <Category>
+            <BestOfferEnabled>true</BestOfferEnabled>
+            <AutoPayEnabled>true</AutoPayEnabled>
+            <CategoryID>162923</CategoryID>
+            <CategoryLevel>3</CategoryLevel>
+            <CategoryName>Celtic</CategoryName>
+            <CategoryParentID>37903</CategoryParentID>
+            <LeafCategory>true</LeafCategory>
+        </Category>
+        <Category>
+            <BestOfferEnabled>true</BestOfferEnabled>
+            <AutoPayEnabled>true</AutoPayEnabled>
+            <CategoryID>37905</CategoryID>
+            <CategoryLevel>3</CategoryLevel>
+            <CategoryName>Egyptian</CategoryName>
+            <CategoryParentID>37903</CategoryParentID>
+            <LeafCategory>true</LeafCategory>
+        </Category>
+        <Category>
+            <BestOfferEnabled>true</BestOfferEnabled>
+            <AutoPayEnabled>true</AutoPayEnabled>
+            <CategoryID>162916</CategoryID>
+            <CategoryLevel>3</CategoryLevel>
+            <CategoryName>Far Eastern</CategoryName>
+            <CategoryParentID>37903</CategoryParentID>
+            <LeafCategory>true</LeafCategory>
+        </Category>
+        <Category>
+            <BestOfferEnabled>true</BestOfferEnabled>
+            <AutoPayEnabled>true</AutoPayEnabled>
+            <CategoryID>37906</CategoryID>
+            <CategoryLevel>3</CategoryLevel>
+            <CategoryName>Greek</CategoryName>
+            <CategoryParentID>37903</CategoryParentID>
+            <LeafCategory>true</LeafCategory>
+        </Category>
+
+    </CategoryArray>
+    <CategoryCount>19188</CategoryCount>
+    <UpdateTime>2017-06-13T02:06:57.000Z</UpdateTime>
+    <CategoryVersion>117</CategoryVersion>
+    <ReservePriceAllowed>true</ReservePriceAllowed>
+    <MinimumReservePrice>0.0</MinimumReservePrice>
+</GetCategoriesResponse>
+'''
