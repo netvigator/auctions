1c7ead258eb7d08edb8e0a6225e9e07babd240f7
Rick Graves <gravesricharde@yahoo.com>
2018-02-22 09:39:23 +0700

    put dItemFoundFields in __init__.py, may need elsewhere, no need to clutter utils.py

--- searching/__init__.py
+++ searching/__init__.py
@@ -1,3 +1,6 @@
+from core.utils     import getDateTimeObjGotEbayStr as getDT
+from Utils.Config   import getBoolOffYesNoTrueFalse as getBo
+
 
 dSearchResult = \
 {'autoPay': 'false',
@@ -40,3 +43,26 @@ dSearchResult = \
  'topRatedListing': 'true',
  'viewItemURL': 'http://www.ebay.com/itm/Digital-Capacitance-Tester-Capacitor-Meter-Auto-Range-MultimeterChecker-470mF-/282330751118'
  }
+
+dItemFoundFields = dict(
+    iItemNumb       = (int,  'itemId',),
+    cTitle          = (None, 'title',),
+    cLocation       = (None, 'location',),
+    cCountry        = (None, 'country',),
+    cMarket         = (None, 'globalId',),
+    cGalleryURL     = (None, 'galleryURL',),
+    cEbayItemURL    = (None, 'viewItemURL',),
+    tTimeBeg        = (getDT,'listingInfo','startTime'),
+    tTimeEnd        = (getDT,'listingInfo','endTime'),
+    bBestOfferable  = (getBo,'listingInfo','bestOfferEnabled'),
+    bBuyItNowable   = (getBo,'listingInfo','buyItNowAvailable'),
+    cListingType    = (None, 'listingInfo','listingType'),
+    lLocalCurrency  = (None, 'sellingStatus','currentPrice','@currencyId'),
+    lCurrentPrice   = (float,'sellingStatus','currentPrice','__value__'),
+    dCurrentPrice   = (float,'sellingStatus','convertedCurrentPrice','__value__'),
+    iCategoryID     = (int,  'primaryCategory','categoryId'),
+    cCategory       = (None, 'primaryCategory','categoryName'),
+    iConditionID    = (int,  'condition','conditionId'),
+    cCondition      = (None, 'condition','conditionDisplayName'),
+    cSellingState   = (None, 'sellingStatus','sellingState') )
+
