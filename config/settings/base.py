36b99341f8d30f027786100e3cc91da42686b991
Rick Graves <gravesricharde@yahoo.com>
2018-02-26 20:51:19 +0700

    paste in celery config from cookiecutter

--- config/settings/base.py
+++ config/settings/base.py
@@ -348,6 +348,14 @@ LOGIN_URL = 'account_login'
 # SLUGLIFIER
 AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 
+########## CELERY
+INSTALLED_APPS += ['auctionbot.taskapp.celery.CeleryConfig']
+CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='django://')
+if CELERY_BROKER_URL == 'django://':
+    CELERY_RESULT_BACKEND = 'redis://'
+else:
+    CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+########## END CELERY
 
 # Location of root django.contrib.admin URL, use {% url 'admin:index' %}
 ADMIN_URL = r'^%s/' % dSecretsConf[ 'admin_url' ]
