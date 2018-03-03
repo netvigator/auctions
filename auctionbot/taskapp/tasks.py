03d275430c1d23a6cc47f031968255c41c53616b
Rick Graves <gravesricharde@yahoo.com>
2018-03-03 20:25:50 +0700

    use shared_task decorator in django, also move celery.ping from here to celery.py

--- auctionbot/taskapp/tasks.py
+++ auctionbot/taskapp/tasks.py
@@ -2,14 +2,7 @@ from celery     import shared_task
 from .celery    import app
 
 
-@shared_task
+@shared_task( name = 'auctionbot.taskapp.tasks.add' )
 def add( i, j ): return i + j
 
 
-# https://stackoverflow.com/questions/46530784/make-django-test-case-database-visible-to-celery/46564964#46564964
-
-@app.task(name='celery.ping')
-def ping():
-    # type: () -> str
-    """Simple task that just returns 'pong'."""
-    return 'pong'
