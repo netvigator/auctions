11efeeeaf192e6147dd6fa545395076dfdc331c9
Rick Graves <gravesricharde@yahoo.com>
2018-03-03 20:25:07 +0700

    use shared_task decorator in django, also move celery.ping here

--- auctionbot/taskapp/celery.py
+++ auctionbot/taskapp/celery.py
@@ -1,9 +1,10 @@
-
 import os
-from celery import Celery
-from django.apps import apps, AppConfig
-from django.conf import settings
 
+from celery         import Celery
+from celery         import shared_task
+
+from django.apps    import apps, AppConfig
+from django.conf    import settings
 
 #if not settings.configured:
     ## set the default Django settings module for the 'celery' program.
@@ -20,16 +21,19 @@ class CeleryConfig(AppConfig):
     def ready(self):
         # Using a string here means the worker will not have to
         # pickle the object when using Windows.
-        app.config_from_object('django.conf:settings')
-        installed_apps = [app_config.name for app_config in apps.get_app_configs()]
+        app.config_from_object('django.conf:settings' , namespace='CELERY' )
+        installed_apps = [
+            app_config.name for app_config in apps.get_app_configs()]
         app.autodiscover_tasks(lambda: installed_apps, force=True)
 
         if hasattr(settings, 'RAVEN_CONFIG'):
             # Celery signal registration
 
             from raven import Client as RavenClient
-            from raven.contrib.celery import register_signal as raven_register_signal
-            from raven.contrib.celery import register_logger_signal as raven_register_logger_signal
+            from raven.contrib.celery \
+                import register_signal as raven_register_signal
+            from raven.contrib.celery \
+                import register_logger_signal as raven_register_logger_signal
 
 
             raven_client = RavenClient(dsn=settings.RAVEN_CONFIG['DSN'])
@@ -40,19 +44,32 @@ class CeleryConfig(AppConfig):
 
             from opbeat.contrib.django.models import client as opbeat_client
             from opbeat.contrib.django.models import logger as opbeat_logger
-            from opbeat.contrib.django.models import register_handlers as opbeat_register_handlers
+            from opbeat.contrib.django.models \
+                import register_handlers as opbeat_register_handlers
             from opbeat.contrib.celery import register_signal as opbeat_register_signal
 
 
             try:
                 opbeat_register_signal(opbeat_client)
             except Exception as e:
-                opbeat_logger.exception('Failed installing celery hook: %s' % e)
+                opbeat_logger.exception(
+                    'Failed installing celery hook: %s' % e)
 
             if 'opbeat.contrib.django' in settings.INSTALLED_APPS:
                 opbeat_register_handlers()
 
 
-@app.task(bind=True)
+@shared_task( bind = True, name = 'celery.debug_task' )
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))  # pragma: no cover
+
+
+# https://stackoverflow.com/questions/46530784/make-django-test-case-database-visible-to-celery/46564964#46564964
+# assert 'celery.ping' in app.tasks
+
+@shared_task( name = 'celery.ping' )
+def ping():
+    # type: () -> str
+    """Pretty sophisticated task that brilliantly returns 'pong'."""
+    return 'pong'
+
