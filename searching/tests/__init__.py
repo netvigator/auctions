b37fad3f9df3a5c3f53ccebc509f9f8c0daad625
Rick Graves <gravesricharde@yahoo.com>
2018-03-04 10:13:49 +0700

    search returned 2,374 items, must give feedback to owner/user

--- searching/tests/__init__.py
+++ searching/tests/__init__.py
@@ -617,3 +617,59 @@ sExampleResponse = \
     }
   ]
 }'''
+
+
+sResponseSearchTooBroad = \
+'''{"findItemsByKeywordsResponse":
+  [{"ack":["Success"],"version":["1.13.0"],"timestamp":["2018-03-03T23:04:24.581Z"],
+    "searchResult":
+      [{"@count":"100",
+        "item":
+          [ {   "itemId": ["122990519283"],
+                "title":["Garol 6AU-1 Catalin Radio "],
+                "globalId":["EBAY-US"],
+                "primaryCategory":[{"categoryId":["38034"],"categoryName":["1930-49"]}],
+                "galleryURL":["http:\/\/thumbs4.ebaystatic.com\/m\/mqcfM87dPUKfbuWlIpUTi-w\/140.jpg"],
+                "viewItemURL":["http:\/\/www.ebay.com\/itm\/Garol-6AU-1-Catalin-Radio-\/122990519283"],
+                "paymentMethod":["PayPal"],
+                "autoPay":["false"],
+                "postalCode":["15085"],
+                "location":["Trafford,PA,USA"],
+                "country":["US"],
+                "shippingInfo":[{"shippingType":["Calculated"],"shipToLocations":["Worldwide"],"expeditedShipping":["true"],"oneDayShippingAvailable":["false"],"handlingTime":["1"]}],
+                "sellingStatus":[{"currentPrice":[{"@currencyId":"USD","__value__":"563.88"}],"convertedCurrentPrice":[{"@currencyId":"USD","__value__":"563.88"}],"bidCount":["14"],"sellingState":["Active"],"timeLeft":["P0DT3H38M14S"]}],
+                "listingInfo":[{"bestOfferEnabled":["false"],"buyItNowAvailable":["false"],"startTime":["2018-02-27T02:42:38.000Z"],"endTime":["2018-03-04T02:42:38.000Z"],"listingType":["Auction"],"gift":["false"],"watchCount":["44"]}],
+                "returnsAccepted":["true"],
+                "condition":[{"conditionId":["3000"],"conditionDisplayName":["Used"]}],
+                "isMultiVariationListing":["false"],
+                "topRatedListing":["true"]},
+            {"itemId":["372238175241"],
+                "title":["VINTAGE 1940s FADA CATALIN BAKELITE RADIO MODEL 1000 CABINET  !!"],
+                "globalId":["EBAY-US"],
+                "primaryCategory":[{"categoryId":["38034"],"categoryName":["1930-49"]}],
+                "galleryURL":["http:\/\/thumbs2.ebaystatic.com\/m\/m5xVHnOJXutmBzyPEQBRqLA\/140.jpg"],
+                "viewItemURL":["http:\/\/www.ebay.com\/itm\/VINTAGE-1940s-FADA-CATALIN-BAKELITE-RADIO-MODEL-1000-CABINET-\/372238175241"],
+                "paymentMethod":["PayPal"],
+                "autoPay":["true"],
+                "postalCode":["08831"],
+                "location":["Monroe Township,NJ,USA"],
+                "country":["US"],
+                "shippingInfo":[{"shippingType":["CalculatedDomesticFlatInternational"],"shipToLocations":["Worldwide"],"expeditedShipping":["true"],"oneDayShippingAvailable":["false"],"handlingTime":["1"]}],
+                "sellingStatus":[{"currentPrice":[{"@currencyId":"USD","__value__":"95.0"}],"convertedCurrentPrice":[{"@currencyId":"USD","__value__":"95.0"}],"sellingState":["Active"],"timeLeft":["P6DT12H41M45S"]}],
+                "listingInfo":[{"bestOfferEnabled":["false"],"buyItNowAvailable":["false"],"startTime":["2018-03-03T11:46:09.000Z"],"endTime":["2018-03-10T11:46:09.000Z"],"listingType":["StoreInventory"],"gift":["false"],"watchCount":["3"]}],
+                "returnsAccepted":["false"],
+                "isMultiVariationListing":["false"],"topRatedListing":["false"]},
+            {"itemId":["132511862199"],
+                "title":["VINTAGE BEAUTIFUL 40s FADA BULLET ART DECO CATALIN BAKELITE ANTIQUE TUBE RADIO "],"globalId":["EBAY-US"],"primaryCategory":[{"categoryId":["50595"],"categoryName":["Vintage Radios"]}],"secondaryCategory":[{"categoryId":["38034"],"categoryName":["1930-49"]}],"galleryURL":["http:\/\/thumbs4.ebaystatic.com\/m\/mPZ1LL6TxMOm1JbAk3Gspeg\/140.jpg"],"viewItemURL":["http:\/\/www.ebay.com\/itm\/VINTAGE-BEAUTIFUL-40s-FADA-BULLET-ART-DECO-CATALIN-BAKELITE-ANTIQUE-TUBE-RADIO-\/132511862199"],"paymentMethod":["PayPal"],"autoPay":["false"],"postalCode":["08831"],"location":["Monroe Township,NJ,USA"],"country":["US"],"shippingInfo":[{"shippingType":["CalculatedDomesticFlatInternational"],"shipToLocations":["Worldwide"],"expeditedShipping":["true"],"oneDayShippingAvailable":["false"],"handlingTime":["1"]}],"sellingStatus":[{"currentPrice":[{"@currencyId":"USD","__value__":"580.0"}],"convertedCurrentPrice":[{"@currencyId":"USD","__value__":"580.0"}],"bidCount":["32"],"sellingState":["Active"],"timeLeft":["P0DT3H28M21S"]}],"listingInfo":[{"bestOfferEnabled":["false"],"buyItNowAvailable":["false"],"startTime":["2018-02-25T02:32:45.000Z"],"endTime":["2018-03-04T02:32:45.000Z"],"listingType":["Auction"],"gift":["false"],"watchCount":["83"]}],
+                "returnsAccepted":["true"],
+                "isMultiVariationListing":["false"],"topRatedListing":["true"]},
+
+            {"itemId":["122988243137"],
+                "title":["95 Vtg Bakelite Catalin Swirl Poker Chips 1-1\/2\" across x 1\/8\" thick Beautiful"],"globalId":["EBAY-US"],"primaryCategory":[{"categoryId":["150119"],"categoryName":["Poker Chips"]}],"galleryURL":["http:\/\/thumbs2.ebaystatic.com\/m\/mz4F-zq_wX3gSt4nGGmHNLw\/140.jpg"],"viewItemURL":["http:\/\/www.ebay.com\/itm\/95-Vtg-Bakelite-Catalin-Swirl-Poker-Chips-1-1-2-across-x-1-8-thick-Beautiful-\/122988243137"],"paymentMethod":["PayPal"],"autoPay":["false"],"postalCode":["53805"],"location":["Boscobel,WI,USA"],"country":["US"],"shippingInfo":[{"shippingType":["Calculated"],"shipToLocations":["Worldwide"],"expeditedShipping":["true"],"oneDayShippingAvailable":["false"],"handlingTime":["1"]}],"sellingStatus":[{"currentPrice":[{"@currencyId":"USD","__value__":"11.5"}],"convertedCurrentPrice":[{"@currencyId":"USD","__value__":"11.5"}],"bidCount":["4"],"sellingState":["Active"],"timeLeft":["P1DT2H45M49S"]}],"listingInfo":[{"bestOfferEnabled":["false"],"buyItNowAvailable":["false"],"startTime":["2018-02-26T01:50:13.000Z"],"endTime":["2018-03-05T01:50:13.000Z"],"listingType":["Auction"],"gift":["false"],"watchCount":["9"]}],
+                "returnsAccepted":["true"],
+                "galleryPlusPictureURL":["http:\/\/galleryplus.ebayimg.com\/ws\/web\/122988243137_1_0_1.jpg"],"condition":[{"conditionId":["3000"],"conditionDisplayName":["Used"]}],"isMultiVariationListing":["false"],"topRatedListing":["true"]}]}],
+        "paginationOutput":[{"pageNumber":["1"],"entriesPerPage":["100"],"totalPages":["24"],"totalEntries":["2374"]}],
+        "itemSearchURL":["http:\/\/www.ebay.com\/sch\/i.html?_nkw=catalin&_ddo=1&_ipg=100&_pgn=1"]
+   }
+ ]
+}'''
