cb32263863eb5931f07606dde807b969d97e20b2
Rick Graves <gravesricharde@yahoo.com>
2018-03-04 18:54:21 +0700

    now access the category list at categories/<market>/

--- ebayinfo/urls.py
+++ ebayinfo/urls.py
@@ -11,7 +11,7 @@ app_name = "ebayinfo"
 urlpatterns = [
     url(
         # regex   = r'^(?P<sMarket>(EBAY|ebay)-[A-Za-z]{2,5})/$',
-        regex   = r'^(?P<sMarket>ebay-[a-z]{2,5})/$',
+        regex   = r'^categories/(?P<sMarket>ebay-[a-z]{2,5})/$',
         view    = views.show_ebay_categories,
         name    = 'ebay_categories_index' ),
     url(
