86b2c0fc48bf13e5147ac8c17d7c0aa85b533bbf
Rick Graves <gravesricharde@yahoo.com>
2018-03-04 10:01:17 +0700

    dItemFoundFields were tuples not dicts, handle optional fields (condition)

--- searching/__init__.py
+++ searching/__init__.py
@@ -3,30 +3,41 @@ from Utils.Config   import getBoolOffYesNoTrueFalse as getBo
 
 sResultFileNamePattern = 'Search_%s_%s_ID_%s.json'
 
-dItemFoundFields = dict(
-    iItemNumb       = (int,  'itemId',),
-    cTitle          = (None, 'title',),
-    cLocation       = (None, 'location',),
-    cCountry        = (None, 'country',),
-    cMarket         = (None, 'globalId',),
-    cGalleryURL     = (None, 'galleryURL',),
-    cEbayItemURL    = (None, 'viewItemURL',),
-    tTimeBeg        = (getDT,'listingInfo','startTime'),
-    tTimeEnd        = (getDT,'listingInfo','endTime'),
-    bBestOfferable  = (getBo,'listingInfo','bestOfferEnabled'),
-    bBuyItNowable   = (getBo,'listingInfo','buyItNowAvailable'),
-    cListingType    = (None, 'listingInfo','listingType'),
-    lLocalCurrency  = (None, 'sellingStatus','currentPrice','@currencyId'),
-    lCurrentPrice   = (float,'sellingStatus','currentPrice','__value__'),
-    dCurrentPrice   = (float,'sellingStatus','convertedCurrentPrice','__value__'),
-    iCategoryID     = (int,  'primaryCategory','categoryId'),
-    cCategory       = (None, 'primaryCategory','categoryName'),
-    iConditionID    = (int,  'condition','conditionId'),
-    cCondition      = (None, 'condition','conditionDisplayName'),
-    cSellingState   = (None, 'sellingStatus','sellingState') )
+# condition is optional
+# https://developer.ebay.com/DevZone/guides/ebayfeatures/Development/Desc-ItemCondition.html
+
+d = dict
+
+dItemFoundFields = d(
+    iItemNumb       = d( f = int,  t = ( 'itemId',) ),
+    cTitle          = d( f = None, t = ( 'title',) ),
+    cLocation       = d( f = None, t = ( 'location',) ),
+    cCountry        = d( f = None, t = ( 'country',) ),
+    cMarket         = d( f = None, t = ( 'globalId',) ),
+    cGalleryURL     = d( f = None, t = ( 'galleryURL',) ),
+    cEbayItemURL    = d( f = None, t = ( 'viewItemURL',) ),
+    tTimeBeg        = d( f = getDT,t = ( 'listingInfo','startTime') ),
+    tTimeEnd        = d( f = getDT,t = ( 'listingInfo','endTime') ),
+    bBestOfferable  = d( f = getBo,t = ( 'listingInfo','bestOfferEnabled') ),
+    bBuyItNowable   = d( f = getBo,t = ( 'listingInfo','buyItNowAvailable') ),
+    cListingType    = d( f = None, t = ( 'listingInfo','listingType') ),
+    lLocalCurrency  = d( f = None,
+                         t = ( 'sellingStatus','currentPrice','@currencyId') ),
+    lCurrentPrice   = d( f = float,
+                         t = ( 'sellingStatus','currentPrice','__value__') ),
+    dCurrentPrice   = d( f = float,
+                         t = ( 'sellingStatus',
+                               'convertedCurrentPrice','__value__') ),
+    iCategoryID     = d( f = int,  t = ( 'primaryCategory','categoryId') ),
+    cCategory       = d( f = None, t = ( 'primaryCategory','categoryName') ),
+    iConditionID    = d( f = int,  t = ( 'condition','conditionId'),
+                         bOptional = True ),
+    cCondition      = d( f = None, t = ( 'condition','conditionDisplayName'),
+                         bOptional = True ),
+    cSellingState   = d( f = None, t = ( 'sellingStatus','sellingState') ) )
 
 
-dUserItemFoundFields = dict(
-    iItemNumb       = (int,  'itemId',),
-    iSearch         = (None, '',),
-    iUser           = (None, '',) )
+dUserItemFoundFields = d(
+    iItemNumb       = d( f = int,  t = ( 'itemId',) ),
+    iSearch         = d( f = None, t = ( '',) ),
+    iUser           = d( f = None, t = ( '',) ) )
