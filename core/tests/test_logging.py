976e8216f89aca19fb146d635b75e7e2c0d66aa8
Rick Graves <gravesricharde@yahoo.com>
2018-03-06 04:53:23 +0700

    implement logging tests

---
+++ core/tests/test_logging.py
@@ -0,0 +1,37 @@
+from logging       import getLogger
+from django.test   import TestCase
+
+
+logger = getLogger( 'test_logger' )
+
+
+def throw_error_log():
+    logger.error('an error')
+    
+def throw_message_log():
+    logger.info('a message')
+
+
+class LoggingTest(TestCase):
+
+   def test_logging_error(self):
+            
+        with self.assertLogs( logger ='test_logger', level='ERROR') as cm:
+
+            throw_error_log()
+
+            self.assertIn(
+                "ERROR:test_logger:an error",
+                cm.output
+            )
+
+   def test_logging_info(self):
+            
+        with self.assertLogs( logger ='test_logger', level='INFO') as cm:
+
+            throw_message_log()
+
+            self.assertIn(
+                "INFO:test_logger:a message",
+                cm.output
+            )
