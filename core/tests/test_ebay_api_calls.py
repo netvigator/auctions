0b223e083aeca5e5a63a9a520c49df2777eca7ea
Rick Graves <gravesricharde@yahoo.com>
2018-03-19 06:44:58 +0700

    implement conf file value fetch tests, make notes about which functions to test for real

--- core/tests/test_ebay_api_calls.py
+++ core/tests/test_ebay_api_calls.py
@@ -1,16 +1,46 @@
 from django.test import TestCase
 
-from ..ebay_api_calls import dEbayConf
+from ..ebay_api_calls import _getApiConfValues
 
 
-class EbayWrapperTests(TestCase):
-    '''ebay wrapper tests'''
+class GetConfFileValuesTests(TestCase):
+    '''ebay API conf file values tests'''
     
-    def test_get_ini_values(self):
+    def test_ini_values(self):
         
-        self.assertEquals( dEbayConf['call']['global_id'], 'EBAY-US' )
-        self.assertEquals( dEbayConf['call']['siteid'   ], '0'       )
+        dConfValues = _getApiConfValues( False )
         
-        self.assertEquals( dEbayConf['endpoints']['finding'],
+        self.assertEquals( dConfValues['call']['global_id'], 'EBAY-US' )
+        self.assertEquals( dConfValues['call']['siteid'   ], '0'       )
+        
+        self.assertEquals( dConfValues['endpoints']['finding'],
                     'http://svcs.ebay.com/services/search/FindingService/v1' )
+        
+        self.assertIsNotNone( dConfValues[ "keys"     ].get( "ebay_app_id" ) )
+
+        dConfValues = _getApiConfValues( True )
+        
+        self.assertEquals( dConfValues['call']['global_id'], 'EBAY-US' )
+        self.assertEquals( dConfValues['call']['siteid'   ], '0'       )
+        
+        self.assertEquals( dConfValues['endpoints']['finding'],
+                    'http://svcs.sandbox.ebay.com/services/search/FindingService/v1' )
+        
+        self.assertIsNotNone( dConfValues[ "keys"     ].get( "ebay_app_id" ) )
+
+'''
+do not test because downloaded file is HUGE:
+    getMarketCategoriesGotSiteID()
+    getMarketCategoriesGotGlobalID()
+
+do test:
+    getCategoryVersionGotSiteID()
+    getCategoryVersionGotGlobalID()
+    getItemsByKeyWords()
+    getItemsByCategory()
+    getItemsByBoth()
+    getSingleItem()
+    getItemStatus()
+'''
+
 
