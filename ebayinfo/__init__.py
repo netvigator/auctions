39b140593428137808a81b16992a757f853b4db6
Rick Graves <gravesricharde@yahoo.com>
2018-03-19 18:06:34 +0700

    fill in iCategoryVer for all sites, got from implementing test for getting current category list versions

--- ebayinfo/__init__.py
+++ ebayinfo/__init__.py
@@ -5,30 +5,50 @@ sMarketsTable = \
   cMarket   | cCountry | cLanguage | iEbaySiteID | bHasCategories | iCategoryVer | cCurrencyDef | cUseCategoryID | iUtcPlusOrMinus 
 ------------+----------+-----------+-------------+----------------+--------------+--------------+----------------+-----------------
  EBAY-US    | US       | en-US     |           0 | t              |          117 | USD          |                |              -8
- EBAY-ENCA  | CA       | en-CA     |           2 | t              |              | CAD          |                |              -8
+ EBAY-ENCA  | CA       | en-CA     |           2 | t              |          116 | CAD          |                |              -8
  EBAY-GB    | GB       | en-GB     |           3 | t              |          108 | GBP          |                |               0
- EBAY-AU    | AU       | en-AU     |          15 | t              |              | AUD          |                |              10
- EBAY-AT    | AT       | de-AT     |          16 | t              |              | EUR          |                |               1
- EBAY-FRBE  | BE       | fr-BE     |          23 | f              |              | EUR          |                |               1
- EBAY-FR    | FR       | fr-FR     |          71 | t              |              | EUR          |                |               1
- EBAY-DE    | DE       | de-DE     |          77 | t              |              | EUR          |                |               1
- EBAY-MOTOR | US       | en-US     |         100 | t              |              | USD          |                |              -8
- EBAY-IT    | IT       | it-IT     |         101 | t              |              | EUR          |                |               1
- EBAY-NLBE  | BE       | nl-BE     |         123 | f              |              | EUR          |                |               1
- EBAY-NL    | NL       | nl-NL     |         146 | t              |              | EUR          |                |               1
- EBAY-ES    | ES       | es-ES     |         186 | t              |              | EUR          |                |               1
- EBAY-CH    | CH       | de-CH     |         193 | t              |              | CHF          |                |               1
- EBAY-HK    | HK       | zh-Hant   |         201 | t              |              | HKD          |                |               8
- EBAY-IN    | IN       | en-IN     |         203 | t              |              | INR          |                |               5
- EBAY-IE    | IE       | en-IE     |         205 | t              |              | EUR          |                |               1
- EBAY-MY    | MY       | en-MY     |         207 | t              |              | MYR          |                |               8
- EBAY-FRCA  | CA       | fr-CA     |         210 | f              |              | CAD          |                |              -8
- EBAY-PH    | PH       | en-PH     |         211 | t              |              | PHP          |                |               8
- EBAY-PL    | PL       | pl-PL     |         212 | t              |              | PLN          |                |               1
+ EBAY-AU    | AU       | en-AU     |          15 | t              |          109 | AUD          |                |              10
+ EBAY-AT    | AT       | de-AT     |          16 | t              |           87 | EUR          |                |               1
+ EBAY-FRBE  | BE       | fr-BE     |          23 | f              |           82 | EUR          |                |               1
+ EBAY-FR    | FR       | fr-FR     |          71 | t              |          106 | EUR          |                |               1
+ EBAY-DE    | DE       | de-DE     |          77 | t              |          119 | EUR          |                |               1
+ EBAY-MOTOR | US       | en-US     |         100 | t              |           74 | USD          |                |              -8
+ EBAY-IT    | IT       | it-IT     |         101 | t              |           98 | EUR          |                |               1
+ EBAY-NLBE  | BE       | nl-BE     |         123 | f              |           83 | EUR          |                |               1
+ EBAY-NL    | NL       | nl-NL     |         146 | t              |           78 | EUR          |                |               1
+ EBAY-ES    | ES       | es-ES     |         186 | t              |           83 | EUR          |                |               1
+ EBAY-CH    | CH       | de-CH     |         193 | t              |           88 | CHF          |                |               1
+ EBAY-HK    | HK       | zh-Hant   |         201 | t              |           21 | HKD          |                |               8
+ EBAY-IN    | IN       | en-IN     |         203 | t              |           65 | INR          |                |               5
+ EBAY-IE    | IE       | en-IE     |         205 | t              |           67 | EUR          |                |               1
+ EBAY-MY    | MY       | en-MY     |         207 | t              |           32 | MYR          |                |               8
+ EBAY-FRCA  | CA       | fr-CA     |         210 | f              |           69 | CAD          |                |              -8
+ EBAY-PH    | PH       | en-PH     |         211 | t              |           32 | PHP          |                |               8
+ EBAY-PL    | PL       | pl-PL     |         212 | t              |           51 | PLN          |                |               1
  EBAY-SG    | SG       | en-SG     |         216 | t              |           30 | SGD          |                |               8
- EBAY-SE    | SE       | sv-SE     |         218 | f              |              | SEK          |                |               1
+ EBAY-SE    | SE       | sv-SE     |         218 | f              |           11 | SEK          |                |               1
  '''
  
+ 
+# to add
+# EBAY-AU
+# EBAY-CH
+# EBAY-DE
+# EBAY-ES
+# EBAY-FRCA
+# EBAY-FRBE
+# EBAY-IN
+# EBAY-IE
+# EBAY-IT
+# EBAY-HK
+# EBAY-MOTOR
+# EBAY-MY
+# EBAY-NLBE
+# EBAY-PH
+# EBAY-PL
+# EBAY-SE
+
+
  
  
 sCategoryDump = \
@@ -38,7 +58,7 @@ sCategoryDump = \
   771 |       12576 | Business & Industrial         |      1 |     12576 | f             |          117 |          0 |               | 1540 | 6325 |       2 |     1 |         1
 22882 |           1 | Collectables                  |      1 |         1 | f             |          108 |          3 |               | 7384 |11499 |       1 |     1 |     19190
 19191 |       20081 | Antiques                      |      1 |     20081 | f             |          108 |          3 |               |    2 |  685 |       1 |     1 |     19190
-    2 |       20081 | Antiques                      |      1 |     20081 | f             |          117 |          0 |               |   2 |  1077 |       2 |     1 |         1
+    2 |       20081 | Antiques                      |      1 |     20081 | f             |          117 |          0 |               |    2 | 1077 |       2 |     1 |         1
  4569 |           1 | Collectibles                  |      1 |         1 | f             |          117 |          0 |               | 9136 |18683 |       2 |     1 |         1
  3380 |       11450 | Clothing, Shoes & Accessories |      1 |     11450 | f             |          117 |          0 |               | 6758 | 8025 |       2 |     1 |         1
  9620 |         293 | Consumer Electronics          |      1 |       293 | f             |          117 |          0 |               |19238 |19817 |       2 |     1 |         1
