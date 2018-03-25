298ab669f46b6b0226c62ab016421929fd74655c
Rick Graves <gravesricharde@yahoo.com>
2018-03-25 10:28:27 +0700

    add helpful explanation/documentation on the variable positions in RESULTS_FILE_NAME_PATTERN

--- searching/__init__.py
+++ searching/__init__.py
@@ -2,8 +2,12 @@ from core.utils     import getDateTimeObjGotEbayStr
 from Utils.Config   import getBoolOffYesNoTrueFalse
 
 RESULTS_FILE_NAME_PATTERN = 'Search_%s_%s_ID_%s_p_%s_.json'
-
-# condition is optional
+# variables: sMarket, sUserName, iSearchID, iPageNumb
+# sName.split('_') gives this:
+# ['Search',sMarket,sUserName,'ID',iSearchID,'p',iPageNumb,'.json' ]
+#   0       1       2          3   4          5  6          7
+#
+# when selling on the ebay site, condition is optional
 # https://developer.ebay.com/DevZone/guides/ebayfeatures/Development/Desc-ItemCondition.html
 
 d = dict
