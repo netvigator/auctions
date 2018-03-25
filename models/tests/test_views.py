3b86195c9d6c1a7b698047db1b03412ea1dac858
Rick Graves <gravesricharde@yahoo.com>
2018-03-25 16:39:54 +0700

    add class ModelViewsHitButtons and tests

--- models/tests/test_views.py
+++ models/tests/test_views.py
@@ -8,6 +8,7 @@ from categories.models          import Category
 
 from ..models                   import Model
 
+from ..views                    import ModelCreateView
 # Create your tests here.
 
 
@@ -19,10 +20,11 @@ class ModelViewsTests(BaseUserTestCase):
         #
         super( ModelViewsTests, self ).setUp()
         #
-        self.oCategory = Category(
-            cTitle          = "My awesome category",
-            iUser           = self.user1 )
-        self.oCategory.save()
+        sCategory = "Widgets"
+        oCategory = Category( cTitle= sCategory, iUser = self.user1 )
+        oCategory.save()
+        #
+        self.oCategory = oCategory
         #
 
     def test_no_models_yet(self):
@@ -53,14 +55,19 @@ class ModelViewsTests(BaseUserTestCase):
         self.client.login(username='username1', password='mypassword')
         #
         
-        oModel      = Model(
-            cTitle = "Crest", iUser = self.user1, iCategory = self.oCategory )
+        oModel  = Model(
+                cTitle      = "Crest",
+                iUser       = self.user1,
+                iCategory   = self.oCategory )
         oModel.save()
         
-        oModel      = Model(
-            cTitle = "Colgate", iUser = self.user1, iCategory = self.oCategory )
+        oModel  = Model(
+                cTitle = "Colgate",
+                iUser       = self.user1,
+                iCategory   = self.oCategory )
         oModel.save()
-        iLeverID    = oModel.id
+        #
+        iLeverID= oModel.id
 
         response = self.client.get(reverse('models:index'))
         #response = self.client.get('/models/')
@@ -104,4 +111,145 @@ class ModelViewsTests(BaseUserTestCase):
         response = self.client.get(reverse('models:index'))
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/accounts/login/?next=/models/')
+
+
+
+
+class ModelViewsHitButtons(BaseUserTestCase):
+    """
+    Test Save and Cancel
+    """
+    def setUp(self):
+        #
+        super( ModelViewsHitButtons, self ).setUp()
+        #
+        sCategory = "Widgets"
+        oCategory = Category( cTitle= sCategory, iUser = self.user1 )
+        oCategory.save()
+        #
+        self.oCategory = oCategory
+        #
+        self.client.login(username ='username1', password='mypassword')
+
+
+
+    def test_get(self):
+        """
+        Test GET requests
+        """
+        request = self.factory.get(reverse('models:add'))
+        request.user = self.user1
+        #
+        response = ModelCreateView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        #print( 'response.context_data has form and view, view keys:' )
+        #for k in response.context_data['view'].__dict__:
+            #print( k )
+        #self.assertEqual(response.context_data['iUser'], self.user1)
+        self.assertEqual(response.context_data['view'].__dict__['request'], request)
+
+
+    # @patch('models.models.Model.save', MagicMock(name="save"))
+    def test_add_post(self):
+        """
+        Test post requests
+        """
+        data = dict(
+            cTitle      = "Permium Number One",
+            iCategory   = self.oCategory,
+            iUser       = self.user1 )
+        # Create the request
+        response = self.client.post( reverse('models:add'), data )
+        # import pdb; pdb.set_trace()
+        self.assertEqual( response.status_code, 200 )
+        #oModel = Model.objects.get( cTitle = "Great Widget" )
+        #self.assertEqual( oModel, self.models )
+
+        #request = self.factory.get(reverse('models:add'))
+        #request.user = self.user1
+
+        #response = self.client.post( request, data )
+
+        #print( 'response.status_code:', response.status_code )
+
+
+    def test_add_hit_cancel(self):
+        #
+        """
+        Hit cancel when adding
+        """
+        data = dict(
+            cTitle      = "Permium Number One",
+            iUser       = self.user1,
+            iCategory   = self.oCategory,
+            Cancel      = True )
+        # Create the request
+        response = self.client.post( reverse('models:add'), data )
+        #print( 'response.status_code:', response.status_code )
+        #print( 'response.__dict__:' )
+        #pprint( response.__dict__ )
+        self.assertEqual( response.status_code, 200 )
+        # self.assertRedirects( response, reverse( 'models:index' ) )
+
+    def test_search_create_view(self):
+        #
+        request = self.factory.get(reverse('models:add'))
+        request.user = self.user1
+        #request.POST._mutable = True
+        #request.POST['cancel'] = True
+        #
+        response = ModelCreateView.as_view()(request)
+        #
+        # print( 'type( request.POST ):', type( request.POST ) )
+        #
+        self.assertEqual(response.status_code, 200 )
+        #print( 'response.template_name:' )
+        #print( response.template_name ) ['models/add.html']
+        #
+    #
+
+    # @patch('models.models.Model.save', MagicMock(name="save"))
+    def test_edit_post(self):
+        """
+        Test post requests
+        """
+        sModel = "Permium Number One"
+        oModel = Model( cTitle    = sModel,
+                        iUser     = self.user1,
+                        iCategory = self.oCategory )
+        oModel.save()
+        #
+        data = dict(
+            cTitle      = "Permium Number Two",
+            iUser       = self.user1 )
+        # Create the request
+        response = self.client.post(
+                reverse('models:edit', kwargs={'pk': oModel.id} ), data )
+        # import pdb; pdb.set_trace()
+        self.assertEqual( response.status_code, 200 )
+
+
+    def test_edit_hit_cancel(self):
+        #
+        """
+        Hit cancel when adding
+        """
+        sModel = "Permium Number One"
+        oModel = Model( cTitle    = sModel,
+                        iUser     = self.user1,
+                        iCategory = self.oCategory )
+        oModel.save()
+        #
+        data = dict(
+            cTitle      = "Permium Number Two",
+            iUser       = self.user1,
+            Cancel      = True )
+        # Create the request
+        response = self.client.post(
+                reverse('models:edit', kwargs={'pk': oModel.id} ), data )
+        #print( 'response.status_code:', response.status_code )
+        #print( 'response.__dict__:' )
+        #pprint( response.__dict__ )
+        self.assertEqual( response.status_code, 200 )
+        # self.assertRedirects( response, reverse( 'models:index' ) )
 
