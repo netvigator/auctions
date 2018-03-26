c0e42ade24ced158e01bbd840b466654fbe942b9
Rick Graves <gravesricharde@yahoo.com>
2018-03-26 18:01:05 +0700

    implement getAllMissingCategoryLists()

--- ebayinfo/tasks.py
+++ ebayinfo/tasks.py
@@ -1,5 +1,32 @@
 
-from .utils         import getWhetherAnyEbayCategoryListsAreUpdated
 
+from .utils         import getWhetherAnyEbayCategoryListsAreUpdated
+from .utils         import getCategoryListThenStore
+
+from .models        import Market, EbayCategory
 
 # ### l = getWhetherAnyEbayCategoryListsAreUpdated() ###
+
+
+
+def getAllMissingCategoryLists():
+    #
+    oMarkets = Market.objects.all()
+    #
+    for oMarket in oMarkets:
+        #
+        bGotThis = EbayCategory.objects.filter( iMarket = oMarket ).exists()
+        #
+        if bGotThis:
+            #
+            print( 'got %s categories already moving on ...' )
+            #
+        else:
+            #
+            getCategoryListThenStore(
+                    uMarket         = oMarket,
+                    uWantVersion    = oMarket.iCategoryVer,
+                    bShowProgress   = True )
+            #
+            #
+
