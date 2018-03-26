98f91a2b709ec5d88cb3cd4b857bb8a9c6734675
Rick Graves <gravesricharde@yahoo.com>
2018-03-26 13:23:54 +0700

    start setting up doAllSearching() will be a daily job but time zone specific

--- searching/tasks.py
+++ searching/tasks.py
@@ -2,7 +2,36 @@
 from .utils         import trySearchCatchExceptions
 from .utils_stars   import findSearchHits
 
+from .models        import Search
 
+from String.Output  import ReadableNo
 
 # trySearchCatchExceptions( iSearchID = None, sFileName = None )
 # findSearchHits( oUser )
+
+
+def doAllSearching():
+    #
+    # really want to select for active users only (not inactive)
+    #
+    oSearches = Search.objects.all()
+    #
+    for oSearch in oSearches:
+        #
+        print( 'Searching for %s for %s ...' %
+                ( oSearch, oSearch.iUser.name ) )
+        #
+        t = trySearchCatchExceptions( iSearchID = oSearch.id ) 
+        #
+        iItems, iStoreItems, iStoreUsers = t
+        #
+        sItems      = ReadableNo( iItems      )
+        sStoreItems = ReadableNo( iStoreItems )
+        sStoreUsers = ReadableNo( iStoreUsers )
+        #
+        print(
+            'got %s items, of which %s were new, and %s were new for %s.' %
+            ( sItems, sStoreItems, sStoreUsers, oSearch.iUser.name ) )
+        print( '' )
+
+
