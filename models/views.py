ed73a939b5fe8bbe47b0a314296a48a8d99d49f4
Rick Graves <gravesricharde@yahoo.com>
2018-03-28 09:44:22 +0700

    move choice Delete or Cancel to parent view DeleteViewGotModel in core.views

--- models/views.py
+++ models/views.py
@@ -34,16 +34,8 @@ class ModelCreateView( CreateViewGotCrispy ):
     model           = Model
     form_class      = ModelForm
     template_name   = 'models/add.html'
-    success_url     = reverse_lazy('models:index')
-
     success_message = 'New Model record successfully saved!!!!'
 
-    def post(self, request, *args, **kwargs):
-        if "cancel" in request.POST:
-            url = reverse_lazy('models:index' )
-            return HttpResponseRedirect(url)
-        else:
-            return super(ModelCreateView, self).post(request, *args, **kwargs)
 
 
 class ModelUpdateView( WereAnyReleventColsChangedMixin, UpdateViewGotCrispy):
@@ -51,7 +43,6 @@ class ModelUpdateView( WereAnyReleventColsChangedMixin, UpdateViewGotCrispy):
     form_class      = ModelForm
     model           = Model
     template_name   = 'models/edit.html'
-
     success_message = 'Model record update successfully saved!!!!'
 
     tRegExRelevantCols = (
@@ -62,25 +53,14 @@ class ModelUpdateView( WereAnyReleventColsChangedMixin, UpdateViewGotCrispy):
         'cLookFor',
         'bSubModelsOK' )
 
-    def post(self, request, *args, **kwargs):
-        if "cancel" in request.POST:
-            self.object = self.get_object()
-            url = reverse_lazy('models:detail', kwargs={'pk': self.object.id})
-            return HttpResponseRedirect(url)
-        else:
-            return super(ModelUpdateView, self).post(request, *args, **kwargs)
 
 
 
 class ModelDeleteView( DeleteViewGotModel ):
-    model   = Model
-    template_name = 'confirm_delete.html'
-    success_url = reverse_lazy('models:index')
 
-    def post(self, request, *args, **kwargs):
-        if "cancel" in request.POST:
-            self.object = self.get_object()
-            url = reverse_lazy('models:detail', kwargs={'pk': self.object.id})
-            return HttpResponseRedirect(url)
-        else:
-            return super(ModelDeleteView, self).post(request, *args, **kwargs)
+    model           = Model
+    template_name   = 'confirm_delete.html'
+    success_url     = reverse_lazy('models:index')
+    success_message = 'Model record successfully deleted!!!!'
+
+
