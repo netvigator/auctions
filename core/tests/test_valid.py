9c61f144941253716de862afc1eb50408820a38c
Rick Graves <gravesricharde@yahoo.com>
2018-03-30 20:36:05 +0700

    move test of gotTextOutsideParens to test_valid.py

---
+++ core/tests/test_valid.py
@@ -0,0 +1,27 @@
+from django.test                import TestCase
+
+from ..validators               import gotTextOutsideParens
+
+
+class GotTextOutsideParensTests(TestCase):
+    #
+    ''' test the gotTextOutsideParens() validator '''
+    #
+    def setUp(self):
+        self.s1 = 'abcde (efghijk)'
+        self.s2 = 'abcde'
+        self.s3 = ' (efghijk) '
+
+    def test_OK_titles( self ):
+        #
+        try:
+            gotTextOutsideParens( self.s1 )
+            gotTextOutsideParens( self.s2 )
+        except ValidationError:
+            self.fail("gotTextOutsideParens() raised ValidationError unexpectedly!")
+    #
+    def test_bad_title( self ):
+        #
+        with self.assertRaises( ValidationError ):
+            gotTextOutsideParens( self.s3 )
+
