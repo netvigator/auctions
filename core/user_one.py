9aee5898f0d507ef5ddb6c75052a5ea53872ccf8
Rick Graves <gravesricharde@yahoo.com>
2018-04-01 17:33:38 +0700

    get_user_model() get user model not user, word better

--- core/user_one.py
+++ core/user_one.py
@@ -1,9 +1,9 @@
 from django.contrib.auth        import get_user_model
 
-oUser = get_user_model()
+oUserModel = get_user_model()
 
-oUserOne = oUser.objects.filter( id = 1 ).first()
-# oUserOne = None # oUser.objects.filter( id = 1 ).first()
+oUserOne = oUserModel.objects.filter( id = 1 ).first()
+# oUserOne = None # oUserModel.objects.filter( id = 1 ).first()
 
 # can get 'field does not exist; error when trying to add a field via migrate
 # workaround: use the commented lines above and below
@@ -11,21 +11,8 @@ oUserOne = oUser.objects.filter( id = 1 ).first()
 #if False: # not oUserOne:
 if not oUserOne:
     #
-    '''
-    oUser = User()
-    #
-    oUser.first_name    = 'Rick'
-    oUser.last_name     = 'Graves'
-    oUser.email         = 'gravesricharde@yahoo.com'
-    oUser.username      = 'netvigator'
-    oUser.password      = 'tba'
-    #
-    oUser.save()
-    #
-    oUserOne = oUser
-    '''
-    oUserOne = get_user_model().objects.create_user(
-        'netvigator',
+    oUserOne = oUserModel(
+        username        = 'netvigator',
         email           = 'gravesricharde@yahoo.com',
         password        = None,
         first_name      = 'Rick',
@@ -33,4 +20,5 @@ if not oUserOne:
     #
     if oUserOne.id != 1:
         oUserOne.id = 1
-        oUserOne.save()
+    #
+    oUserOne.save()
