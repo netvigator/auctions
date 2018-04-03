565a40d1200145b57321d7ce1973cc281503308d
Rick Graves <gravesricharde@yahoo.com>
2018-04-03 16:18:53 +0700

    now 2 subclasses, instead of 1 BrandForm, now have CreateBrandForm & UpdateBrandForm

--- brands/forms.py
+++ brands/forms.py
@@ -1,5 +1,7 @@
-from core.forms import ModelFormValidatesTitle
-from .models    import Brand
+from crispy_forms.layout    import Submit
+
+from core.forms             import ModelFormValidatesTitle
+from .models                import Brand
 
 
 tModelFields = (
@@ -13,8 +15,34 @@ tModelFields = (
     'cExcludeIf' )
 
 
-class BrandForm( ModelFormValidatesTitle ):
+class CreateBrandForm( ModelFormValidatesTitle ):
     #
+
+    def __init__( self, *args, **kwargs ):
+        #
+        super( CreateBrandForm, self ).__init__( *args, **kwargs )
+        #
+        self.helper.add_input(Submit('submit', 'Create', css_class='btn-primary'))
+        self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
+        #
+
     class Meta:
-        model = Brand
+        model  = Brand
+        fields = tModelFields
+
+
+class UpdateBrandForm( ModelFormValidatesTitle ):
+    #
+    which = 'Update'
+
+    def __init__( self, *args, **kwargs ):
+        #
+        super( UpdateBrandForm, self ).__init__( *args, **kwargs )
+        #
+        self.helper.add_input(Submit('submit', 'Update', css_class='btn-primary'))
+        self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
+        #
+
+    class Meta:
+        model  = Brand
         fields = tModelFields
