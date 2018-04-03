29754f7e80548b4018f6fb1d45ff5bc93b69669a
Rick Graves <gravesricharde@yahoo.com>
2018-04-03 16:23:36 +0700

    now 2 subclasses, instead of 1 CategoryForm, now have CreateCategoryForm & UpdateCategoryForm

--- categories/forms.py
+++ categories/forms.py
@@ -1,5 +1,7 @@
-from core.forms import ModelFormValidatesTitle
-from .models    import Category
+from crispy_forms.layout    import Submit
+
+from core.forms             import ModelFormValidatesTitle
+from .models                import Category
 
 
 tModelFields = (
@@ -17,8 +19,35 @@ tModelFields = (
     )
 
 
-class CategoryForm( ModelFormValidatesTitle ):
+class CreateCategoryForm( ModelFormValidatesTitle ):
     #
+    
+    def __init__( self, *args, **kwargs ):
+        #
+        super( CreateCategoryForm, self ).__init__( *args, **kwargs )
+        #
+        self.helper.add_input(Submit('submit', 'Create', css_class='btn-primary'))
+        self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
+        #
+
+    class Meta:
+        model   = Category
+        fields  = tModelFields
+
+
+
+class UpdateCategoryForm( ModelFormValidatesTitle ):
+    #
+    which = 'Update'
+
+    def __init__( self, *args, **kwargs ):
+        #
+        super( UpdateCategoryForm, self ).__init__( *args, **kwargs )
+        #
+        self.helper.add_input(Submit('submit', 'Update', css_class='btn-primary'))
+        self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
+        #
+
     class Meta:
         model   = Category
         fields  = tModelFields
