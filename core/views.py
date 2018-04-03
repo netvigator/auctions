afc5c06db10b6da7f121c221bca2757f36b2debd
Rick Graves <gravesricharde@yahoo.com>
2018-04-03 17:34:43 +0700

    move repeated code to mixins, inherit from FormValidMixin GetModelInContextMixin & GetFormMixin

--- core/views.py
+++ core/views.py
@@ -10,7 +10,9 @@ from django.views.generic.detail    import DetailView
 from django.views.generic.edit      import CreateView, UpdateView, DeleteView
 
 
-from .mixins                        import DoesLoggedInUserOwnThisRowMixin
+from .mixins                        import ( DoesLoggedInUserOwnThisRowMixin,
+                                             FormValidMixin, GetFormMixin,
+                                             GetModelInContextMixin )
 
 # Create your views here but keep them thin.
 
@@ -55,19 +57,14 @@ class ListViewGotModel( LoginRequiredMixin, ListView ):
         return self.model.objects.filter( iUser = self.request.user )
 
 
-class CreateViewCanCancel( LoginRequiredMixin, SuccessMessageMixin, CreateView ):
+class CreateViewCanCancel( LoginRequiredMixin, 
+            SuccessMessageMixin, FormValidMixin, GetFormMixin,
+            CreateView ):
     '''
     Enhanced CreateView which includes crispy form Create and Cancel buttons.
     '''
 
     success_message = 'New record successfully saved!!!!'
-
-    def form_valid(self, form):
-        # model form does not accept user in kwargs
-        obj = form.save( commit = False )
-        obj.user = self.user = self.request.user
-        form.instance.iUser  = self.request.user
-        return super(CreateViewCanCancel, self).form_valid(form)
 
 
     def get_object(self):
@@ -85,20 +82,9 @@ class CreateViewCanCancel( LoginRequiredMixin, SuccessMessageMixin, CreateView )
             return ( super( CreateViewCanCancel, self )
                      .post( request, *args, **kwargs ) )
 
-    def get_form( self, form_class = None ):
-        '''
-        can get form in view:
-        form = self.get_form( self.form_class )
-        see below for mixin that worked
-        (but not used because there was a much easier way)
-        '''
-        form = super( CreateViewCanCancel, self ).get_form(form_class)
-        form.request = self.request
-        self.form = form
-        return form
 
 
-class DeleteViewGotModel( LoginRequiredMixin,
+class DeleteViewGotModel( LoginRequiredMixin, GetModelInContextMixin,
                 DoesLoggedInUserOwnThisRowMixin, SuccessMessageMixin, DeleteView ):
     '''
     Enhanced DeleteView which also includes the model in the context data,
@@ -107,14 +93,6 @@ class DeleteViewGotModel( LoginRequiredMixin,
  
     success_message = 'Record successfully deleted!!!!'
 
-    def get_context_data(self, **kwargs):
-        '''
-        Adds the model to the context data.
-        '''
-        context = super(DeleteView, self).get_context_data(**kwargs)
-        context['model']        = self.model
-        # context['model_fields'] = self.model._meta.get_fields()
-        return context
 
     def post(self, request, *args, **kwargs):
         if "cancel" in request.POST:
@@ -125,20 +103,15 @@ class DeleteViewGotModel( LoginRequiredMixin,
             return super(DeleteViewGotModel, self).post(request, *args, **kwargs)
 
 
-class UpdateViewCanCancel( LoginRequiredMixin, SuccessMessageMixin,
-                DoesLoggedInUserOwnThisRowMixin, UpdateView ):
+class UpdateViewCanCancel(
+            LoginRequiredMixin, SuccessMessageMixin, FormValidMixin,
+            DoesLoggedInUserOwnThisRowMixin, GetFormMixin, GetModelInContextMixin,
+
+            UpdateView ):
     '''
     Enhanced UpdateView which includes crispy form Update and Cancel buttons.
     '''
     success_message = 'Record successfully saved!!!!'
-
-    def form_valid(self, form):
-        # model form does not accept user in kwargs
-        obj = form.save( commit = False )
-        obj.user = self.user = self.request.user
-        form.instance.iUser  = self.request.user
-        # obj.save()
-        return super(UpdateViewCanCancel, self).form_valid(form)
 
     def post(self, request, *args, **kwargs):
         if "cancel" in request.POST:
@@ -148,75 +121,15 @@ class UpdateViewCanCancel( LoginRequiredMixin, SuccessMessageMixin,
         else:
             return super(UpdateViewCanCancel, self).post(request, *args, **kwargs)
 
-    def get_form( self, form_class = None ):
-        '''
-        can get form in view:
-        form = self.get_form( self.form_class )
-        see below for mixin that worked
-        (but not used because there was a much easier way)
-        '''
-        form = super( UpdateViewCanCancel, self ).get_form(form_class)
-        form.request = self.request
-        self.form = form
-        return form
-
-    def get_context_data(self, **kwargs):
-        '''
-        Adds the model to the context data.
-        '''
-        context          = super( UpdateView, self).get_context_data(**kwargs)
-        context['model'] = self.model
-        if hasattr( self, 'parent' ):
-            context['parent'] = self.parent
-        # context['model_fields'] = self.model._meta.get_fields()
-        return context
 
 
 class DetailViewGotModel( LoginRequiredMixin,
-                DoesLoggedInUserOwnThisRowMixin, DetailView ):
+                DoesLoggedInUserOwnThisRowMixin, GetModelInContextMixin,
+                DetailView ):
     '''
     Enhanced DetailView which also includes the model in the context data,
     so that the template has access to its model class.
     '''
  
-    def get_context_data(self, **kwargs):
-        '''
-        Adds the model to the context data.
-        '''
-        context          = super(DetailView, self).get_context_data(**kwargs)
-        context['model'] = self.model
-        if hasattr( self, 'parent' ):
-            context['parent'] = self.parent
-        # context['model_fields'] = self.model._meta.get_fields()
-        return context
+    pass
 
-
-"""
-this worked but this is not used cuz there was a much easier way
-
-class SayCurrentPriceBetterMixin( object ):
-    '''streamline saying the current price if in USD'''
-
-    #
-    def get_context_data( self, **kwargs ):
-        '''
-        Adds the the current price line to the context data.
-        '''
-        context = super(
-            SayCurrentPriceBetterMixin, self ).get_context_data(**kwargs)
-        #
-        form = self.get_form( self.form_class )
-        #
-        sayCurrentPrice = form.instance.iItemNumb._meta.get_field(
-                                'lCurrentPrice').verbose_name
-        #
-        sayCurrentPrice = sayCurrentPrice.replace(
-                'local currency',
-                form.instance.iItemNumb.lLocalCurrency )
-        #
-        context['sayCurrentPrice'] = sayCurrentPrice
-        #
-        self.form = form
-        #
-        return context
-"""
