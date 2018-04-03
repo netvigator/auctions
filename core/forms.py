89e9ef85ff8ef86f194078926e884f721ca3f971
Rick Graves <gravesricharde@yahoo.com>
2018-04-03 16:45:38 +0700

    now have class BaseModelFormGotCrispy, subclassed here as ModelFormValidatesTitle, also find user instance more carefully

--- core/forms.py
+++ core/forms.py
@@ -3,14 +3,43 @@ from django.forms           import ModelForm
 
 from core.validators        import gotTextOutsideParens
 
+from crispy_forms.helper    import FormHelper
+from crispy_forms.layout    import Submit
 
-class ModelFormValidatesTitle( ModelForm ):
+
+
+class BaseModelFormGotCrispy( ModelForm ):
     #
-    which = 'Create' # can be over written in view get_form
+    '''base crispy form, can subclass to create or update'''
     #
     def __init__( self, *args, **kwargs ):
         #
+        self.request = kwargs.get( 'request' )
+        # Voila, now you can access request via self.request!
+        #
+        if not hasattr( self, 'user' ): self.user = None
+        #
         if 'user' in kwargs: self.user = kwargs.pop('user')
+        #
+        super( BaseModelFormGotCrispy, self ).__init__( *args, **kwargs )
+        #
+        self.helper = FormHelper()
+        #
+        #
+        # subclass should set:
+        # self.helper.add_input(Submit('submit', 'Create', css_class='btn-primary'))
+        # or
+        # self.helper.add_input(Submit('submit', 'Update', css_class='btn-primary'))
+        # and
+        # self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
+    
+
+
+class ModelFormValidatesTitle( BaseModelFormGotCrispy ):
+    #
+    which = 'Create' # can be over written
+    #
+    def __init__( self, *args, **kwargs ):
         #
         super( ModelFormValidatesTitle, self ).__init__( *args, **kwargs )
         #
@@ -19,15 +48,20 @@ class ModelFormValidatesTitle( ModelForm ):
 
     def gotTitleAready( self, cTitle ):
         #
-        oUser = None
+        # oUser = None
         #
-        if hasattr( self, 'user' ):
+        #
+        if hasattr( self, 'user' ) and self.user is not None:
             oUser = self.user
-        elif hasattr( self, 'request' ):
+        elif ( hasattr( self, 'request' ) and
+               hasattr( self.request, 'user' ) and
+               self.request.user is not None ):
             oUser = self.request.user
-        elif hasattr( self.instance, 'user' ):
+        elif ( hasattr( self.instance, 'user' ) and
+               self.instance.user is not None ):
             oUser = self.instance.user
-        elif hasattr( self.instance, 'iUser' ): # testing
+        elif ( hasattr( self.instance, 'iUser' ) and
+               self.instance.iUser is not None ): # testing
             oUser = self.instance.iUser
         #
         if ( self.Meta.model.objects.filter(
