3d649e6753f4422d5d7043db3541b09ac30240f4
Rick Graves <gravesricharde@yahoo.com>
2018-04-14 21:14:23 -0700

    no longer using regex_field.fields, lack of installation causes django to crash, had to comment out referring lines

--- categories/models.py
+++ categories/models.py
@@ -3,8 +3,6 @@ from django.db                  import models
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-from regex_field.fields         import RegexField
-
 from core.models                import ( IntegerRangeField, sTitleHelpText,
                                          sKeyWordsHelpText, sLookForHelpText,
                                          sExcludeIfHelpText )
