b0575d4165b6d6d1510c8809aff62df7f615efc3
Rick Graves <gravesricharde@yahoo.com>
2018-04-18 20:42:00 -0700

    for the brand category clickable list on the brand add/edit forms, cannot user crispy buttons, must set self.helper.form_tag = False

--- brands/forms.py
+++ brands/forms.py
@@ -1,7 +1,10 @@
-from crispy_forms.layout    import Submit
-
 from core.forms             import ModelFormValidatesTitle
+from django.forms           import ModelForm, formset_factory, BooleanField
+
+from crispy_forms.layout    import Field, Layout, Submit
 from .models                import Brand
+
+from categories.models      import Category
 
 
 tModelFields = (
@@ -22,9 +25,19 @@ class CreateBrandForm( ModelFormValidatesTitle ):
         #
         super( CreateBrandForm, self ).__init__( *args, **kwargs )
         #
-        self.helper.add_input(Submit('submit', 'Create', css_class='btn-primary'))
-        self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
+        self.helper.form_tag = False
+        #self.helper.add_input(Submit('submit', 'Create', css_class='btn-primary'))
+        #self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
         #
+        self.helper.layout = Layout(
+                'cTitle',
+                'bWanted',
+                'bAllOfInterest',
+                'cLookFor',
+                'iStars',
+                'cComment',
+                'cNationality',
+                Field('cExcludeIf', rows='2') )
 
     class Meta:
         model  = Brand
@@ -39,10 +52,13 @@ class UpdateBrandForm( ModelFormValidatesTitle ):
         #
         super( UpdateBrandForm, self ).__init__( *args, **kwargs )
         #
-        self.helper.add_input(Submit('submit', 'Update', css_class='btn-primary'))
-        self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
+        self.helper.form_tag = False
+        #self.helper.add_input(Submit('submit', 'Update', css_class='btn-primary'))
+        #self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
         #
 
     class Meta:
         model  = Brand
         fields = tModelFields
+
+
