030540cfced0809029e06b81add5e29ee2269360
Rick Graves <gravesricharde@yahoo.com>
2018-04-22 13:40:42 -0700

    troubleshoot tweaks

--- brands/tests/test_models.py
+++ brands/tests/test_models.py
@@ -8,22 +8,32 @@ from ..models           import Brand
 
 class ModelModelTest(BaseUserTestCase):
 
-    def test_string_representation(self):
-        sBrand = "My brand name"
-        oBrand = Brand(cTitle= sBrand )
-        self.assertEqual(str(oBrand), oBrand.cTitle)
-
-    def test_get_absolute_url(self):
-        """browser refresh trick: detail URL includes a UTC query string"""
+    def setUp( self ):
+        #
+        super( ModelModelTest, self ).setUp()
+        #
+        self.client.login(username ='username1', password='mypassword')
+        #
         oBrand = Brand(
             cTitle          = "My premium brand",
             iUser           = self.user1,
             id              = 1 )
+        #
         oBrand.save()
         #
-        tParts = getUrlQueryStringOff( oBrand.get_absolute_url() )
+        self.oBrand = oBrand
+
+
+    def test_string_representation(self):
         #
-        self.assertEqual( tParts[0], '/brands/%s/' % oBrand.id )
+        self.assertEqual(str(self.oBrand), self.oBrand.cTitle)
+
+    def test_get_absolute_url(self):
+        """browser refresh trick: detail URL includes a UTC query string"""
+        #
+        tParts = getUrlQueryStringOff( self.oBrand.get_absolute_url() )
+        #
+        self.assertEqual( tParts[0], '/brands/%s/' % self.oBrand.id )
         #
         self.assertTrue( queryGotUpdated( tParts[1] ) )
         #
