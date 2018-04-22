0c9ac8f1232381582d06695d4f923ae5ba86f934
Rick Graves <gravesricharde@yahoo.com>
2018-04-22 13:41:51 -0700

    lots of tests for bisect style pagination choices

--- core/tests/test_mixins.py
+++ core/tests/test_mixins.py
@@ -1,8 +1,368 @@
-from django.core.urlresolvers import reverse
+from copy                       import deepcopy
 
-from core.utils_test        import setUpBrandsCategoriesModels
+from django.core.urlresolvers   import reverse
+from django.http.request        import HttpRequest
+from django.test                import RequestFactory
 
-from searching.utils_stars  import getFoundItemTester
+from core.utils_test            import ( setUpBrandsCategoriesModels,
+                                         BaseUserTestCase )
+
+from ..mixins                   import GetPaginationExtraInfoInContext
+from ..views                    import ListViewGotModel
+
+from searching.utils_stars      import getFoundItemTester
+from searching.views            import ItemsFoundIndexView
+from searching.tests.test_stars import SetUpForKeyWordFindSearchHitsTests
+
+from Object.Get                 import QuickObject
+
+
+class TestPagination( SetUpForKeyWordFindSearchHitsTests ):
+    '''class to test pagination thoroughly'''
+    
+    def setUp( self ):
+        #
+        super( TestPagination, self ).setUp()
+        #
+        self.factory = RequestFactory()
+        #
+        self.request = self.factory.get(reverse(
+                'searching:items_found_index' ) )
+        #
+        self.request.user = self.user1
+        #
+        self.view = ItemsFoundIndexView()
+        #
+        self.view.request = self.request
+        
+
+
+    def test_pagination_tests( self ):
+        '''test pagination thoroughly'''
+        #
+        self.view.kwargs = { 'select' : 'P' } # testing glitch workaround
+
+        self.view.object_list = self.view.get_queryset()
+        
+        context = self.view.get_context_data( object_list = range(1,10000) )
+
+        oThisPage = deepcopy( context.get('page_obj') )
+        #
+        self.assertEqual( oThisPage.number, 1 )
+        self.assertEqual( context.get( 'show_range' ), range(1,3) )
+        self.assertEqual( context.get( 'iBeg'       ),   0 )
+        self.assertEqual( context.get( 'iEnd'       ),   2 )
+        self.assertEqual( context.get( 'iMidLeft'   ),   0 )
+        self.assertEqual( context.get( 'iMidRight'  ),  50 )
+        self.assertEqual( context.get( 'iMaxPage'   ), 100 )
+        #
+        oThisPage.number = 50
+        #
+        self.request.GET.previous = 1
+        #
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = '1' )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(49,52) )
+        self.assertEqual( context.get( 'iBeg'       ),  49 )
+        self.assertEqual( context.get( 'iEnd'       ),  51 )
+        self.assertEqual( context.get( 'iMidLeft'   ),  25 )
+        self.assertEqual( context.get( 'iMidRight'  ),  75 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 25
+        #
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(24,27) )
+        self.assertEqual( context.get( 'iBeg'       ),  24 )
+        self.assertEqual( context.get( 'iEnd'       ),  26 )
+        self.assertEqual( context.get( 'iMidLeft'   ),  13 )
+        self.assertEqual( context.get( 'iMidRight'  ),  37 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 13
+        #
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(12,15) )
+        self.assertEqual( context.get( 'iBeg'       ),  12 )
+        self.assertEqual( context.get( 'iEnd'       ),  14 )
+        self.assertEqual( context.get( 'iMidLeft'   ),   7 )
+        self.assertEqual( context.get( 'iMidRight'  ),  19 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 7
+        #
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range( 6, 9) )
+        self.assertEqual( context.get( 'iBeg'       ),   6 )
+        self.assertEqual( context.get( 'iEnd'       ),   8 )
+        self.assertEqual( context.get( 'iMidLeft'   ),   4 )
+        self.assertEqual( context.get( 'iMidRight'  ),  10 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 4
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range( 3, 6) )
+        self.assertEqual( context.get( 'iBeg'       ),   3 )
+        self.assertEqual( context.get( 'iEnd'       ),   5 )
+        self.assertEqual( context.get( 'iMidLeft'   ),   2 )
+        self.assertEqual( context.get( 'iMidRight'  ),  52 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 2
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range( 1, 4) )
+        self.assertEqual( context.get( 'iBeg'       ),   0 )
+        self.assertEqual( context.get( 'iEnd'       ),   3 )
+        self.assertEqual( context.get( 'iMidLeft'   ),   0 )
+        self.assertEqual( context.get( 'iMidRight'  ),  51 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 75
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(74,77) )
+        self.assertEqual( context.get( 'iBeg'       ),  74 )
+        self.assertEqual( context.get( 'iEnd'       ),  76 )
+        self.assertEqual( context.get( 'iMidLeft'   ),  38 )
+        self.assertEqual( context.get( 'iMidRight'  ),  87 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 87
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(86,89) )
+        self.assertEqual( context.get( 'iBeg'       ),  86 )
+        self.assertEqual( context.get( 'iEnd'       ),  88 )
+        self.assertEqual( context.get( 'iMidLeft'   ),  81 )
+        self.assertEqual( context.get( 'iMidRight'  ),  93 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 93
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(92,95) )
+        self.assertEqual( context.get( 'iBeg'       ),  92 )
+        self.assertEqual( context.get( 'iEnd'       ),  94 )
+        self.assertEqual( context.get( 'iMidLeft'   ),  90 )
+        self.assertEqual( context.get( 'iMidRight'  ),  96 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 96
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(95,98) )
+        self.assertEqual( context.get( 'iBeg'       ),  95 )
+        self.assertEqual( context.get( 'iEnd'       ),  97 )
+        self.assertEqual( context.get( 'iMidLeft'   ),  94 )
+        self.assertEqual( context.get( 'iMidRight'  ),  98 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 98
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(97,101) )
+        self.assertEqual( context.get( 'iBeg'       ),  97 )
+        self.assertEqual( context.get( 'iEnd'       ), 100 )
+        self.assertEqual( context.get( 'iMidLeft'   ),  49 )
+        self.assertEqual( context.get( 'iMidRight'  ),   0 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        #
+        sPrevPage = str( 1 )
+        #
+        oThisPage.number = 50
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(49,52) )
+        self.assertEqual( context.get( 'iBeg'       ),  49 )
+        self.assertEqual( context.get( 'iEnd'       ),  51 )
+        self.assertEqual( context.get( 'iMidLeft'   ),  25 )
+        self.assertEqual( context.get( 'iMidRight'  ),  75 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 25
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(24,27) )
+        self.assertEqual( context.get( 'iBeg'       ),  24 )
+        self.assertEqual( context.get( 'iEnd'       ),  26 )
+        self.assertEqual( context.get( 'iMidLeft'   ),  13 )
+        self.assertEqual( context.get( 'iMidRight'  ),  37 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 13
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(12,15) )
+        self.assertEqual( context.get( 'iBeg'       ),  12 )
+        self.assertEqual( context.get( 'iEnd'       ),  14 )
+        self.assertEqual( context.get( 'iMidLeft'   ),   7 )
+        self.assertEqual( context.get( 'iMidRight'  ),  19 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 19
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(18,21) )
+        self.assertEqual( context.get( 'iBeg'       ),  18 )
+        self.assertEqual( context.get( 'iEnd'       ),  20 )
+        self.assertEqual( context.get( 'iMidLeft'   ),  16 )
+        self.assertEqual( context.get( 'iMidRight'  ),  59 )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+        #
+        sPrevPage = str( oThisPage.number )
+        #
+        oThisPage.number = 16
+        #
+        #print('')
+        #print('oThisPage.number:', oThisPage.number  )
+        context = self.view.get_context_data(
+                object_list = range(1,10000),
+                page_obj    = oThisPage,
+                sPrevPage   = sPrevPage )
+        #
+        self.assertEqual( context.get( 'show_range' ), range(15,18) )
+        self.assertEqual( context.get( 'iBeg'       ),  15 )
+        self.assertEqual( context.get( 'iEnd'       ),  17 )
+        self.assertEqual( context.get( 'iMidLeft'   ),   8 )
+        self.assertEqual( context.get( 'iMidRight'  ),  58 )
+        #print('')
+        #print( "show_range:  ", context.get( 'show_range' ) )
+        #print( "iBeg:        ", context.get( 'iBeg'       ) )
+        #print( "iEnd:        ", context.get( 'iEnd'       ) )
+        #print( "iMidLeft:    ", context.get( 'iMidLeft'   ) )
+        #print( "iMidRight:   ", context.get( 'iMidRight'  ) )
+
+
 
 
 class EditingTitleShouldBlankFinder( setUpBrandsCategoriesModels ):
