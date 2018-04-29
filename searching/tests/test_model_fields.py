395a984a5b2c799ae5b7162e09985d995bb4bd97
Rick Graves <gravesricharde@yahoo.com>
2018-04-28 20:10:57 -0700

    make sure all fields in dItemFields are in the itemsFound table

---
+++ searching/tests/test_model_fields.py
@@ -0,0 +1,41 @@
+from django.test        import TestCase
+
+from ..models           import ItemFound
+
+from core.utils_test    import AssertEmptyMixin
+
+from File.Get           import getListOffFileLines
+from String.Get         import getTextBefore
+
+
+class MakeSureAllFieldsAreInTableTest( AssertEmptyMixin, TestCase ):
+    '''make sure all dItemFields are in the items table'''
+
+    def test_make_sure_all_fields_are_in_table( self ):
+        '''test to make sure all dItemFields are in the items table'''
+        #
+        lLines = getListOffFileLines( 'searching/__init__.py' )
+        #
+        lItemsFoundLines = []
+        #
+        for s in lLines:
+            #
+            if s.startswith( 'dUserItemFoundUploadFields' ):
+                break
+            #
+            lItemsFoundLines.append( s )
+            #
+        
+        lItemFields = [ getTextBefore( s, '= d(' ).strip()
+                        for s
+                        in lItemsFoundLines
+                        if '= d( t' in s ]
+        #
+        self.assertGreater( len( lItemFields ), 20 )
+        #
+        lOmitted = [ s for s in lItemFields if not hasattr( ItemFound, s ) ]
+        #
+        self.assertEmpty( lOmitted )
+
+
+ 
