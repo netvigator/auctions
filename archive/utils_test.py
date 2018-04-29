47847f25ab3a0978444aa2107c0a59ac2ea49a27
Rick Graves <gravesricharde@yahoo.com>
2018-04-28 20:31:16 -0700

    start implementing getSingleItemResponseExample()

--- archive/utils_test.py
+++ archive/utils_test.py
@@ -1,14 +1,29 @@
 from os.path                import join
 
+from core.ebay_api_calls    import getSingleItem
+
 from searching.models       import ItemFound
 from searching.utils_test   import getItemHitsLog
 
+def getSingleItemResponseExample():
+    
+    sHitLogFile = join( 'searching', 'tests', 'ItemHitsLog.log' )
 
-sHitLogFile = join( 'searching', 'tests', 'ItemHitsLog.log' )
+    lItemHits = getItemHitsLog( sHitLogFile )
 
-lItemHits = getItemHitsLog( sHitLogFile )
+    setItemNumbs = frozenset( ( int( d['iItemNumb'] ) for d in lItemHits ) )
 
-setItemNumbs = frozenset( ( int( d['iItemNumb'] ) for d in lItemHits ) )
+    lGotItems = ItemFound.objects.filter( pk__in = setItemNumbs ).order_by( '-tTimeEnd' )
 
-lGotItems = ItemFound.objects.filter( pk__in = setItemNumbs ).order_by( 'tTimeEnd' )
-
+    for oItem in lGotItems:
+        #
+        if True or oItem.bBuyItNowable:
+            #
+            print( oItem.iItemNumb )
+            #
+            break
+        #
+    #
+    s = getSingleItem( oItem.iItemNumb )
+    #
+    return s
