a0a8c6a29bd8bdc022f1243785cc16bb9190795c
Rick Graves <gravesricharde@yahoo.com>
2018-04-28 20:16:21 -0700

    add getSingleItem response tests

--- archive/tests/test_utils.py
+++ archive/tests/test_utils.py
@@ -1,5 +1,11 @@
 from django.test        import TestCase
 
+from archive            import getListAsLines
+
+from archive.tests      import s142766343340, s232742493872, s232709513135
+
+from ..utils            import storeJsonSingleItemResponse
+
 
 class GetListAsLinesTest( TestCase ):
     '''class for testing getListAsLines()'''
@@ -7,8 +13,28 @@ class GetListAsLinesTest( TestCase ):
     def test_getListAsLines( self ):
         '''test getListAsLines()'''
         #
-        from ..utils        import getListAsLines
         #
         from archive.tests  import sPicList, sExpectList # in __init__.py
         #
         self.assertEqual( getListAsLines( sPicList ), sExpectList )
+
+
+class SomeItemsTest( TestCase ):
+    '''test some getSingleItem imports'''
+
+    def test_s142766343340( self ):
+        '''test getSingleItem 1946 Bendix Catalin'''
+        #
+        dGotItem = storeJsonSingleItemResponse( s142766343340 )
+
+    def test_s232742493872( self ):
+        '''test getSingleItem 1946 Bendix Catalin'''
+        #
+        dGotItem = storeJsonSingleItemResponse( s232742493872 )
+
+    def test_s232709513135( self ):
+        '''test getSingleItem 1946 Bendix Catalin'''
+        #
+        dGotItem = storeJsonSingleItemResponse( s232709513135 )
+
+        
