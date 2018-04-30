e1e548d319c826ae1eb892cab29fe34785fde7f5
Rick Graves <gravesricharde@yahoo.com>
2018-04-30 05:06:55 -0700

    getValueOffItemDict() not working right, need to test it directly

---
+++ core/tests/test_utils_ebay.py
@@ -0,0 +1,30 @@
+from django.test            import TestCase
+
+from ..utils_ebay           import getValueOffItemDict
+from ..utils_test           import AssertEmptyMixin
+
+from searching              import dItemFoundFields         # in __init__.py
+from searching.tests        import sResponseSearchTooBroad  # in __init__.py
+from searching.utilsearch   import getSearchResultGenerator
+
+
+class GetValueOffItemDictTests( AssertEmptyMixin, TestCase ):
+    '''need to test getValueOffItemDict()'''
+
+    def test_get_value_off_item_dict( self, **kwargs ):
+        #
+        oItemIter = getSearchResultGenerator( '', 1, sResponseSearchTooBroad )
+        #
+        dItem = next( oItemIter )
+        #
+        dFields     = dItemFoundFields
+        getValue    = getValueOffItemDict
+        #
+        dNewResult  = { k: getValue( dItem, k, v, **kwargs )
+                        for k, v in dFields.items() }
+        #
+        self.assertIsNotNone( dNewResult.get( 'iCategoryID' ) )
+        #
+        self.assertEqual( dNewResult.get( 'iCategoryID' ), 38034 )
+        #
+        self.assertEmpty( dNewResult.get( 'i2ndCatHeirarchy' ) )
