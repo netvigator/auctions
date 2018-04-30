6691247b25292860b72a9065984afc51cae0502a
Rick Graves <gravesricharde@yahoo.com>
2018-04-29 20:46:29 -0700

    implement storeJsonSingleItemResponse() store single item in archive

--- archive/utils.py
+++ archive/utils.py
@@ -1,12 +1,16 @@
 import logging
 
+
 from archive            import dItemFields as dFields # in __init__.py
+from .forms             import ItemForm
+from .models            import Item
 
 from core.utils_ebay    import getValueOffItemDict
 
+from Time.Test          import isDateTimeObj
 from Time.Output        import getNowIsoDateTimeFileNameSafe
 
-from pprint import pprint
+
 
 
 class GetSingleItemNotWorkingError( Exception ): pass
@@ -74,8 +78,76 @@ def storeJsonSingleItemResponse( sContent, **kwargs ):
     dGotItem = { k: getValueOffItemDict( dItem, k, v, **kwargs )
                  for k, v in dFields.items() }
     #
+    bAnyChanged = False
     #
-    return dGotItem
+    if Item.objects.filter( pk = dGotItem['iItemNumb'] ).exists():
+        #
+        iSavedRowID = dGotItem['iItemNumb']
+        #
+        oItem = Item.objects.get( pk = dGotItem['iItemNumb'] )
+        #
+        form = ItemForm( instance = oItem )
+        #
+        for sField in dGotItem:
+            #
+            sValTable  = getattr( oItem, sField )
+            sValImport = dGotItem[ sField ]
+            #
+            if (    ( sValTable or sValImport ) and
+                      sValTable != sValImport ):
+                #
+                setattr( oItem, sField, sValImport )
+                #
+                bAnyChanged = True
+                #
+            #
+        if bAnyChanged:
+            #
+            oItem.save()
+            #
+        #
+    else:
+        #
+        form = ItemForm( data = dGotItem )
+        #
+        iSavedRowID = None
+        #
+        if form.is_valid():
+            #
+            oItemInstance = form.save()
+            #
+            iSavedRowID = oItemInstance.pk
+            #
+        else:
+            #
+            # ### form errors are common,
+            # ### not all real categories are in the test database
+            #
+            #
+            # print( 'dNewResult["iCategoryID"]:', dNewResult['iCategoryID'] )
+            sMsg = ('form did not save' )
+            #
+            logger.error( sMsg )
+            #print( '' )
+            #print( 'log this error, form did not save' )
+            #
+            if form.errors:
+                for k, v in form.errors.items():
+                    logger.warning( '%s -- %s' % ( k, str(v) ) )
+                    # print( k, ' -- ', str(v) )
+            else:
+                logger.info( 'no form errors at bottom!' )
+            #
+            #tProblems = ( 'iItemNumb', 'cMarket', 'iCategoryID', 'cCategory',
+                        #'iCatHeirarchy', 'i2ndCategoryID', 'c2ndCategory',
+                        #'i2ndCatHeirarchy', 'cCountry' )
+            ##
+            #print( '' )
+            #print( 'fields with errors:' )
+            #for sField in tProblems:
+                #print( 'dNewResult["%s"]:' % sField, dNewResult.get( sField ) )
+    #
+    return iSavedRowID
 
 
 
