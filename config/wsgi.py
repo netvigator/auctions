66b98518dbcfac1725d6372bbc1a28ff9433b3c2
Rick Graves <gravesricharde@yahoo.com>
2018-05-10 06:24:50 +0700

    troubleshooting production server, trying new sys path and importing Sentry

--- config/wsgi.py
+++ config/wsgi.py
@@ -22,9 +22,10 @@ from django.core.wsgi import get_wsgi_application
 # auctionbot directory.
 # app_path = os.path.dirname(os.path.abspath(__file__)).replace('/config', '')
 app_path = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(os.path.join(app_path, 'auctionbot'))
+# sys.path.append(os.path.join(app_path, 'auctionbot'))
 
 if True or os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
+    # need Sentry with or without DJANGO_SETTINGS_MODULE in the env
     from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
