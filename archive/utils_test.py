982445d3c99969ff94d5c1668a04f31100edc158
Rick Graves <gravesricharde@yahoo.com>
2018-05-18 06:49:04 +0700

    extreme makeover, getSingleItemResponseExample() is now getSingleItemResponseCandidate()

--- archive/utils_test.py
+++ archive/utils_test.py
@@ -5,25 +5,67 @@ from core.ebay_api_calls    import getSingleItem
 from searching.models       import ItemFound
 from searching.utils_test   import getItemHitsLog
 
-def getSingleItemResponseExample():
-    
+from Numb.Get               import getRandomDigits
+from Time.Delta             import getIsoDateTimeNowPlus, getDeltaDaysFromISOs
+
+class NoExampleRecords( Exception ): pass
+
+
+
+def getSingleItemResponseCandidate( bWantEnded = True ):
+    #
+    sStillAvailableDate = getIsoDateTimeNowPlus( -89 )
+    #
     sHitLogFile = join( 'searching', 'tests', 'ItemHitsLog.log' )
-
-    lItemHits = getItemHitsLog( sHitLogFile )
-
-    setItemNumbs = frozenset( ( int( d['iItemNumb'] ) for d in lItemHits ) )
-
-    lGotItems = ItemFound.objects.filter( pk__in = setItemNumbs ).order_by( '-tTimeEnd' )
-
-    for oItem in lGotItems:
+    #
+    def getActiveOrEnded( sDate ):
         #
-        if True or oItem.bBuyItNowable:
+        if bWantEnded:
             #
-            print( oItem.iItemNumb )
+            bWantThis = int( getDeltaDaysFromISOs( sDate ) ) > 1
             #
-            break
+        else:
+            #
+            bWantThis = int( getDeltaDaysFromISOs( sDate ) ) < -1
+            #
+        #
+        return bWantThis
+    #
+    lItemHits = [ d for d in getItemHitsLog( sHitLogFile )
+                  if ( d[ 'tTimeEnd' ] > sStillAvailableDate and
+                      getActiveOrEnded( d[ 'tTimeEnd' ] ) ) ]
+    #
+    lPrioritySelect = []
+    #
+    for i in range( len( lItemHits ) ):
+        #
+        d = lItemHits[ i ]
+        #
+        iDaysAgo = int( getDeltaDaysFromISOs( d[ 'tTimeEnd' ] ) )
+        #
+        lPrioritySelect.append( ( iDaysAgo * 3 + int( d[ 'iHitStars' ] ), i ) )
         #
     #
-    s = getSingleItem( oItem.iItemNumb )
+    if not lPrioritySelect:
+        #
+        sayWhich = 'ended' if bWantEnded else 'active'
+        #
+        raise NoExampleRecords( 'no %s records in %s!' %
+                               ( sayWhich, sHitLogFile ) )
+        #
     #
-    return s
+    lPrioritySelect.sort()
+    lPrioritySelect.reverse()
+    #
+    iTryThis = len( lPrioritySelect ) + 1
+    #
+    while iTryThis > len( lPrioritySelect ):
+        #
+        iTryThis = int( getRandomDigits(1) )
+        #
+    #
+    dTryThis = lItemHits[ lPrioritySelect[ iTryThis ][1] ]
+    #
+    # s = getSingleItem( dTryThis[ 'iItemNumb' ] )
+    #
+    return dTryThis
