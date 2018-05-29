a63f1922e1a7f5eb47c4ebd7e4dfae4ed3b37d8a
Rick Graves <gravesricharde@yahoo.com>
2018-05-30 06:45:21 +0700

    implement periodic tasks, remove development code

--- searching/tasks.py
+++ searching/tasks.py
@@ -4,20 +4,16 @@ from django.contrib.auth    import get_user_model
 from django.db.models       import Q
 from django.utils           import timezone
 
-from celery                 import Celery
+from celery                 import Celery, shared_task
 from celery.schedules       import crontab
-from celery                 import shared_task
+
+from .models                import Search, SearchLog, UserItemFound
 
 from .utils                 import ( trySearchCatchExceptStoreInFile,
                                      storeSearchResultsInDB )
 from .utils_stars           import findSearchHits
 
-from .models                import Search, SearchLog, UserItemFound
-
-from core.utils             import getBegTime, sayDuration
-
-from String.Output          import ReadableNo
-
+from core.utils             import getBegTime
 
 app = Celery()
 
@@ -33,18 +29,19 @@ app = Celery()
 
 
 
-@shared_task( name = 'auctionbot.searching.tasks.trySearchCatchExceptStoreInFile' )
+@shared_task( name = 'searching.tasks.trySearchCatchExceptStoreInFile' )
 def doTrySearchCatchExceptStoreInFileTask( iSearchID ):
     #
     trySearchCatchExceptStoreInFile( iSearchID )
 
 
-
-def doSearchingPutResultsInFiles( bOnlyList = False, bConsoleOut = False ):
+# called as a daily (periodic) task
+@shared_task( name = 'searching.tasks.doSearchingPutResultsInFiles' )
+def doSearchingPutResultsInFiles( bOnlyList = False ):
     #
     # really want to select for active users only (not inactive)
     #
-    tBeg = getBegTime( bConsoleOut )
+    tBeg = getBegTime()
     #
     t12hAgo = tBeg - timezone.timedelta( hours = 12 )
     t04hAgo = tBeg - timezone.timedelta( hours =  4 )
@@ -77,31 +74,16 @@ def doSearchingPutResultsInFiles( bOnlyList = False, bConsoleOut = False ):
             #
         else:
             #
-            if bConsoleOut:
-                #
-                print( 'Doing %s "%s" search for %s ...' %
-                        ( oSearch.id, oSearch, oSearch.iUser.name ) )
-            #
             sLastFile = doTrySearchCatchExceptStoreInFileTask.delay(
                             iSearchID = oSearch.id ) 
-            #
-            if bConsoleOut:
-                #
-                print(
-                    'Search %s "%s" for %s yielded file "%s"' %
-                    ( oSearch.id, oSearch.cTitle, oSearch.iUser.name, sLastFile ) )
-                print( '' )
             #
             if iSeq < iSearches: sleep( 1 )
             #
         #
     #
-    if bConsoleOut:
-        #
-        sayDuration( tBeg )
 
 
-@shared_task( name = 'auctionbot.searching.tasks.storeSearchResultsInDB' )
+@shared_task( name = 'searching.tasks.storeSearchResultsInDB' )
 def storeSearchResultsInDbTask( iLogID,
                                 sMarket,
                                 sUserName,
@@ -116,6 +98,8 @@ def storeSearchResultsInDbTask( iLogID,
     #
 
 
+# called as a hourly (periodic) task
+@shared_task( name = 'searching.tasks.doPutSearchResultsInItemsFoundTasks' )
 def doPutSearchResultsInItemsFoundTasks():
     #
     qsLogSearches = (
@@ -146,70 +130,7 @@ def doPutSearchResultsInItemsFoundTasks():
 
 
 
-
-def putSearchResultsInItemsFound( bOnlyList = False, bConsoleOut = False ):
-    #
-    tBeg = getBegTime( bConsoleOut )
-    #
-    qsLogSearches = (
-            SearchLog.objects.filter(
-                tBegSearch__isnull = False,
-                tEndSearch__isnull = False,
-                tBegStore__isnull  = True,
-                cResult = 'Success' ).order_by( "tBegSearch" ) )
-    #
-    for oLogSearch in qsLogSearches:
-        #
-        iLogID      = oLogSearch.pk
-        iSearchID   = oLogSearch.iSearch.pk
-        sSearchName = oLogSearch.iSearch.cTitle
-        sUserName   = oLogSearch.iSearch.iUser.username
-        sMarket     = oLogSearch.iSearch.iUser.iEbaySiteID.cMarket
-        #
-        if bOnlyList:
-            #
-            print( 'would store items from the "%s" search named %s' %
-                    ( sUserName, sSearchName ) )
-            #
-        else:
-            #
-            if bConsoleOut:
-                #
-                print( 'Storing the items from the %s search named "%s" ...' %
-                        ( sUserName, sSearchName ) )
-            #
-            t = storeSearchResultsInDB( iLogID,
-                                        sMarket,
-                                        sUserName,
-                                        iSearchID,
-                                        sSearchName )
-            #
-            iItems, iStoreItems, iStoreUsers = t
-            #
-            sItems      = ReadableNo( iItems      )
-            sStoreItems = ReadableNo( iStoreItems )
-            sStoreUsers = ReadableNo( iStoreUsers )
-            #
-            if bConsoleOut:
-                #
-                sStoreVerb = 'was' if iStoreItems == 1 else 'were'
-                sUsersVerb = 'was' if iStoreUsers == 1 else 'were'
-                #
-                print(
-                    'got %s items, of which %s %s new, '
-                    'and %s %s new for %s.' %
-                    ( sItems,      sStoreItems, sStoreVerb,
-                      sStoreUsers, sUsersVerb,  sUserName ) )
-                print( '' )
-            #
-    #
-    if bConsoleOut:
-        #
-        sayDuration( tBeg )
-    #
-
-
-@shared_task( name = 'auctionbot.searching.tasks.findSearchHits' )
+@shared_task( name = 'searching.tasks.findSearchHits' )
 def findSearchHitsTask( iUser, bCleanUp = True, bShowProgress = False ):
     #
     print( 'calling findSearchHits() for %s now ....' % iUser )
@@ -220,6 +141,8 @@ def findSearchHitsTask( iUser, bCleanUp = True, bShowProgress = False ):
     #
 
 
+# called as a hourly (periodic) task
+@shared_task( name = 'searching.tasks.doFindSearhHitsTasks' )
 def doFindSearhHitsTasks( bCleanUpAfterYourself = True, bConsoleOut = False ):
     #
     oUserModel = get_user_model()
@@ -237,41 +160,15 @@ def doFindSearhHitsTasks( bCleanUpAfterYourself = True, bConsoleOut = False ):
     #
 
 
-def doFindSearhHits( bCleanUpAfterYourself = True, bConsoleOut = False ):
-    #
-    tBeg = getBegTime( bConsoleOut )
-    #
-    oUserModel = get_user_model()
-    #
-    for oUser in oUserModel.objects.all():
-        #
-        if ( UserItemFound.objects
-                        .filter( iUser = oUser.id,
-                        tLook4Hits__isnull = True ).exists() ):
-            #
-            findSearchHits(
-                    iUser           = oUser.id,
-                    bCleanUp        = bCleanUpAfterYourself,
-                    bShowProgress   = bConsoleOut )
-    #
-    if bConsoleOut:
-        #
-        sayDuration( tBeg )
 
 
-
-def doAllUnattended():
+def doAllUnattended(): # example only!
     #
-    doSearchingPutResultsInFiles( bConsoleOut = True )
-    putSearchResultsInItemsFound( bConsoleOut = True )
-    doFindSearhHits( bConsoleOut = True )
+    doSearchingPutResultsInFiles( bOnlyList = True )
+    doPutSearchResultsInItemsFoundTasks()
+    doFindSearhHitsTasks( bConsoleOut = True )
 
 
-def doMostUnattended():
-    #
-    doSearchingPutResultsInFiles( bConsoleOut = True )
-    putSearchResultsInItemsFound( bConsoleOut = True )
-    # doFindSearhHits( bConsoleOut = True )
 
 
 
