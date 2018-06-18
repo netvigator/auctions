66b86b7e5d8fa0f7cfbe35f702bba0e58931c205
Rick Graves <gravesricharde@yahoo.com>
2018-06-18 08:07:35 +0700

    move selection code to getItemsFoundForUpdate() in utils, so it can be tested

--- archive/tasks.py
+++ archive/tasks.py
@@ -14,7 +14,8 @@ from celery.schedules       import crontab
 
 #from auctionbot             import celery_app as app # app = Celery()
 
-from .utils                 import getSingleItemThenStore
+from .utils                 import ( getSingleItemThenStore,
+                                     getItemsFoundForUpdate )
 
 from searching.models       import ItemFound, UserItemFound
 
@@ -38,73 +39,7 @@ def doGetSingleItemThenStoreTask( iItemNumb, **kwargs ):
 @shared_task( name = 'archive.tasks.getFetchUserItems' )
 def doGetFetchUserItemsTasks( bOnlySay = False, bDoFinalOnly = False ):
     #
-    # select useritemsfound that have not been fetched yet
-    #
-    qsUserItemNumbs = ( UserItemFound.objects.filter(
-                                bGetPictures        = True,
-                                tRetrieved__isnull  = True )
-                            .values_list( 'iItemNumb', flat = True )
-                            .distinct() )
-    #
-    # select itemsfound that have been fetched already
-    #
-    qsAlreadyFetched = ( ItemFound.objects
-                            .filter( iItemNumb__in = qsUserItemNumbs )
-                            .filter( tRetrieved__isnull = False )
-                            .prefetch_related(
-                                    'tRetrieved', 'tRetrieveFinal' ) )
-    #
-    # update useritemsfound, step thru itemsfound,
-    # mark useritemsfound that have results fetched already
-    #
-    for oItemFound in qsAlreadyFetched:
-        #
-        qsUserItemFound = UserItemFound.filter(
-                                iItemNumb = oItemFound.iItemNumb )
-        #
-        for oUserItemFound in qsUserItemFound:
-            #
-            oUserItemFound.tRetrieved     = oItemFound.tRetrieved
-            oUserItemFound.tRetrieveFinal = oItemFound.tRetrieveFinal
-            #
-            oUserItemFound.save()
-            #
-        #
-    #
-    # select itemsfound for which we have final results
-    #
-    qsAlreadyFinal = ( ItemFound.objects
-                            .filter( iItemNumb__in = qsUserItemNumbs )
-                            .filter( tRetrieveFinal__isnull = False )
-                            .prefetch_related( 'tRetrieveFinal' ) )
-    #
-    # update useritemsfound, step thru itemsfound,
-    # mark useritemsfound for which we have final results
-    #
-    for oItemFound in qsAlreadyFinal:
-        #
-        qsUserItemFound = UserItemFound.filter(
-                                iItemNumb = oItemFound.iItemNumb )
-        #
-        for oUserItemFound in qsUserItemFound:
-            #
-            oUserItemFound.tRetrieveFinal = oItemFound.tRetrieveFinal
-            #
-            oUserItemFound.save()
-            #
-        #
-    #
-    if qsAlreadyFetched.exists() or qsAlreadyFinal.exists():
-        #
-        # must redo query
-        # select useritemsfound for which we need to fetch results
-        #
-        qsUserItemNumbs = ( UserItemFound.objects.filter(
-                                    bGetPictures        = True,
-                                    tRetrieved__isnull  = True )
-                                .values_list( 'iItemNumb', flat = True )
-                                .distinct() )
-        #
+    qsUserItemNumbs = getItemsFoundForUpdate()
     #
     if bOnlySay or bDoFinalOnly:
         #
@@ -127,7 +62,7 @@ def doGetFetchUserItemsTasks( bOnlySay = False, bDoFinalOnly = False ):
                 pk__in = UserItemFound.objects
                     .filter(    tRetrieved__isnull      = False,
                                 tRetrieveFinal__isnull  = True )
-                    .values_list( 'iItemNumb', flat=True ) )
+                    .values_list( 'iItemNumb', flat = True ) )
     #
     if bOnlySay:
         #
