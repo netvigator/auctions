8e865bab0f53d370920cb042a4ebea8df0b207c2
Rick Graves <gravesricharde@yahoo.com>
2018-06-25 06:38:12 +0700

    parameterize time_out_connect & time_out_read, getting lots of connect time outs recently

--- core/ebay_api_calls.py
+++ core/ebay_api_calls.py
@@ -111,7 +111,7 @@ def _getResponseEbayApi(
     #
     oResponse = requests.get(
                     sEndPointURL,
-                    params = dData,
+                    params  = dData,
                     timeout = uTimeOuts )
     #
     return oResponse.text
@@ -132,10 +132,12 @@ def _getItemInfo( uItemNumb, sCallName,
     #
     dConfValues = _getApiConfValues( bUseSandbox )
     #
-    sEndPointURL= dConfValues[ "endpoints"][ 'shopping'     ]
-    iSiteID     = dConfValues[ "call"     ][ "global_id"    ]
-    sCompatible = dConfValues[ "call"     ][ "compatibility"]
-    sAppID      = dConfValues[ "keys"     ][ "ebay_app_id"  ]
+    sEndPointURL= dConfValues[ "endpoints"][ 'shopping'        ]
+    iSiteID     = dConfValues[ "call"     ][ "global_id"       ]
+    sCompatible = dConfValues[ "call"     ][ "compatibility"   ]
+    sAppID      = dConfValues[ "keys"     ][ "ebay_app_id"     ]
+    iTimeOutConn= dConfValues[ "call"     ][ "time_out_connect"]
+    iTimeOutRead= dConfValues[ "call"     ][ "time_out_read"   ]
     #
     dMore       = dict( appid   = sAppID,
                         siteid  = str( iSiteID ),
@@ -143,7 +145,9 @@ def _getItemInfo( uItemNumb, sCallName,
     #
     dParams.update( dMore )
     #
-    return _getResponseEbayApi( sEndPointURL, dParams )
+    tTimeOuts   = ( iTimeOutConn, iTimeOutRead )
+    #
+    return _getResponseEbayApi( sEndPointURL, dParams, tTimeOuts )
 
 
 _tIncludeDefaults = ( 'Details', 'TextDescription' )
@@ -160,7 +164,7 @@ def getSingleItem(
 
 def getItemStatus( uItemNumb, bUseSandbox = False ):
     #
-    return _getItemInfo( uItemNumb, 'GetItemStatus', 
+    return _getItemInfo( uItemNumb, 'GetItemStatus',
                          bUseSandbox = bUseSandbox )
 
 
@@ -174,12 +178,14 @@ def _getCategoriesOrVersion(
     #
     dConfValues = _getApiConfValues( bUseSandbox )
     #
-    sEndPointURL= dConfValues[ "endpoints"][ 'trading'      ]
-    sCompatible = dConfValues[ "call"     ][ "compatibility"]
-    sAppID      = dConfValues[ "keys"     ][ "ebay_app_id"  ]
-    sCertID     = dConfValues[ "keys"     ][ "ebay_certid"  ]
-    sDevID      = dConfValues[ "keys"     ][ "ebay_dev_id"  ]
-    sToken      = dConfValues[ "auth"     ][ "token"        ]
+    sEndPointURL= dConfValues[ "endpoints"][ 'trading'         ]
+    sCompatible = dConfValues[ "call"     ][ "compatibility"   ]
+    sAppID      = dConfValues[ "keys"     ][ "ebay_app_id"     ]
+    sCertID     = dConfValues[ "keys"     ][ "ebay_certid"     ]
+    sDevID      = dConfValues[ "keys"     ][ "ebay_dev_id"     ]
+    sToken      = dConfValues[ "auth"     ][ "token"           ]
+    iTimeOutConn= dConfValues[ "call"     ][ "time_out_connect"]
+    iTimeOutRead= dConfValues[ "call"     ][ "time_out_read"   ]
     #
     dHttpHeaders= {
             "X-EBAY-API-DEV-NAME"           : sDevID,
@@ -217,10 +223,13 @@ def _getCategoriesOrVersion(
                     xml_declaration = True,
                     encoding        = "utf-8" )
     #
+    tTimeOuts   = ( iTimeOutConn, iTimeOutRead )
+    #
     return _postResponseEbayApi(
             'trading',
             sEndPointURL,
             sRequest,
+            tTimeOuts,
             **dHttpHeaders )
 
 
@@ -339,16 +348,24 @@ def findItems(
     #
     dHeader = _getMarketHeader( sMarketID )
     #
+    dConfValues = _getApiConfValues( bUseSandbox )
+    #
+    iTimeOutConn= dConfValues[ "call"     ][ "time_out_connect"]
+    iTimeOutRead= dConfValues[ "call"     ][ "time_out_read"   ]
+    #
+    tTimeOuts   = ( iTimeOutConn, iTimeOutRead )
+    #
     return _getDecoded(
                 _getEbayFindingResponse(
                     sKeyWords   = sKeyWords,
                     sCategoryID = sCategoryID,
                     iPage       = iPage,
                     bUseSandbox = bUseSandbox,
+                    uTimeOuts   = tTimeOuts,
                     **dHeader ) )
 
 #sResults = findItems( sKeyWords = 'Simpson 360', '58277' )
-##
+#
 #QuietDump( sResults, 'Results_Adv_Simpson360.json' )
 
 
@@ -416,96 +433,20 @@ def getMarketCategoriesGotGlobalID(
 # QuietDump( getMarketCategoriesGotGlobalID(),            'Categories_All_EBAY-US.xml' )
 # QuietDump( getMarketCategoriesGotGlobalID( 'EBAY-GB' ), 'Categories_All_EBAY-GB.xml' )
 
-
-
 '''
 
 Best Practices
 GetSingleItem has been optimized for response size, speed and usability.
-So, it returns the most commonly used fields by default. 
+So, it returns the most commonly used fields by default.
 Use the IncludeSelector field to get more data—
 but please note that getting more data can result in longer response times.
 ...
-So, after you initially retrieve an item's details, 
-cache the item data locally, and then use GetItemStatus 
-from then on to more quickly update the details that tend to change. 
-Depending on your use case, you can call GetSingleItem again occasionally 
+So, after you initially retrieve an item's details,
+cache the item data locally, and then use GetItemStatus
+from then on to more quickly update the details that tend to change.
+Depending on your use case, you can call GetSingleItem again occasionally
 to see if the seller has revised any other data in the listing.
 
 http://developer.ebay.com/devzone/shopping/docs/callref/getsingleitem.html#IncludeSelector
-
-
-def GetSingleItem(item_id, include_selector=None, encoding="JSON"):
-
-BestOfferEnabled
-
-iItemNumb       = models.BigIntegerField( 'ebay item number', primary_key = True )
-cTitle          = models.CharField( 'auction headline', max_length = 48, db_index = True )
-Item.Title
-Item.Subtitle
-mLastBid        = MoneyField( 'winning bid', max_digits = 10, decimal_places = 2, default_currency='USD', null = True )
-Item.CurrentPrice
-Item.ConvertedCurrentPrice
-clastbid        = models.CharField( 'winning bid (text)', max_length = 18, db_index = False, null = True )
-tGotLastBid     = models.DateTimeField( 'retrieved last bid date/time', null = True )
-mBuyItNow       = MoneyField( 'buy it now price', max_digits = 10, decimal_places = 2, default_currency='USD', null = True )
-cbuyitnow       = models.CharField( 'buy it now price (text)', max_length = 18, db_index = False, null = True )
-Item.BuyItNowPrice
-Item.BuyItNowAvailable
-binvaliditem    = models.BooleanField( 'invalid item?', default = False )
-iBidCount       = models.PositiveSmallIntegerField( 'number of bids' )
-BidCount
-tAuctionEnd     = models.DateTimeField( 'auction ending date/time' )
-Item.EndTime
-tAuctionBeg     = models.DateTimeField( 'auction beginning date/time' )
-Item.StartTime
-iQuantity       = models.PositiveSmallIntegerField( 'quantity' )
-Item.Quantity
-tcannotfind     = models.DateTimeField( 'cannot retrieve outcome date/time' )
-tlook4images    = models.DateTimeField( 'tried to retrieve images date/time', null = True )
-bgotimages      = models.NullBooleanField( 'got images?' )
-bReserveMet     = models.NullBooleanField( 'reserve met?', null = True )
-Item.ReserveMet
-bBuyItNow       = models.BooleanField( 'buy it now?', default = False )
-bRelisted       = models.BooleanField( 'relisted?', default = False )
-cLocation       = models.CharField( 'location', max_length = 48 )
-Location
-cregion         = models.CharField( 'region', max_length = 48 )
-cSeller         = models.CharField( 'seller', max_length = 48 )
-Item.Seller
-Seller
-iseller
-Item.Seller.UserID
-iSellerFeedback = models.PositiveIntegerField( 'seller feedback' )
-FeedbackScore
-PositiveFeedbackPercent
-TopRatedSeller
-cBuyer          = models.CharField( 'buyer', max_length = 48, null = True )
-Item.HighBidder 
-iBuyer          = models.PositiveIntegerField( 'buyer ID', null = True )
-Item.HighBidder.UserID
-iBuyerFeedback  = models.PositiveIntegerField( 'buyer feedback', null = True )
-Item.HighBidder.FeedbackRatingStar
-Item.HighBidder.FeedbackScore
-cshipping       = models.CharField( 'shipping info', max_length = 188 )
-cDescription    = models.TextField( 'description' )
-Description
-iImages         = models.PositiveSmallIntegerField( '# of pictures' )
-GalleryURL
-PictureURL
-Variations.Pictures
-Item.Variations.Pictures
-Item.Variations.Pictures.VariationSpecificName
-Item.Variations.Pictures.VariationSpecificPictureSet
-Item.Variations.Pictures.VariationSpecificPictureSet.PictureURL
-iRelistItemNumb = models.BigIntegerField( 'relist item number' )
-SecondaryCategoryID
-Site
-
-Item.HighBidder
-Item.HighBidder.UserID
-
-def GetItemStatus(item_id, encoding="JSON"):
-
 
 '''
