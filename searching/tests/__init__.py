f1861f0a3158009771ff63099d860d6927d8bae0
Rick Graves <gravesricharde@yahoo.com>
2018-09-10 05:46:02 +0700

    add test item for Western Electric KS-15874-L2 Cardmatic Tube Tester

--- searching/tests/__init__.py
+++ searching/tests/__init__.py
@@ -577,6 +577,28 @@ sResponseSearchTooBroad = \
               "isMultiVariationListing":["false"],
               "topRatedListing":["false"]
 
+           },{"itemId":["202430076409"],
+              "title":["Classic Western Electric KS-15874-L2 Cardmatic Tube Tester Made By Hickok Great!"],
+              "globalId":["EBAY-US"],
+              "subtitle":["Totally Complete Outfit!  Really NIce!"],
+              "primaryCategory":[{"categoryId":["170062"],"categoryName":["Tube Testers"]}],"secondaryCategory":[{"categoryId":["7275"],"categoryName":["Parts & Tubes"]}],
+              "galleryURL":["http:\/\/thumbs2.ebaystatic.com\/m\/mgpbjxNWrOrRHjbTm5iC75w\/140.jpg"],
+              "viewItemURL":["http:\/\/www.ebay.com\/itm\/Classic-Western-Electric-KS-15874-L2-Cardmatic-Tube-Tester-Made-Hickok-Great-\/202430076409"],
+              "paymentMethod":["PayPal"],"autoPay":["false"],
+              "postalCode":["61021"],"location":["Dixon,IL,USA"],"country":["US"],
+              "shippingInfo":[{"shippingType":["Calculated"],"shipToLocations":["Worldwide"],"expeditedShipping":["true"],"oneDayShippingAvailable":["false"],"handlingTime":["1"]}],
+              "sellingStatus":[{"currentPrice":[{"@currencyId":"USD","__value__":"154.06"}],"convertedCurrentPrice":[{"@currencyId":"USD","__value__":"154.06"}],
+              "bidCount":["5"],
+              "sellingState":["Active"],
+              "timeLeft":["P4DT1H0M15S"]}],
+              "listingInfo":[{"bestOfferEnabled":["false"],"buyItNowAvailable":["false"],
+              "startTime":["2018-09-08T00:24:25.000Z"],
+              "endTime":["2018-09-13T00:24:25.000Z"],
+              "listingType":["Auction"],"gift":["false"],
+              "watchCount":["14"]}],"returnsAccepted":["true"],
+              "condition":[{"conditionId":["3000"],"conditionDisplayName":["Used"]}],"isMultiVariationListing":["false"],
+              "topRatedListing":["false"]
+
            },{"itemId":["323425124965"],
               "title":["Vintage Klipsch Heresy Red cloth Alnico horns, K-22 woofers, K-55V K700, K-77"],
               "globalId":["EBAY-US"],
