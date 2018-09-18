903bcdd820f0f26a3737273aef3b55a179a38d6e
Rick Graves <gravesricharde@yahoo.com>
2018-09-18 10:02:40 +0700

    add keepers to url import list

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
-    
+
     url(r'^admin/',     include('admin_honeypot.urls',
                                     namespace='admin_honeypot')),
     # User management
@@ -21,12 +21,13 @@ urlpatterns = [
     url(r'^accounts/',  include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
-    
-    url(r'^brands/',    include('brands.urls', namespace="brands")),
-    url(r'^ebayinfo/',  include('ebayinfo.urls', namespace="ebayinfo")),
-    url(r'^models/',    include('models.urls', namespace="models")),
-    url(r'^categories/',include('categories.urls', namespace="categories")),
-    url(r'^searching/', include('searching.urls', namespace="searching")),
+
+    url(r'^brands/',    include('brands.urls',      namespace="brands")),
+    url(r'^ebayinfo/',  include('ebayinfo.urls',    namespace="ebayinfo")),
+    url(r'^models/',    include('models.urls',      namespace="models")),
+    url(r'^categories/',include('categories.urls',  namespace="categories")),
+    url(r'^searching/', include('searching.urls',   namespace="searching")),
+    url(r'^keepers/',   include('keepers.urls',     namespace="keepers")),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
