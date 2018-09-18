e3da8652cb5cf745cbd4e0aa538e038c907ac2a1
Rick Graves <gravesricharde@yahoo.com>
2018-09-18 10:05:26 +0700

    getting started to access keeper items

--- keepers/urls.py
+++ keepers/urls.py
@@ -9,25 +9,28 @@ app_name = "keepers"
 urlpatterns = [
     url(
         regex   = r'^$',
-        view    = views.BrandIndexView.as_view(),
+        view    = views.KeeperIndexView.as_view(),
         name    = 'index' ),
-    url(
-        regex   = r'^add/$',
-        view    = views.BrandCreateView.as_view(),
-        name    = 'add'  ),
-    url(
-        regex   = r'^(?P<pk>[0-9]+)/$',
-        view    = views.BrandDetailView.as_view(),
-        name    = 'detail'),
-    url(
-        regex   = r'^(?P<pk>[0-9]+)/delete/$',
-        view    = views.BrandDeleteView.as_view(),
-        name    = 'delete'),
-    url(
-        regex   = r'^(?P<pk>[0-9]+)/edit/$',
-        view    = views.BrandUpdateView.as_view(),
-        name    = 'edit' ),
 
 ]
 
 
+'''
+    url(
+        regex   = r'^add/$',
+        view    = views.KeeperCreateView.as_view(),
+        name    = 'add'  ),
+    url(
+        regex   = r'^(?P<pk>[0-9]+)/$',
+        view    = views.KeeperDetailView.as_view(),
+        name    = 'detail'),
+    url(
+        regex   = r'^(?P<pk>[0-9]+)/delete/$',
+        view    = views.KeeperDeleteView.as_view(),
+        name    = 'delete'),
+    url(
+        regex   = r'^(?P<pk>[0-9]+)/edit/$',
+        view    = views.KeeperUpdateView.as_view(),
+        name    = 'edit' ),
+
+'''
