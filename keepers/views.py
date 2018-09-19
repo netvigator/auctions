d93368bdc397b9e5e9fd948939e4d045d49db4a8
Rick Graves <gravesricharde@yahoo.com>
2018-09-19 09:25:33 +0700

    add get_queryset() to class KeeperIndexView, user can only access user's rows

--- keepers/views.py
+++ keepers/views.py
@@ -1,20 +1,36 @@
 from django.shortcuts   import render
 from django.views       import generic
 
-from core.mixins        import GetPaginationExtraInfoInContext
+from core.mixins        import GetPaginationExtraInfoInContext, FindUserMixin
 
 from core.views         import ( DeleteViewGotModel,
                                  DetailViewGotModel,  ListViewGotModel )
+
+from searching.models   import UserItemFound
 
 from .models            import Keeper
 
 
 
-class KeeperIndexView(
+class KeeperIndexView( FindUserMixin,
             GetPaginationExtraInfoInContext, ListViewGotModel ):
     model               = Keeper
-    template_name       = 'keeper/keepers_index.html'
-    context_object_name = 'keepers_list'
+    template_name       = 'keepers/index.html'
+    template_name       = 'brands/index.html'
+    context_object_name = 'keeper_list'
     paginate_by         = 100
 
-
+    def get_queryset(self):
+        #
+        oUser = self.findUser()
+        #
+        qsUserItemNumbs = ( UserItemFound.objects.filter(
+                                iUser               = oUser )
+                            .values_list( 'iItemNumb', flat = True )
+                            .distinct() )
+        #
+        qsKeepers = Keeper.objects.filter(
+                            iItemNumb__in = qsUserItemNumbs )
+                        .order_by( '-tTimeEnd' )
+        #
+        return qsKeepers
