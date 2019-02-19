e74748b30a646005ea72ea668cacd8bbd00ac630
Rick Graves <gravesricharde@yahoo.com>
2019-02-19 09:39:23 +0700

    implement EBAY_SHIPPING_CHOICES dEBAY_SHIPPING_CHOICE_CODE & getChoiceCode() to implenent capturing ebay shipping type info

--- searching/__init__.py
+++ searching/__init__.py
@@ -25,6 +25,36 @@ else:
 getMakeDir( SEARCH_FILES_FOLDER )
 
 
+# https://developer.ebay.com/devzone/finding/callref/types/ShippingInfo.html
+tEBAY_SHIPPING_TYPES = (
+    ( 0,    'Calculated',
+            'Calculated' ),
+    ( 1,    'CalculatedDomesticFlatInternational',
+            'Calculated Domestic Flat International' ),
+    ( 2,    'Flat',
+            'Flat' ),
+    ( 3,    'FlatDomesticCalculatedInternational',
+            'Flat Domestic Calculated International' ),
+    ( 4,    'Free',
+            'Free' ),
+    ( 5,    'FreePickup',
+            'Free Pick up' ),
+    ( 6,    'Freight',
+            'Freight' ),
+    ( 7,    'FreightFlat',
+            'Freight Flat' ),
+    ( 8,    'NotSpecified',
+            'Not Specified' ) )
+
+EBAY_SHIPPING_CHOICES = tuple(
+        [ ( t[0], t[2] ) for t in tEBAY_SHIPPING_TYPES ] )
+
+dEBAY_SHIPPING_CHOICE_CODE = dict(
+        [ ( t[1], t[0] ) for t in tEBAY_SHIPPING_TYPES ] )
+
+
+def getChoiceCode( sChoice ): return dEBAY_SHIPPING_CHOICE_CODE.get( sChoice )
+
 d = dict
 
 # null = True, blank = True fields in model MUST be marked bOptional here!
@@ -57,13 +87,14 @@ dItemFoundFields = d(
     dCurrentPrice   = d( t = ( 'sellingStatus',
                                'convertedCurrentPrice','__value__'),
                          f = float ),
+    iShippingType   = d( t = ( 'shippingInfo', 'shippingType'),
+                         f = getChoiceCode ),
     iCategoryID     = d( t = ( 'primaryCategory','categoryId'),
                          f = int ),
     cCategory       = d( t = ( 'primaryCategory','categoryName') ),
 
     iCatHeirarchy   = d( t = ( 'iCatHeirarchy',), # not from ebay, calculated
                          bCalculate = True ),
-
     i2ndCategoryID  = d( t = ( 'secondaryCategory','categoryId'),
                          f = int,
                          bOptional = True ),
@@ -71,7 +102,6 @@ dItemFoundFields = d(
                          bOptional = True ),
     i2ndCatHeirarchy= d( t = ( 'i2ndCatHeirarchy',), # not from ebay, calculated
                          bCalculate = True ),
-
     iConditionID    = d( t = ( 'condition','conditionId'),
                          f = int,
                          bOptional = True ),
