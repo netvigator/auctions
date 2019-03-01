33e8b8001bbf93d9eddaba23ce86d2ca185dc639
Rick Graves <gravesricharde@yahoo.com>
2019-03-01 08:35:15 +0700

    put class TestUrlProcessing with def test_get_reverse() here

--- keepers/tests/test_urls.py
+++ keepers/tests/test_urls.py
@@ -1,1 +1,16 @@
- 
+
+
+from ..models           import Keeper
+from .test_utils        import StoreItemsTest
+
+
+class TestUrlProcessing( StoreItemsTest ):
+
+
+    def test_get_reverse( self ):
+        #
+        oItem = Keeper.objects.get( pk = 142766343340 )
+        #
+        self.assertEqual( oItem.get_absolute_url(), '/keepers/142766343340' )
+
+
