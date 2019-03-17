efe5f1779a38e8260d40a45d2585bef4dd3d3400
Rick Graves <gravesricharde@yahoo.com>
2019-03-18 06:50:18 +0700

    need to test each & every core tag, this is a start

---
+++ core/tests/test_core_tags.py
@@ -0,0 +1,24 @@
+from django.test                import TestCase
+from ..templatetags.core_tags   import ( getDashForReturn,
+                                         getDashForReturnButDropLast )
+
+class GetDashForReturnTests(TestCase):
+    '''get dash for return tests'''
+
+    def test_get_dash_for_return(self):
+        #
+        s = 'abc\rdef\nghi\rklm'
+        #
+        sExpect = 'abc - def - ghi - klm'
+        #
+        self.assertEqual( getDashForReturn( s ), sExpect )
+
+
+    def test_get_dash_for_return_but_drop_last(self):
+        #
+        s = 'abc\rdef\nghi\rklm'
+        #
+        sExpect = 'abc - def - ghi'
+        #
+        self.assertEqual( getDashForReturnButDropLast( s ), sExpect )
+
