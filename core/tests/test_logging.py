2416eee828c48ecedb29356479abf40265d1c4ad
Rick Graves <gravesricharde@yahoo.com>
2019-03-21 18:21:02 +0700

    force move from django.test.TestCase to TestCasePlus

--- core/tests/test_logging.py
+++ core/tests/test_logging.py
@@ -1,21 +1,21 @@
-from logging       import getLogger
-from django.test   import TestCase
+from logging            import getLogger
 
+from core.utils_test    import TestCasePlus
 
 logger = getLogger( 'test_logger' )
 
 
 def throw_error_log():
     logger.error('an error')
-    
+
 def throw_message_log():
     logger.info('a message')
 
 
-class LoggingTest(TestCase):
+class LoggingTest( TestCasePlus ):
 
    def test_logging_error(self):
-            
+
         with self.assertLogs( logger ='test_logger', level='ERROR') as cm:
 
             throw_error_log()
@@ -26,7 +26,7 @@ class LoggingTest(TestCase):
             )
 
    def test_logging_info(self):
-            
+
         with self.assertLogs( logger ='test_logger', level='INFO') as cm:
 
             throw_message_log()
