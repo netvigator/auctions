07b5172d44139324b0fc3a56339358cbea4e32a7
Rick Graves <gravesricharde@yahoo.com>
2019-03-31 11:53:12 +0700

    add test for getLineBreakForReturn(), assign starting string once only at top of class

--- core/tests/test_core_tags.py
+++ core/tests/test_core_tags.py
@@ -1,25 +1,31 @@
 from core.utils_test            import TestCasePlus
 
 from ..templatetags.core_tags   import ( getDashForReturn,
+                                         getLineBreakForReturn,
                                          getDashForReturnButDropLast )
 
 class GetDashForReturnTests( TestCasePlus ):
     '''get dash for return tests'''
+    #
+    s = 'abc\rdef\nghi\rklm'
+    #
 
     def test_get_dash_for_return(self):
         #
-        s = 'abc\rdef\nghi\rklm'
-        #
         sExpect = 'abc - def - ghi - klm'
         #
-        self.assertEqual( getDashForReturn( s ), sExpect )
+        self.assertEqual( getDashForReturn( self.s ), sExpect )
 
 
     def test_get_dash_for_return_but_drop_last(self):
         #
-        s = 'abc\rdef\nghi\rklm'
-        #
         sExpect = 'abc - def - ghi'
         #
-        self.assertEqual( getDashForReturnButDropLast( s ), sExpect )
+        self.assertEqual( getDashForReturnButDropLast( self.s ), sExpect )
 
+
+    def test_get_line_break_for_return( self ):
+        #
+        sExpect = 'abc<BR>def<BR>ghi<BR>klm'
+        #
+        self.assertEqual( getLineBreakForReturn( self.s ), sExpect )
