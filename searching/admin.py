381aa46dce1d6e3ac84f6a1122200668e6eb3022
Rick Graves <gravesricharde@yahoo.com>
2019-05-05 20:08:15 +0700

    ItemFoundAdmin & UserItemFoundAdmin now in finders.admin.py

--- searching/admin.py
+++ searching/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Search, ItemFound, UserItemFound
+from .models import Search
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -33,112 +33,4 @@ class SearchAdmin(admin.ModelAdmin):
         obj.save()
 
 
-
-class ItemFoundAdmin(admin.ModelAdmin):
-    list_display = (
-        'iItemNumb',
-        'cTitle',
-        'tCreate',
-        'tTimeBeg',
-        'tTimeEnd',
-        'cLocation',
-        'cCountry',
-        'cListingType',
-        'cMarket',
-        'bBuyItNowable',
-        'bBestOfferable',
-        'lLocalCurrency',
-        'lCurrentPrice',
-        'dCurrentPrice',
-        'iCategoryID',
-        'cCategory',
-        'CatHeirarchy',
-        'i2ndCategoryID',
-        'c2ndCategory',
-        'SecondCatHeir',
-        'iConditionID',
-        'cCondition',
-        'cSellingState',
-        )
-    readonly_fields = (
-        'iItemNumb',
-        'cTitle',
-        'cLocation',
-        'cCountry',
-        'cMarket',
-        'cGalleryURL',
-        'cEbayItemURL',
-        'tTimeBeg',
-        'tTimeEnd',
-        'bBestOfferable',
-        'bBuyItNowable',
-        'cListingType',
-        'lLocalCurrency',
-        'lCurrentPrice',
-        'dCurrentPrice',
-        'iCategoryID',
-        'cCategory',
-        'CatHeirarchy',
-        'i2ndCategoryID',
-        'c2ndCategory',
-        'SecondCatHeir',
-        'iConditionID',
-        'cCondition',
-        'cSellingState',
-        'tCreate' )
-
-    def CatHeirarchy( self, obj ):
-        #
-        return obj.iCatHeirarchy.cCatHierarchy
-
-    def SecondCatHeir( self, obj ):
-        #
-        if obj.i2ndCatHeirarchy is None:
-            return None
-        else:
-            return obj.i2ndCatHeirarchy.cCatHierarchy
-
-
-class UserItemFoundAdmin(admin.ModelAdmin):
-
-    list_display = (
-        'iItemNumb',
-        'iHitStars',
-        'bGetPictures',
-        'tLook4Hits',
-        'iSearch',
-        'iModel',
-        'iBrand',
-        'iCategory',
-        'cWhereCategory',
-        'bListExclude',
-        'tGotPics',
-        'bAuction',
-        'iUser',
-        'tCreate',
-        'tModify',
-        'tRetrieved',
-        'tRetrieveFinal' )
-
-    readonly_fields = (
-        'iItemNumb',
-        'iHitStars',
-        'bGetPictures',
-        'tLook4Hits',
-        'iSearch',
-        'iModel',
-        'iBrand',
-        'iCategory',
-        'cWhereCategory',
-        'bListExclude',
-        'tGotPics',
-        'bAuction',
-        'iUser',
-        'tCreate',
-        'tModify',
-        'tRetrieved',
-        'tRetrieveFinal' )
-
 admin.site.register(Search,SearchAdmin)
-admin.site.register(ItemFound,ItemFoundAdmin)
-admin.site.register(UserItemFound, UserItemFoundAdmin)
