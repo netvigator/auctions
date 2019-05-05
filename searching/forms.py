5d5ca2d9525038ea870d36670fc2e8393a896b86
Rick Graves <gravesricharde@yahoo.com>
2019-05-05 20:13:09 +0700

    Item Found forms moved to finders/forms.py

--- searching/forms.py
+++ searching/forms.py
@@ -5,13 +5,11 @@ from django.forms           import ModelForm, CheckboxInput
 
 from crispy_forms.layout    import Field, Layout, Submit
 
-from searching              import dItemFoundFields, dUserItemFoundUploadFields
-
 from core.forms             import BaseModelFormGotCrispy
 
 from models.models          import Model
 
-from .models                import Search, ItemFound, UserItemFound
+from .models                import Search
 from .utilsearch            import getPriorityChoices, ALL_PRIORITIES
 from .validators            import isPriorityValid
 
@@ -73,11 +71,11 @@ class BaseSearchForm( BaseModelFormGotCrispy ):
         if bCreating and iDummyOriginal and not iDummyCategory:
             sMsg = 'Your ebay category "%s" is invalid' % iDummyOriginal
             raise ValidationError( sMsg, code='invalid ebay category' )
-            
+
         if not ( sKeyWords or iDummyCategory ):
             sMsg = 'key words or ebay category required (having both is OK)'
             raise ValidationError( sMsg, code='invalid' )
-        
+
         #print( 'iDummyCategory:', iDummyCategory )
         #print( 'iDummyOriginal:', iDummyOriginal )
         #
@@ -124,7 +122,7 @@ class BaseSearchForm( BaseModelFormGotCrispy ):
             #
             raise ValidationError('Title "%s" already exists' % cTitle,
                         code='title already exists' )
-        
+
         #
         #
         doCheckSearch = (
@@ -201,113 +199,6 @@ class UpdateSearchForm( BaseSearchForm ):
         self.helper.add_input(Submit('submit', 'Update', css_class='btn-primary'))
         self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
         #
-
-
-
-
-tItemFoundFields = tuple( dItemFoundFields.keys() )
-
-class ItemFoundForm(ModelForm):
-    #
-    '''using a form to validate incoming info from ebay'''
-    #
-    class Meta:
-        model   = ItemFound
-        fields  = tItemFoundFields
-
-
-
-
-tUserItemFoundUploadFields = tuple( dUserItemFoundUploadFields.keys() )
-
-class UserItemFoundUploadForm(ModelForm):
-    #
-    '''using a form to validate incoming info from ebay'''
-    #
-    class Meta:
-        model   = UserItemFound
-        fields  = tUserItemFoundUploadFields
-
-
-
-
-
-tUserItemFoundFields = (
-    'iModel',
-    'gModel',
-    'iBrand',
-    'iCategory',
-    'bGetPictures',
-    'iHitStars'  )
-
-
-class UserItemFoundForm( BaseModelFormGotCrispy ):
-    #
-    '''using a form on the edit user item found page'''
-    #
-    gModel = forms.ChoiceField( (),
-                    label='Generic Models '
-                          '(more than one brand may offer this model)' )
-
-    
-    def __init__( self, *args, **kwargs ):
-        #
-        super( UserItemFoundForm, self ).__init__( *args, **kwargs )
-        #
-        if self.instance.iBrand is not None:
-            self.fields["iModel"].queryset = (
-                    Model.objects.filter(
-                            iUser  = self.user,
-                            iBrand = self.instance.iBrand ) )
-        else:
-            self.fields["iModel"].queryset = Model.objects.filter(
-                                              iUser = self.user )
-
-        self.helper.add_input(Submit('submit', 'Update', css_class='btn-primary'))
-        self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
-        #
-        self.fields['gModel'].choices = (
-                ( o.pk, o.cTitle )
-                  for o in Model.objects.filter( 
-                            iUser  = self.user,
-                            bGenericModel = True) )
-        #
-        self.fields['gModel'].required = False
-        #
-        if self.instance.iModel.bGenericModel:
-            #
-            self.fields['gModel'].initial = self.instance.iModel_id
-            #
-        #
-        self.helper.layout = Layout(
-                'iModel',
-                'gModel',
-                'iBrand',
-                'iCategory',
-                'bGetPictures',
-                Field( 'iHitStars', readonly = True ), )
-
-    def clean( self ):
-        #
-        if any( self.errors ):
-            # Don't bother validating the formset unless each form is valid on its own
-            return
-        #
-        cleaned = super( UserItemFoundForm, self ).clean()
-        #
-        igModel = self.cleaned_data['gModel']
-        iModel  = self.cleaned_data['iModel']
-        #
-        if iModel is None and igModel is not None:
-            #
-            self.cleaned_data['iModel'] = Model.objects.get( pk = igModel )
-            #
-        #
-        return cleaned
-        
-    class Meta:
-        model   = UserItemFound
-        fields  = tUserItemFoundFields
 
 
 
