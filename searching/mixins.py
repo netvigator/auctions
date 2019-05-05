fcc682205326593d59d64418e5da415fcb8f2e65
Rick Graves <gravesricharde@yahoo.com>
2019-05-05 19:48:28 +0700

    class AnyReleventHitStarColsChangedMixin moved to finders/mixins.py

--- searching/mixins.py
+++ searching/mixins.py
@@ -4,7 +4,7 @@ from django.urls                import reverse_lazy
 
 from core.mixins                import WereAnyReleventColsChangedBase
 
-from .models                    import Search, UserItemFound
+from .models                    import Search
 
 
 
@@ -26,36 +26,4 @@ class SearchViewSuccessPostFormValidMixin(object):
         return super( SearchViewSuccessPostFormValidMixin, self ).form_valid(form)
 
 
-
-
-class AnyReleventHitStarColsChangedMixin( WereAnyReleventColsChangedBase ):
-    '''
-    for testing whether any HitStar relevant fields have changed 
-    '''
-
-    def redoHitStars( self, form ):
-        #
-        iHitStars = 0
-        #
-        if (    form.instance.iModel and
-                form.instance.iBrand and
-                form.instance.iCategory ):
-            #
-            iHitStars = (
-                    form.instance.iModel.iStars *
-                    form.instance.iBrand.iStars *
-                    form.instance.iCategory.iStars )
-            #
-        #
-        form.instance.iHitStars = iHitStars
-
-    def form_valid( self, form ):
-        #
-        if self.anyReleventColsChanged( form, self.tHitStarRelevantCols ):
-            #
-            self.redoHitStars( form )
-            #
-        #
-        return super(
-                AnyReleventHitStarColsChangedMixin, self ).form_valid( form )
 
