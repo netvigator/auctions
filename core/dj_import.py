1e5048cf764b5ffbf01fc5fc825a826723868c21
Rick Graves <gravesricharde@yahoo.com>
2019-05-14 03:42:07 +0800

    bring in some more long import lines to shorten them elsewhere

--- core/dj_import.py
+++ core/dj_import.py
@@ -1,4 +1,10 @@
+from django.contrib.auth        import get_user_model
+from django.core.exceptions     import ObjectDoesNotExist, ImproperlyConfigured
 from django.core.urlresolvers   import reverse, resolve
 from django.http.request        import HttpRequest
+from django.utils.safestring    import mark_safe
+
+from django_countries.conf      import settings as countriesSettings
 
 
+import xml.etree.ElementTree as ET
