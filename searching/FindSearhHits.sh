ca471f5c1912bd3c478f2f4b3edb543326950a70
Rick Graves <gravesricharde@yahoo.com>
2019-05-22 04:03:57 +0700

    must activate virtual env for this script to work!!!

--- searching/FindSearhHits.sh
+++ searching/FindSearhHits.sh
@@ -1,2 +1,3 @@
 cd /home/rick/Devel/auctions
+source /home/rick/.virtualenvs/auctions/bin/activate
 python manage.py shell --command="from searching.tasks import doFindSearhHitsTasks; doFindSearhHitsTasks()"
