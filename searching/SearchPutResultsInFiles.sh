34c3034430c1169c8520a9599473f227437c9ce8
Rick Graves <gravesricharde@yahoo.com>
2019-05-21 07:11:15 +0700

    need absolute path to manage.py cuz will be run by apache user

--- searching/SearchPutResultsInFiles.sh
+++ searching/SearchPutResultsInFiles.sh
@@ -1,2 +1,2 @@
-cd ~/Devel/auctions
+cd /home/rick/Devel/auctions
 python manage.py shell --command="from searching.tasks import doSearchingPutResultsInFilesTasks; doSearchingPutResultsInFilesTasks()"
