0c71c35b2576034808bb33237097832a8761d3a8
Rick Graves <gravesricharde@yahoo.com>
2019-05-27 08:56:58 +0700

    put constants for core.mixins.GetUserItemsTableMixin here

---
+++ core/__init__.py
@@ -0,0 +1,9 @@
+sUserItemTableTemplate = '<table RULES=ROWS>%s</table>'
+
+sUserItemRowTemplate = (
+    '<TR>'
+    '<TD VALIGN=TOP>%s</TD>'
+    '<TD VALIGN=TOP>%s</TD>'
+    '<TD VALIGN=TOP>%s</TD>'
+    '<TD VALIGN=TOP>%s%s</TD>'
+    '</TR>' )
