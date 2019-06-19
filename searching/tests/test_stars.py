ea585abbc453022e04cab62575df8f7bf03f375f
Rick Graves <gravesricharde@yahoo.com>
2019-06-19 09:11:11 +0700

    no more iItemNumb_id in useritemfound, must use iItemNumb directly

--- searching/tests/test_stars.py
+++ searching/tests/test_stars.py
@@ -106,7 +106,7 @@ class KeyWordFindSearchHitsTests( SetUpForHitStarsWebTests ):
             #
             print('')
             print(lTest[0])
-            print( '%s length:' % lTest[0].iItemNumb_id,
+            print( '%s length:' % lTest[0].iItemNumb,
                    len( lTest ),
                    'expected:',
                    iExpect )
@@ -153,7 +153,7 @@ class KeyWordFindSearchHitsTests( SetUpForHitStarsWebTests ):
             #
             if oTemp.iHitStars == 0: continue
             #
-            dItemsToTest.setdefault( oTemp.iItemNumb_id, [] ).append( oTemp )
+            dItemsToTest.setdefault( oTemp.iItemNumb, [] ).append( oTemp )
             #
             iCount += 1
             #
@@ -379,7 +379,7 @@ class KeyWordFindSearchHitsTests( SetUpForHitStarsWebTests ):
         #
         self.assertIsNotNone( oTest )
         #
-        oItemFound = ItemFound.objects.get( pk = oTest.iItemNumb_id )
+        oItemFound = ItemFound.objects.get( pk = oTest.iItemNumb )
         #
         #
         self.assertEqual( oTest.iBrand.cTitle, 'Jensen' )
