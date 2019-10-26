f885b07acf81d573327ec76a6638799517765b88
Rick Graves <gravesricharde@yahoo.com>
2019-10-27 05:17:37 +0700

    loading celery here allows celery to work in development (at least)

--- auctionbot/__init__.py
+++ auctionbot/__init__.py
@@ -2,6 +2,6 @@ __version__ = '0.1.0'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
 
 
-# from .taskapp.celery import app as celery_app
+from .taskapp.celery import app as celery_app
 
-# __all__ = ['celery_app']
+__all__ = ['celery_app']
