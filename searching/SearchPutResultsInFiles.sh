88b09749712491bad759f2d513ad3ed478f2439e
Rick Graves <gravesricharde@yahoo.com>
2019-11-06 09:07:51 +0700

    add temp msg re permissions in tmp that block saving files

--- searching/SearchPutResultsInFiles.sh
+++ searching/SearchPutResultsInFiles.sh
@@ -1,3 +1,4 @@
+echo check permissions in tmp before launching celery!
 cd /home/rick/Devel/auctions
 source /home/rick/.virtualenvs/auctions/bin/activate
 python manage.py shell --command="from searching.tasks import doSearchingPutResultsInFilesTasks; doSearchingPutResultsInFilesTasks()"
