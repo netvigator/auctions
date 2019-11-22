090ee5161f962de5ba1eaebcb9305a12d52585f0
Rick Graves <gravesricharde@yahoo.com>
2019-11-22 08:07:19 +0700

    implement setShippingTypeLocalPickupOptional() here

---
+++ finders/utils.py
@@ -0,0 +1,21 @@
+
+def setShippingTypeLocalPickupOptional( dNewResult ):
+    #
+    '''
+    if shippingType is 5 FreePickup, it means
+    9 Free Local Pick Up is optional if handlingTime is set, but`
+    Pick Up ONLY! if handlingTime is not set
+    this needs to be set after reading the api data and before the form
+    tested in searching/test_Utils.py
+    '''
+    #
+    iShippingType = dNewResult.get( 'iShippingType' )
+    iHandlingTime = dNewResult.get( 'iHandlingTime' )
+    #
+    if iShippingType == 5 and iHandlingTime and iHandlingTime > 0:
+        #
+        dNewResult[ 'iShippingType' ] = 9
+        #
+    #
+
+
