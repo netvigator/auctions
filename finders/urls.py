b8d57e58d87db287a0f4bc2f3f72ef91c78f83cb
Rick Graves <gravesricharde@yahoo.com>
2019-12-10 06:38:49 +0700

    MUST put the hit url above the index url, somehow the index url intercepts!

--- finders/urls.py
+++ finders/urls.py
@@ -10,6 +10,10 @@ from . import views
 app_name = "finders"
 
 urlpatterns = [
+    url( # this one needs to be above the index url !!!!
+        regex   = r'^hit/(?P<pk>[0-9]+)/$',
+        view    = views.ItemFoundHitView.as_view(),
+        name    = 'hit' ),
     url(
         regex   = r'^(?P<pk>[0-9]+)/$',
         view    = views.ItemFoundDetailView.as_view(),
@@ -22,10 +26,6 @@ urlpatterns = [
         regex = r'(?P<select>[ADPZ]){0,1}/{0,1}$',
         view    = views.FindersIndexView.as_view(),
         name    = 'index' ),
-    url(
-        regex   = r'^hit/(?P<pk>[0-9]+)/$',
-        view    = views.ItemFoundHitView.as_view(),
-        name    = 'hit' ),
 
 ]
 
