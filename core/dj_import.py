1fe17bcad065f132671a7297bde63a2e7e297f8e
Rick Graves <gravesricharde@yahoo.com>
2019-12-12 09:55:24 +0700

    add some updates here, make import statements elsewhere shorter

--- core/dj_import.py
+++ core/dj_import.py
@@ -1,10 +1,20 @@
+# cannot import from here everywhere!
+# you get circular import type problem if the importer is too low level!
+
 from django.contrib.auth        import get_user_model
+# from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import AbstractUser
+from django.utils.encoding      import python_2_unicode_compatible
 from django.core.exceptions     import ObjectDoesNotExist, ImproperlyConfigured
-from django.core.urlresolvers   import reverse, resolve
+# django 2 made obsolete from django.core.urlresolvers   import reverse, resolve
+# from django.urls                import reverse, resolve, reverse_lazy
 from django.http.request        import HttpRequest
 from django.utils.safestring    import mark_safe
+from django.utils.translation   import ugettext_lazy as _
+from django.views.generic       import DetailView, ListView
+from django.views.generic       import RedirectView, UpdateView
 
 from django_countries.conf      import settings as countriesSettings
-
+from django_countries.fields    import CountryField
 
 import xml.etree.ElementTree as ET
