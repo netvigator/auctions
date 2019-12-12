41e5a971400dab9985f7cb84770e51641cd4b1e9
Rick Graves <gravesricharde@yahoo.com>
2019-12-12 09:42:35 +0700

    as of django 2.0, reverse now imports from django.urls not django.core.urlresolvers, also on_delete now required kwarg for models.ForeignKey()

--- auctionbot/users/models.py
+++ auctionbot/users/models.py
@@ -1,13 +1,13 @@
-from django.contrib.auth.models import AbstractUser
-from django.core.urlresolvers   import reverse
-from django.db                  import models
-from django.utils.encoding      import python_2_unicode_compatible
-from django.utils.translation   import ugettext_lazy as _
+# django 2 made obsolete from django.core.urlresolvers   import reverse
+from django.db              import models
+from django.urls            import reverse
+
+from core.dj_import         import AbstractUser, _, python_2_unicode_compatible
 
 import pytz
-from timezone_field             import TimeZoneField
+from timezone_field         import TimeZoneField
 
-from ebayinfo.models            import Market
+from ebayinfo.models        import Market
 
 iEbayUSA = Market.objects.get(
             cMarket = 'EBAY-US' ).pk or 0 # on error may need to comment out!
@@ -18,14 +18,14 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name        = models.CharField(_('Name of User'), blank=True, max_length=255)
-    #models.ForeignKey( Market, models.PositiveIntegerField( 
-    iEbaySiteID = models.ForeignKey( Market,
+    #models.ForeignKey( Market, models.PositiveIntegerField(
+    iEbaySiteID = models.ForeignKey( Market, on_delete=models.CASCADE,
                     verbose_name = 'ebay market (default)',
-                    default = iEbayUSA, on_delete=models.CASCADE )
+                    default = iEbayUSA )
 
     '''for testing, it is a big challenge to set default iEbaySiteID from markets table,
     because when testing, markets table starts out empty'''
-    
+
     cBio        = models.TextField( 'Bio info', max_length=500, blank=True)
     cLocation   = models.CharField( 'Location', max_length=30, blank=True)
     zTimeZone   = TimeZoneField( default = 'UTC' )
