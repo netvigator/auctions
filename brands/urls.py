74e431f09a72aaa2cca7889ef266e73751fb3bcc
Rick Graves <gravesricharde@yahoo.com>
2019-12-17 06:43:03 +0700

    change order for edit & delete urls, PK now comes after not before

--- brands/urls.py
+++ brands/urls.py
@@ -20,17 +20,17 @@ urlpatterns = [
         view    = views.BrandDetailView.as_view(),
         name    = 'detail'),
     url(
-        regex   = r'^(?P<pk>[0-9]+)/delete/$',
+        regex   = r'^delete/(?P<pk>[0-9]+)/$',
         view    = views.BrandDeleteView.as_view(),
         name    = 'delete'),
     url(
-        regex   = r'^(?P<pk>[0-9]+)/edit/$',
+        regex   = r'^edit/(?P<pk>[0-9]+)/$',
         view    = views.BrandUpdateView.as_view(),
         name    = 'edit' ),
-    
+
 ]
 
-'''    
+'''
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^new$', views.CreateBrandView.as_view(),
         name='brands-new',),
