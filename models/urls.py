fd7233abb5caa71a69e09c2aac5ff18a79dc1c19
Rick Graves <gravesricharde@yahoo.com>
2019-12-17 06:44:30 +0700

    change order for edit & delete urls, PK now comes after not before

--- models/urls.py
+++ models/urls.py
@@ -15,18 +15,18 @@ urlpatterns = [
         view    = views.ModelDetailView.as_view(),
         name    = 'detail'),
     url(
-        regex   = r'^(?P<pk>[0-9]+)/edit/$',
+        regex   = r'^edit/(?P<pk>[0-9]+)/$',
         view    = views.ModelUpdateView.as_view(),
         name    = 'edit' ),
     url(
-        regex   = r'^(?P<pk>[0-9]+)/delete/$',
+        regex   = r'^delete/(?P<pk>[0-9]+)/$',
         view    = views.ModelDeleteView.as_view(),
         name    = 'delete'),
     url(
         regex   = r'^add/$',
         view    = views.ModelCreateView.as_view(),
         name    = 'add'  ),
-    
+
 ]
 '''
 
