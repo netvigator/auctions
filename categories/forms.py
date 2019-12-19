16464e1924155f022c6aca429343dc54c67896e5
Rick Graves <gravesricharde@yahoo.com>
2019-12-20 05:40:29 +0700

    implement crispy Layout for category create and update forms

--- categories/forms.py
+++ categories/forms.py
@@ -1,4 +1,4 @@
-from crispy_forms.layout    import Submit
+from crispy_forms.layout    import Field, Layout, Submit
 
 from core.forms             import ModelFormValidatesTitle
 from .models                import Category
@@ -19,9 +19,24 @@ tModelFields = (
     )
 
 
+def _getLayout():
+    #
+    return Layout(
+            'cTitle',
+            Field('cLookFor', rows='2'),
+            Field('cKeyWords', rows='2'),
+            'iStars',
+            'bAllOfInterest',
+            'bWantPair',
+            'bAccessory',
+            'bComponent',
+            'iFamily',
+            Field('cExcludeIf', rows='2'),
+            'bModelsShared' )
+
 class CreateCategoryForm( ModelFormValidatesTitle ):
     #
-    
+
     def __init__( self, *args, **kwargs ):
         #
         super( CreateCategoryForm, self ).__init__( *args, **kwargs )
@@ -29,6 +44,7 @@ class CreateCategoryForm( ModelFormValidatesTitle ):
         self.helper.add_input(Submit('submit', 'Create', css_class='btn-primary'))
         self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
         #
+        self.helper.layout = _getLayout()
 
     class Meta:
         model   = Category
@@ -47,6 +63,7 @@ class UpdateCategoryForm( ModelFormValidatesTitle ):
         self.helper.add_input(Submit('submit', 'Update', css_class='btn-primary'))
         self.helper.add_input(Submit('cancel', 'Cancel', css_class='btn-primary'))
         #
+        self.helper.layout = _getLayout()
 
     class Meta:
         model   = Category
