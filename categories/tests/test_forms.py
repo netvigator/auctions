c4eacd93978374f1aa6cf82c4d47d91fdc275b4f
Rick Graves <gravesricharde@yahoo.com>
2019-12-23 03:37:04 +0700

    server crashes if maybePrint is in core.utils_test cuz WebTest is imported there and that is not installed on the server, must put maybePrint in core.utils

--- categories/tests/test_forms.py
+++ categories/tests/test_forms.py
@@ -1,7 +1,7 @@
 from django.urls        import reverse
 
-from core.utils_test    import ( BaseUserWebTestCase, getUrlQueryStringOff,
-                                 maybePrint )
+from core.utils         import maybePrint
+from core.utils_test    import BaseUserWebTestCase, getUrlQueryStringOff
 
 from ..models           import Category
 from ..forms            import CreateCategoryForm, UpdateCategoryForm
