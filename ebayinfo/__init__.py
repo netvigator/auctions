841c198e9ebcb52c40464c565e220f56f3407d19
Rick Graves <gravesricharde@yahoo.com>
2019-12-24 07:47:24 +0800

    move here: EBAY_SHIPPING_CHOICES, dEBAY_SHIPPING_CHOICE_CODE & getEbayShippingChoiceCode(), this is the logical place, & more findable

--- ebayinfo/__init__.py
+++ ebayinfo/__init__.py
@@ -17,3 +17,45 @@ dMarketsRelated = {
           3 :   0,  # EBAY-GB    : EBAY-US
           0 :   3 } # EBAY-US    : EBAY-GB
 
+
+# https://developer.ebay.com/devzone/finding/callref/types/ShippingInfo.html
+_tEBAY_SHIPPING_TYPES = (
+    ( 0,    'Calculated',
+            'Calculated' ),
+    ( 1,    'CalculatedDomesticFlatInternational',
+            'Calculated Domestic Flat International' ),
+    ( 2,    'Flat',
+            'Flat' ),
+    ( 3,    'FlatDomesticCalculatedInternational',
+            'Flat Domestic Calculated International' ),
+    ( 4,    'Free',
+            'Free' ),
+    ( 5,    'FreePickup',
+            'Pick Up ONLY!' ),
+    ( 6,    'Freight',
+            'Freight' ),
+    ( 7,    'FreightFlat',
+            'Freight Flat' ),
+    ( 8,    'NotSpecified',
+            'Not Specified' ),
+    ( 9,    'FreePickupOption',
+            'Free Pick Up Option' ) )
+# latter added, some but not all FreePickup items are pick up only!
+# utils.setShippingTypeLocalPickupOptional() accesses this info
+# tested in searching/tests/test_models.py
+
+# EBAY_SHIPPING_CHOICES tuple used in finders & keepers model definitions
+EBAY_SHIPPING_CHOICES = tuple(
+        [ ( t[0], t[2] ) for t in _tEBAY_SHIPPING_TYPES ] )
+
+# dEBAY_SHIPPING_CHOICE_CODE used in getEbayShippingChoiceCode()
+dEBAY_SHIPPING_CHOICE_CODE = dict(
+        [ ( t[1], t[0] ) for t in _tEBAY_SHIPPING_TYPES ] )
+
+
+def getEbayShippingChoiceCode( sChoice ):
+    #
+    # logically this would be in ebayinfo.utils
+    # but that gets hard to debug errors
+    #
+    return dEBAY_SHIPPING_CHOICE_CODE.get( sChoice )
