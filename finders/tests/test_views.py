bb32fac1332069f66f912f8da252333306ca831a
Rick Graves <gravesricharde@yahoo.com>
2019-12-25 06:31:16 +0800

    import SetUpForHitStarsWebTests from searching.tests.base instead of from searching.tests.test_stars, avoid running tests over and over

--- finders/tests/test_views.py
+++ finders/tests/test_views.py
@@ -2,19 +2,19 @@ import logging
 
 
 # django 2 made obsolete from django.core.urlresolvers   import reverse
-from django.urls        import reverse
+from django.urls            import reverse
 
-from core.utils_test    import ( SetUpBrandsCategoriesModelsTestPlus,
-                                 BaseUserWebTestCase,
-                                 setup_view_for_tests )
+from core.utils_test        import ( SetUpBrandsCategoriesModelsTestPlus,
+                                     BaseUserWebTestCase,
+                                     setup_view_for_tests )
 
-from core.utils         import getExceptionMessageFromResponse
+from core.utils             import getExceptionMessageFromResponse
 
-from searching.tests.test_stars import SetUpForHitStarsWebTests
+from searching.tests.base   import SetUpForHitStarsWebTests
 
-from ..views            import ( ItemsFoundIndexView,
-                                 ItemFoundDetailView,
-                                 ItemFoundUpdateView )
+from ..views                import ( ItemsFoundIndexView,
+                                     ItemFoundDetailView,
+                                     ItemFoundUpdateView )
 
 
 from pprint import pprint
