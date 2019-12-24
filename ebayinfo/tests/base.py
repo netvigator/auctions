afe9e4ee8296efe75b73bfa0bf29356b4e54df3b
Rick Graves <gravesricharde@yahoo.com>
2019-12-25 07:22:09 +0800

    move code that was getMarketsIntoDatabase() from ebayinfo/utils_test.py into PutMarketsInDatabaseTestBase setUp() here

--- ebayinfo/tests/base.py
+++ ebayinfo/tests/base.py
@@ -1,11 +1,15 @@
-from core.utils_test    import TestCasePlus
-
-from ..utils_test       import getMarketsIntoDatabase
+from core.utils_test    import ( getTableFromScreenCaptureGenerator,
+                                 getNamePositionDict, TestCasePlus )
 
 from ..models           import Market
 
 from ebayinfo.tests     import ( EBAY_US_CURRENT_VERSION,
-                                 EBAY_SG_CURRENT_VERSION )
+                                 EBAY_SG_CURRENT_VERSION,
+                                 sMarketsTable )
+
+from pyPks.Utils.Config import getBoolOffYesNoTrueFalse as getBool
+
+
 
 class PutMarketsInDatabaseTestBase( TestCasePlus ):
     '''test getMarketsIntoDatabase()'''
@@ -14,8 +18,34 @@ class PutMarketsInDatabaseTestBase( TestCasePlus ):
         #
         super( PutMarketsInDatabaseTestBase, self ).setUp()
         #
-        getMarketsIntoDatabase()
-
+        '''fetches the markets table text dump,
+        uses that to populate the markets table.
+        useful for testing, where the database starts empty.'''
+        #
+        oTableIter = getTableFromScreenCaptureGenerator( sMarketsTable )
+        #
+        lHeader = next( oTableIter )
+        #
+        d = getNamePositionDict( lHeader )
+        #
+        for lParts in oTableIter:
+            #
+            oMarket = Market(
+                    iEbaySiteID     = int(      lParts[ d['iEbaySiteID'    ] ] ),
+                    cMarket         =           lParts[ d['cMarket'        ] ],
+                    cCountry        =           lParts[ d['cCountry'       ] ],
+                    cLanguage       =           lParts[ d['cLanguage'      ] ],
+                    bHasCategories  = getBool(  lParts[ d['bHasCategories' ] ] ),
+                    cCurrencyDef    =           lParts[ d['cCurrencyDef'   ] ],
+                    iUtcPlusOrMinus = int(      lParts[ d['iUtcPlusOrMinus'] ] ) )
+            #
+            if lParts[ d['iCategoryVer'] ]:
+                oMarket.iCategoryVer= int(      lParts[ d['iCategoryVer'   ] ] )
+            #
+            if lParts[ d['cUseCategoryID' ] ]:
+                oMarket.cUseCategoryID=         lParts[ d['cUseCategoryID' ] ]
+            #
+            oMarket.save()
 
 
 
