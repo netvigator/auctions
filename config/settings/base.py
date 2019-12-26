2ca7407149a63f13e512cdf199dc2f031f2c129a
Rick Graves <gravesricharde@yahoo.com>
2019-12-26 16:54:10 +0800

    no using define, which is commented out in core.templatetags.core_tags, so remove it from the templates list

--- config/settings/base.py
+++ config/settings/base.py
@@ -268,7 +268,6 @@ TEMPLATES = [
                                       'core.templatetags.core_tags',
                 'getLineBreakForReturn' :
                                       'core.templatetags.core_tags',
-                'define'            : 'core.templatetags.core_tags',
                 'model_name_plural' : 'core.templatetags.core_tags',
                 'field_name'        : 'core.templatetags.core_tags',}
         },
