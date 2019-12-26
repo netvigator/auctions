4752967117d4f377263140d3f615a99869ea1e6f
Rick Graves <gravesricharde@yahoo.com>
2019-12-26 11:59:50 +0800

    update template names, remove ItemsFoundIndexView (obsolete)

--- finders/views.py
+++ finders/views.py
@@ -20,7 +20,7 @@ class FindersIndexView(
             GetFindersSelectionsOnPost, GetPaginationExtraInfoInContext,
             ListViewGotModel ):
 
-    template_name       = 'finders/fIndex.html'
+    template_name       = 'finders/index.html'
     model               = UserFinder
     context_object_name = 'finders_list'
     paginate_by         = 100
@@ -63,102 +63,11 @@ class FindersIndexView(
 
 
 
-class ItemsFoundIndexView(
-            GetFindersSelectionsOnPost, GetPaginationExtraInfoInContext,
-            ListViewGotModel ):
-
-    template_name       = 'finders/index.html'
-    model               = UserItemFound
-    context_object_name = 'finders_list'
-    paginate_by         = 100
-
-    def get_queryset( self ):
-        #
-        # ADPZ
-        # qs = super( ItemsFoundIndexView, self ).get_queryset()
-        # sSelect = 'P'
-        #
-        sSelect = self.kwargs.get('select', 'P' )
-        #
-        if not sSelect: sSelect = 'P' # was p now P after non auctions flushed
-        #
-        if sSelect == 'A': # all
-            qsGot = UserItemFound.objects.select_related().filter(
-                        iUser               = self.request.user,
-                        bListExclude        = False,
-                        tRetrieved__isnull  = True,
-                        iHitStars__isnull   = False
-                    ).order_by( '-iHitStars', 'iModel', 'tCreate' )
-        elif sSelect == 'P': # postive (non-zero hit stars)
-            qsGot = UserItemFound.objects.select_related().filter(
-                        iUser               = self.request.user,
-                        iHitStars__isnull   = False,
-                        bListExclude        = False,
-                        tRetrieved__isnull  = True,
-                        iHitStars__gt       = 0
-                    ).order_by( '-iHitStars', 'iModel', 'tCreate' )
-        elif sSelect == 'D': # "deleted" (excluded from list)
-            qsGot = UserItemFound.objects.select_related().filter(
-                        iUser               = self.request.user,
-                        iHitStars__isnull   = False,
-                        tRetrieved__isnull  = True,
-                        bListExclude        = True
-                    ).order_by( '-iHitStars', 'iModel', 'tCreate' )
-        elif sSelect == 'Z': # iHitStars = 0
-            qsGot = UserItemFound.objects.select_related().filter(
-                        iUser               = self.request.user,
-                        iHitStars           = 0,
-                        tRetrieved__isnull  = True,
-                        bListExclude        = False
-                    ).order_by( '-iHitStars', 'iModel', 'tCreate' )
-        #if sSelect == 'a': # all auctions
-            #qsGot = UserItemFound.objects.select_related().filter(
-                        #iUser               = self.request.user,
-                        #bAuction            = True,
-                        #bListExclude        = False,
-                        #tRetrieved__isnull  = True,
-                        #iHitStars__isnull   = False
-                    #).order_by( '-iHitStars', 'iModel', 'tCreate' )
-        #elif sSelect == 'p': # postive (non-zero hit stars) auctions
-            #qsGot = UserItemFound.objects.select_related().filter(
-                        #iUser               = self.request.user,
-                        #bAuction            = True,
-                        #iHitStars__isnull   = False,
-                        #bListExclude        = False,
-                        #tRetrieved__isnull  = True,
-                        #iHitStars__gt       = 0
-                    #).order_by( '-iHitStars', 'iModel', 'tCreate' )
-        #elif sSelect == 'd': # "deleted" (excluded from list) auctions
-            #qsGot = UserItemFound.objects.select_related().filter(
-                        #iUser               = self.request.user,
-                        #bAuction            = True,
-                        #iHitStars__isnull   = False,
-                        #tRetrieved__isnull  = True,
-                        #bListExclude        = True
-                    #).order_by( '-iHitStars', 'iModel', 'tCreate' )
-        #elif sSelect == 's': # iHitStars = 0 auctions
-            #qsGot = UserItemFound.objects.select_related().filter(
-                        #iUser               = self.request.user,
-                        #bAuction            = True,
-                        #iHitStars           = 0,
-                        #tRetrieved__isnull  = True,
-                        #bListExclude        = False
-                    #).order_by( '-iHitStars', 'iModel', 'tCreate' )
-        #
-        return qsGot
-
-
-
-
-
-
-
-
 class ItemFoundDetailView( GetUserItemsTableMixin, DetailViewGotModel ):
 
     model           = UserFinder
     parent          = ItemFound
-    template_name   = 'finders/fDetail.html'
+    template_name   = 'finders/detail.html'
     form_class      = UserItemFoundForm
 
     def get_context_data( self, **kwargs ):
@@ -196,7 +105,7 @@ class ItemFoundHitView( GetUserItemsTableMixin, DetailViewGotModel ):
 
     model           = UserItemFound
     parent          = ItemFound
-    template_name   = 'finders/detail.html'
+    template_name   = 'finders/hit-detail.html'
     form_class      = UserItemFoundForm
 
     def get_context_data( self, **kwargs ):
