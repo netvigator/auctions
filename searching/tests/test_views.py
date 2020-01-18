317609adf13d55760d833a992cce814aea5bdbcf
Rick Graves <gravesricharde@yahoo.com>
2020-01-19 05:23:22 +0700

    core.utils_test moved to core.tests.base, import from new location

--- searching/tests/test_views.py
+++ searching/tests/test_views.py
@@ -2,7 +2,7 @@ import logging
 
 from django.urls        import reverse
 
-from core.utils_test    import ( SetUpBrandsCategoriesModelsTestPlus,
+from core.tests.base    import ( SetUpBrandsCategoriesModelsTestPlus,
                                  BaseUserWebTestCase, setup_view_for_tests )
 
 from core.utils         import getExceptionMessageFromResponse
