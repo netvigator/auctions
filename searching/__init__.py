c5527599335d4ae1bab9d89a776dbc8c3abd9c63
Rick Graves <gravesricharde@yahoo.com>
2020-06-15 04:49:18 +0700

    avoid circular import problem, move getPriorityChoices() and ALL_PRIORITIES here

--- searching/__init__.py
+++ searching/__init__.py
@@ -1,3 +1,5 @@
+from string         import ascii_uppercase, digits
+
 from django.conf    import settings
 
 from pyPks.Dir.Get  import getMakeDir
@@ -42,4 +44,39 @@ else:
 
 
 getMakeDir( SEARCH_FILES_FOLDER )
+
+
+def getPriorityChoices( oModel = None, oUser = None, sInclude = None ):
+    #
+    '''get list of priorities for Search.cPriority'''
+    #
+    tAlpha = tuple( ascii_uppercase )
+    tNums  = tuple( digits )[1:]
+    #
+    # iterAll = ( '%s%s' % (A, N) for N in tNums for A in tAlpha )
+    #
+    setAll = set( ( '%s%s' % (A, N) for N in tNums for A in tAlpha ) )
+    #
+    if sInclude:
+        def doOmitFromChoices( s ): return s != sInclude
+    else:
+        def doOmitFromChoices( s ): return True
+    #
+    if oUser and oModel:
+        #
+        oSearches = oModel.objects.filter( iUser = oUser )
+        #
+        setAll.difference_update( ( oSearch.cPriority
+                                    for oSearch in oSearches
+                                    if doOmitFromChoices( oSearch.cPriority ) ) )
+        #
+    #
+    lAll = list( setAll )
+    #
+    lAll.sort()
+    #
+    return tuple( ( ( s, s ) for s in lAll ) )
+
+
+ALL_PRIORITIES = getPriorityChoices()
 
