8b4068de4f8948431bf4b8ea217b383ca753ce0f
Rick Graves <gravesricharde@yahoo.com>
2020-07-06 02:15:16 +0700

    no more hard coding file locations, start with ~ to represent user directory

--- searching/PutSearchResultsInFinders.sh
+++ searching/PutSearchResultsInFinders.sh
@@ -1,3 +1,3 @@
-cd /home/rick/Devel/auctions
-source /home/rick/.virtualenvs/auctions/bin/activate
+cd ~/Devel/auctions
+source ~/.virtualenvs/auctions/bin/activate
 python manage.py shell --command="from searching.tasks import doPutSearchResultsInFindersTasks; doPutSearchResultsInFindersTasks()"
