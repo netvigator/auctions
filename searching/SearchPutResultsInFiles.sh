aefc32e42c89e52dc7cab468260bd99d740a0d30
Rick Graves <gravesricharde@yahoo.com>
2020-07-06 02:15:35 +0700

    no more hard coding file locations, start with ~ to represent user directory

--- searching/SearchPutResultsInFiles.sh
+++ searching/SearchPutResultsInFiles.sh
@@ -1,4 +1,4 @@
-echo check permissions in tmp before launching celery!
-cd /home/rick/Devel/auctions
-source /home/rick/.virtualenvs/auctions/bin/activate
+#echo check permissions in tmp before launching celery!
+cd ~/Devel/auctions
+source ~/.virtualenvs/auctions/bin/activate
 python manage.py shell --command="from searching.tasks import doSearchingPutResultsInFilesTasks; doSearchingPutResultsInFilesTasks()"
