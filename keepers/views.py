400bcb784bdaba4571ab2f0a0c8d5d42843202f4
Rick Graves <gravesricharde@yahoo.com>
2020-08-09 14:34:34 +0700

    1) KeeperDetailView & KeeperIndexView now inherit from GetUserSelectionsOnPost mixin, to handle editing hits 2) add a get_context_data() to KeeperDetailView, but the item hits in the context 3) import TitleSearchMixin, seaching in auction titles to be implemented

--- keepers/views.py
+++ keepers/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts   import render
 from django.views       import generic
 
-from core.mixins        import GetPaginationExtraInfoInContext
+from core.mixins        import ( GetPaginationExtraInfoInContext,
+                                 GetUserSelectionsOnPost,
+                                 TitleSearchMixin )
 
 from core.views         import ( DeleteViewGotModel,
                                  DetailViewGotModel,  ListViewGotModel )
@@ -11,14 +13,37 @@ from .models            import Keeper, UserKeeper
 
 
 
-class KeeperDetailView( DetailViewGotModel ):
+class KeeperDetailView( GetUserSelectionsOnPost, DetailViewGotModel ):
 
     model   = Keeper
     template_name = 'keepers/detail.html'
 
+    def get_context_data( self, **kwargs ):
+        '''
+        want more info to the context data.
+        '''
+        context = super(
+                KeeperDetailView, self
+                ).get_context_data( **kwargs )
+
+        # qsThisItem = UserItemFound.objects.filter(
+        #
+        qsThisItemAllHits = UserKeeper.objects.filter(
+                iItemNumb_id    = context[ 'object' ].iItemNumb,
+                iUser           = self.request.user,
+                ).order_by( '-iHitStars' )
+        #
+        context['HitsForThis']  = qsThisItemAllHits
+        #
+        return context
 
 
-class KeeperIndexView( GetPaginationExtraInfoInContext, ListViewGotModel ):
+
+class KeeperIndexView(
+            GetUserSelectionsOnPost,
+            GetPaginationExtraInfoInContext,
+            TitleSearchMixin,
+            ListViewGotModel ):
     model               = Keeper
     template_name       = 'keepers/index.html'
     context_object_name = 'keeper_list'
