3b47ebbf96cdbfa7bba374f09bd8e9e67e72a792
Rick Graves <gravesricharde@yahoo.com>
2020-08-15 09:13:47 +0700

    python3 enhancement, super() does not need parameters

--- brands/views.py
+++ brands/views.py
@@ -55,7 +55,7 @@ class BrandDetailView( DetailViewGotModelAlsoPost ):
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
-        context = super(BrandDetailView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         # Add in a QuerySet of all the categories
         #context['fields_list'] = Brand.getFieldsForView(
         #
