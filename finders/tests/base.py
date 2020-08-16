6c436984f28f92bb76d15d338004e47ea9a448e2
Rick Graves <gravesricharde@yahoo.com>
2020-08-16 08:53:21 +0700

    prepare to test new form functionality, move importable code here, also rename class SetupUserItemsFoundAndUserFinders to SetupUserItemsFoundAndUserFindersWebTest

--- finders/tests/base.py
+++ finders/tests/base.py
@@ -1,11 +1,18 @@
+from django.utils           import timezone
+
 from ..models               import UserItemFound, UserFinder
 
-from searching.tests.base   import StoreUserItemFoundWebTestBase
+from searching.tests.base   import ( StoreUserItemFoundWebTestBase,
+                                     SetUpForHitStarsWebTests )
 
+from brands.models          import Brand
+from categories.models      import Category
 from models.models          import Model
 
+from searching.models       import Search
 
-class SetupUserItemsFoundAndUserFinders( StoreUserItemFoundWebTestBase ):
+
+class SetupUserItemsFoundAndUserFindersWebTest( StoreUserItemFoundWebTestBase ):
 
     def setUp( self ):
         #
@@ -46,3 +53,45 @@ class SetupUserItemsFoundAndUserFinders( StoreUserItemFoundWebTestBase ):
         #
         self.oUserItemFound = oUserItemFound
         self.oUserFinder    = oUserFinder
+
+
+class SetUpUserItemFoundWebTests( SetUpForHitStarsWebTests ):
+    #
+    def setUp( self ):
+        #
+        super().setUp()
+        #
+        oSample = UserFinder.objects.get(
+            cTitle = 'Altec Lansing 288-8K High Frequency Drive MRII 542 Horn',
+            iUser = self.user1 )
+        #
+        self.oSample   = oSample
+        #
+        oBrand = Brand.objects.get(
+                cTitle = 'Altec-Lansing',
+                iUser  = self.user1 )
+        oModel = Model.objects.get(
+                cTitle = '511A',
+                iBrand = oBrand,
+                iUser  = self.user1 )
+        oCategory = Category.objects.get(
+                cTitle = 'Horn',
+                iUser  = self.user1 )
+        #
+        oSearch = Search.objects.all()[1]
+        #
+        oUserItemFound = UserItemFound(
+                iItemNumb   = self.oSample.iItemNumb,
+                iBrand      = oBrand,
+                iModel      = oModel,
+                iCategory   = oCategory,
+                iUser       = self.user1,
+                iSearch     = oSearch,
+                tCreate     = timezone.now() )
+        #
+        oUserItemFound.save()
+        #
+        self.oUserItemFound = oUserItemFound
+        self.oBrand         = oBrand
+        self.oCategory      = oCategory
+        #
