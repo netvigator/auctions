af08857b0452f1eab89a91d8158775ed7969091a
Rick Graves <gravesricharde@yahoo.com>
2020-08-16 20:18:47 +0700

    1) split getSubstituteForReturn() code out to utils so it can be tested thoroughly, 2) getSubstituteForReturn() now defaults to substituting dash (can override), and 3) remove some unused tags/filters

--- core/tags/core_tags.py
+++ core/tags/core_tags.py
@@ -1,8 +1,7 @@
 from django             import template
 from core.dj_import     import mark_safe
+from core.utils         import getSubstituteForReturn
 
-from pyPks.String.Find  import oFinderCRorLFnMore as oFinderCRorLF
-from pyPks.Time.Output  import getIsoDateTimeFromDateTime
 
 # ### if you add a tag, you must register it in config/settings.base!!! ###
 # ### if you add a tag, you must register it in config/settings.base!!! ###
@@ -25,45 +24,13 @@ def sayListingType( sListType ):
     return sListType
 
 
-@register.filter()
-def getIsoDateTime( tDT ):
-    #
-    '''return non ambigious ISO date time format'''
-    #
-    return getIsoDateTimeFromDateTime( tDT )
-
-
-#@register.filter()
-#def getNbsp(value):
-    ##
-    #'substitute &nbsp; for spaces'
-    ##
-    #return mark_safe("&nbsp;".join(value.split(' ')))
-
-
-def _getSubstituteForReturn( s, sSub, bOmitLast = False ):
-    #
-    if s:
-        #
-        l = [ s for s in oFinderCRorLF.split( s ) if s ]
-        #
-        if bOmitLast and len( l ) > 1:
-            #
-            del l[-1]
-            #
-        #
-        return sSub.join( l )
-        #
-    else:
-        return '' # run replace on None and you get an error
-
 
 @register.filter()
 def getDashForReturn( s ):
     #
     '''html rendering ignores return characters, substitute dashes'''
     #
-    return _getSubstituteForReturn( s, ' - ' )
+    return getSubstituteForReturn( s )
 
 
 
@@ -73,21 +40,7 @@ def getDashForReturnButDropLast( s ):
     '''html rendering ignores return characters, substitute dashes,
        but drop what is after the last return character'''
     #
-    return _getSubstituteForReturn( s, ' - ', bOmitLast = True )
-
-
-
-@register.filter()
-def getLastDroppedFromCommaSeparatedString( s ):
-    #
-    l = s.split( ', ' )
-    #
-    if len( l ) > 1:
-        #
-        del l[-1]
-        #
-    #
-    return ', '.join( l )
+    return getSubstituteForReturn( s, bOmitLast = True )
 
 
 
@@ -96,7 +49,8 @@ def getLineBreakForReturn( s ):
     #
     '''html rendering ignores return characters, substitute <BR>'''
     #
-    return mark_safe( _getSubstituteForReturn( s, '<BR>' ) )
+    return mark_safe( getSubstituteForReturn( s, '<BR>' ) )
+
 
 
 @register.simple_tag
@@ -130,13 +84,3 @@ def field_name(value, field):
 
     return value._meta.get_field(field).verbose_name.title()
 
-
-#@register.assignment_tag does not work under django 2.0 and later
-#def define(val=None):
-    ##
-    #'''
-    #set a variable on the fly in html
-    #from https://stackoverflow.com/questions/1070398/how-to-set-a-value-of-a-variable-inside-a-template-code
-    #'''
-    ##
-    #return val
