dc3ccae103e7e02036c51edca452b0b7dff9bad0
Rick Graves <gravesricharde@yahoo.com>
2021-04-13 04:33:59 +0700

    1) add shebang /usr/bin/env bash, 2) add reminder that this is only run on development machine [not on server], and 3) use ~ for home instead of hard coded

--- searching/tests/SearchStoreResultsTests.sh
+++ searching/tests/SearchStoreResultsTests.sh
@@ -1,3 +1,7 @@
-cd /home/rick/Devel/auctions
-source /home/rick/.virtualenvs/auctions/bin/activate
+#!/usr/bin/env bash
+#
+# this is only run on a development machine!
+#
+cd ~/Devel/auctions
+source !/.virtualenvs/auctions/bin/activate
 python manage.py test searching.tests.test_utils.DoSearchStoreResultsTests
