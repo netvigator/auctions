813129e3fefa4a7f3b6a8e6b37294ddd8ceee3a1
Rick Graves <gravesricharde@yahoo.com>
2021-04-13 04:29:46 +0700

    1) add shebang /usr/bin/env bash, and 2) add warning/reminder about update in development not on server

--- searching/PutSearchResultsInFinders.sh
+++ searching/PutSearchResultsInFinders.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+#
+# do not modify on server! this file is under version control!
+# update on development machine, commit changes, then update server
+#
 cd ~/Devel/auctions
 source ~/.virtualenvs/auctions/bin/activate
 python manage.py shell --command="from searching.tasks import doPutSearchResultsInFindersTasks; doPutSearchResultsInFindersTasks()"
