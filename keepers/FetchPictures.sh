60b517715063425d6dc452cf7e6659198b95b6d7
Rick Graves <gravesricharde@yahoo.com>
2021-04-13 04:28:15 +0700

    1) add shebang /usr/bin/env bash, 2) add warning/reminder about update in development not on server, and 3) use ~ for home instead of hard coded

--- keepers/FetchPictures.sh
+++ keepers/FetchPictures.sh
@@ -1,4 +1,9 @@
-cd /home/rick/Devel/auctions
-source /home/rick/.virtualenvs/auctions/bin/activate
+#!/usr/bin/env bash
+#
+# do not modify on server! this file is under version control!
+# update on development machine, commit changes, then update server
+#
+cd ~/Devel/auctions
+source ~/.virtualenvs/auctions/bin/activate
 python manage.py shell --command="from keepers.tasks import doGetItemPicturesTasks; doGetItemPicturesTasks()"
 
