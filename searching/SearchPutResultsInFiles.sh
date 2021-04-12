4fc3911ba94c527cb3c5fd74ade7e641368c069c
Rick Graves <gravesricharde@yahoo.com>
2021-04-13 04:31:18 +0700

    1) add shebang /usr/bin/env bash, and 2) add warning/reminder about update in development not on server

--- searching/SearchPutResultsInFiles.sh
+++ searching/SearchPutResultsInFiles.sh
@@ -1,4 +1,9 @@
-#echo check permissions in tmp before launching celery!
+#!/usr/bin/env bash
+#
+# do not modify on server! this file is under version control!
+# update on development machine, commit changes, then update server
+#
+# echo check permissions in tmp before launching celery!
 cd ~/Devel/auctions
 source ~/.virtualenvs/auctions/bin/activate
 python manage.py shell --command="from searching.tasks import doSearchingPutResultsInFilesTasks; doSearchingPutResultsInFilesTasks()"
