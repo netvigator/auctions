c00135e7fbccba4f817ffeafd4afa4bdfa71b54f
Rick Graves <gravesricharde@yahoo.com>
2021-05-24 05:48:57 +0700

    giving up on Celery (at least for now), keep framework, comment out code, include notice

--- auctionbot/taskapp/tasks.py
+++ auctionbot/taskapp/tasks.py
@@ -1,8 +1,12 @@
-from celery     import shared_task
+#from celery     import shared_task
+
 from .celery    import app
 
+# 2021-05-24 celery not working, so giving up on it!
+# instead, will set nice level on cron job processes
+# will leave the celery structure in place, to allow retrying later if desired
 
-@shared_task( name = 'auctionbot.taskapp.tasks.add' )
+#@shared_task( name = 'auctionbot.taskapp.tasks.add' )
 def add( i, j ): return i + j
 
 
