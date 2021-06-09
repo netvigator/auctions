b3b4d08aace78ea39d16f82de3eb0d6404a74572
Rick Graves <gravesricharde@yahoo.com>
2021-06-10 05:02:25 +0700

    remove class TestEbayCategoriesSetUp from here into ebayinfo, where it obviously belongs

--- core/tests/test_test.py
+++ core/tests/test_test.py
@@ -73,13 +73,3 @@ class CoreUserTests( TestCasePlus ):
 
         self.assertEqual( oUserOne.username, 'netvigator')
 
-
-
-class TestEbayCategoriesSetUp( GetEbayCategoriesWebTestSetUp ):
-
-    def test_set_up_categories( self ):
-        #
-        '''test whether all the categories are in the table'''
-        #
-        self.assertEqual(
-                EbayCategory.objects.all().count(), self.iCategories )
