cb90ab1516fa9ffae792cc0a412dc1eaebb3102c
Rick Graves <gravesricharde@yahoo.com>
2021-06-11 06:46:52 +0700

    begin implementing new system, store all ebay info in class before calling tests, so new test class EbayCategoriesWebSetUp, eventually TestEbayCategoriesSetUp will be obsolete

--- ebayinfo/tests/test_test.py
+++ ebayinfo/tests/test_test.py
@@ -1,8 +1,13 @@
-from ..models           import EbayCategory, Market
+from ..models           import EbayCategory
+
+from .base              import GetMarketsAndCategoriesWebTestSetUp
 
 from core.tests.base    import GetEbayCategoriesWebTestSetUp
 
 class TestEbayCategoriesSetUp( GetEbayCategoriesWebTestSetUp ):
+    #
+    '''obsolete when the changes started in June 2021 are complete'''
+    #
 
     def test_set_up_categories( self ):
         #
@@ -11,3 +16,15 @@ class TestEbayCategoriesSetUp( GetEbayCategoriesWebTestSetUp ):
         self.assertEqual(
                 EbayCategory.objects.all().count(), self.iCategories )
 
+
+
+class EbayCategoriesWebSetUp( GetMarketsAndCategoriesWebTestSetUp ):
+    #
+    '''new June 2021'''
+    #
+    def test_set_up_categories( self ):
+        #
+        '''test whether all the categories are in the table'''
+        #
+        self.assertEqual(
+                EbayCategory.objects.all().count(), self.iCategories )
