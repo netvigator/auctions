18f3a78b7c9b188bd40639e60230614eedb53610
Rick Graves <gravesricharde@yahoo.com>
2021-07-02 06:14:32 +0700

    for scripts run from cron jobs, must load env vars by calling source $HOME/.bashrc

--- searching/SearchPutResultsInFiles.sh
+++ searching/SearchPutResultsInFiles.sh
@@ -4,6 +4,7 @@
 # update on development machine, commit changes, then update server
 #
 # echo check permissions in tmp before launching celery!
+source $HOME/.bashrc
 cd ~/Devel/auctions
 source ~/.virtualenvs/auctions/bin/activate
 nice python manage.py shell --command="from searching.tasks import doSearchingPutResultsInFilesTasks; doSearchingPutResultsInFilesTasks()"
