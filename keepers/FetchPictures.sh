1cc63803edca64d4d8092e9b7bf6c6835e5918e8
Rick Graves <gravesricharde@yahoo.com>
2021-07-02 06:13:57 +0700

    for scripts run from cron jobs, must load env vars by calling source $HOME/.bashrc

--- keepers/FetchPictures.sh
+++ keepers/FetchPictures.sh
@@ -3,6 +3,7 @@
 # do not modify on server! this file is under version control!
 # update on development machine, commit changes, then update server
 #
+source $HOME/.bashrc
 cd ~/Devel/auctions
 source ~/.virtualenvs/auctions/bin/activate
 nice python manage.py shell --command="from keepers.tasks import doGetItemPicturesTasks; doGetItemPicturesTasks()"
