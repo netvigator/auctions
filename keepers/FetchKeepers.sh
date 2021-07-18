e9eb440615b58a5b97f5e6105c07b6f002830000
Rick Graves <gravesricharde@yahoo.com>
2021-07-19 06:08:30 +0700

    take out source $HOME/.bashrc, it does not pass env vars to script, add comment

--- keepers/FetchKeepers.sh
+++ keepers/FetchKeepers.sh
@@ -3,7 +3,7 @@
 # do not modify on server! this file is under version control!
 # update on development machine, commit changes, then update server
 #
-source $HOME/.bashrc
+# does not pass env vars to script: source $HOME/.bashrc cuz HOME is not set
 cd ~/Devel/auctions
 source ~/.virtualenvs/auctions/bin/activate
 nice python manage.py shell --command="from keepers.tasks import doGetFetchUserItemsTasks; doGetFetchUserItemsTasks()"
