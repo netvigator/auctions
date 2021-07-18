ef4c691521f99c7b1be72005ad294af22ae8fb50
Rick Graves <gravesricharde@yahoo.com>
2021-07-19 06:13:45 +0700

    take out source $HOME/.bashrc, it does not pass env vars to script, must set the env var in crontab itself, crontab settings version control under auctions/auctionbot

--- keepers/FetchPictures.sh
+++ keepers/FetchPictures.sh
@@ -3,7 +3,9 @@
 # do not modify on server! this file is under version control!
 # update on development machine, commit changes, then update server
 #
-source $HOME/.bashrc
+# does not pass env vars to script: source $HOME/.bashrc cuz HOME is not set
+# instead put this above the lines (without the #):
+# DJANGO_SETTINGS_MODULE=config.settings.production
 cd ~/Devel/auctions
 source ~/.virtualenvs/auctions/bin/activate
 nice python manage.py shell --command="from keepers.tasks import doGetItemPicturesTasks; doGetItemPicturesTasks()"
