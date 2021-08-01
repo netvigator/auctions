a91459a4069231f045023f2586e7615461406976
Rick Graves <gravesricharde@yahoo.com>
2021-08-01 15:54:14 +0700

    CollectionsListView displays the collections an unregistered user can visit, and UserVisitView is where the unregistered user lands to access the info in a particular collection. not sure if this is the best app location for these views ???

--- config/views.py
+++ config/views.py
@@ -1,4 +1,4 @@
-from django.views.generic   import ListView
+from django.views.generic   import ListView, DetailView
 
 from auctionbot.users.models import User
 
@@ -16,8 +16,28 @@ class CollectionsListView(ListView):
 
     def get_queryset(self):
         #
+        if not self.request.session or not self.request.session.session_key:
+            self.request.session.save()
+        #
         qsCollections = User.objects.exclude(
                 cCollection__isnull = True ).exclude(
                 cCollection = '' )
         #
         return qsCollections
+
+
+class UserVisitView( DetailView):
+    model = User
+    template_name = 'users/visiting.html'
+
+    def get_context_data( self, **kwargs ):
+        #
+        context = super().get_context_data(**kwargs)
+        #
+        self.request.session[   'visiting'] = context[ 'object' ].id
+        self.request.session['is_visiting'] = True
+        #
+        print( 'set request.session.visiting to', context[ 'object' ],
+                'id:', context[ 'object' ].id )
+        #
+        return context
