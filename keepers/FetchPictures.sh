b3f18af15e7242e5fd5bc31c855ed700cd855c0f
Rick Graves <gravesricharde@yahoo.com>
2021-08-26 08:53:29 +0700

    going virtual environment free on production server (no need), will configure plain shell to run everything

--- keepers/FetchPictures.sh
+++ keepers/FetchPictures.sh
@@ -3,10 +3,8 @@
 # do not modify on server! this file is under version control!
 # update on development machine, commit changes, then update server
 #
-# does not pass env vars to script: source $HOME/.bashrc cuz HOME is not set
-# instead put this above the lines (without the #):
-# DJANGO_SETTINGS_MODULE=config.settings.production
-cd ~/Devel/auctions
-source ~/.virtualenvs/auctions/bin/activate
+# put above crontab settings:
+# PROJECT_HOME=/home/django/auctions
+cd $PROJECT_HOME
 nice python manage.py shell --command="from keepers.tasks import doGetItemPicturesTasks; doGetItemPicturesTasks()"
 
