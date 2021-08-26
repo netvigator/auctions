eaeb7052c4b7e79fcc55008c23459998dfb77ca8
Rick Graves <gravesricharde@yahoo.com>
2021-08-26 11:15:20 +0700

    getting a message in production, update static dir w temp value

--- config/settings/base.py
+++ config/settings/base.py
@@ -292,15 +292,17 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # STATIC FILE CONFIGURATION
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+# STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+STATIC_ROOT = '/home/django/django_project/django_project/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [
-    str(APPS_DIR.path('static')),
-]
+#STATICFILES_DIRS = [
+    #str(APPS_DIR.path('static')),
+#]
+STATICFILES_DIRS = [ STATIC_ROOT ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
