3471a171a0955a897ee13e7a0235ff6fe0453289
Rick Graves <gravesricharde@yahoo.com>
2021-08-26 12:09:44 +0700

    no /srv/big/backups here, put temp backup dir in django_project/django_project/backups

--- searching/backup-json.sh
+++ searching/backup-json.sh
@@ -1,13 +1,13 @@
-# on the auctionbot webserver to back up json files 
-# allows for troubleshooting 
+# on the auctionbot webserver to back up json files
+# allows for troubleshooting
 cd /tmp/searches
 # ls
 datedir=`date '+%Y-%m-%d'`
 cd $datedir
-file_start="/srv/big/backups/searches"
+file_start="/home/django/django_project/django_project/backups"
 # echo $file_start
 timeslot=`date '+%Y-%m-%d.tar.gz'`
 # echo $timeslot
 file=($file_start-$timeslot)
 echo $file
-nice tar -zcf $file *.json 
+nice tar -zcf $file *.json
