beea80bcd854a8925a9ba650ae8779a2200ac393
Rick Graves <gravesricharde@yahoo.com>
2021-08-26 08:53:42 +0700

    going virtual environment free on production server (no need), will configure plain shell to run everything

--- searching/FindSearhHits.sh
+++ searching/FindSearhHits.sh
@@ -3,9 +3,9 @@
 # do not modify on server! this file is under version control!
 # update on development machine, commit changes, then update server
 #
-source $HOME/.bashrc
-cd ~/Devel/auctions
-source ~/.virtualenvs/auctions/bin/activate
+# put above crontab settings:
+# PROJECT_HOME=/home/django/auctions
+cd $PROJECT_HOME
 SECONDS=0
 nice python manage.py shell --command="from keepers.tasks import doGetFetchUserItemsTasks; doGetFetchUserItemsTasks()"
 nice python manage.py shell --command="from searching.tasks import doFindSearhHitsTasks; doFindSearhHitsTasks()"
