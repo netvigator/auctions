b81822825480190274f589c221ffe697536c5f17
Rick Graves <gravesricharde@yahoo.com>
2021-09-04 09:05:55 +0700

    need to backup database from within python to access database secrets

---
+++ auctionbot/backup_database.py
@@ -0,0 +1,42 @@
+'''
+all secrets are in Secrets.ini
+do not want to repeat them somewere (more DRY)
+therefore must get all secrets from Secrets.ini
+'''
+
+from subprocess             import run
+
+from config.settings.base   import DATABASES
+
+from Time.Output            import getNowIsoDateTimeFileNameSafe
+
+
+POSTGRES_USER       = DATABASES['default']['USER'    ]
+POSTGRES_PASSWORD   = DATABASES['default']['PASSWORD']
+DATABASE_HOST       = DATABASES['default']['HOST'    ]
+DATABASE_PORT       = DATABASES['default']['PORT'    ]
+
+
+sURI = 'postgresql://%s:%s@%s:%s/auctions' % (
+    POSTGRES_USER, POSTGRES_PASSWORD, DATABASE_HOST, DATABASE_PORT )
+
+tDump = (   'pg_dump',
+            sURI,
+            '-Fc')
+
+def do_backup():
+    #
+    sNow = getNowIsoDateTimeFileNameSafe()
+    #
+    sFile = "/tmp/auctions-database-%s.backup" % sNow
+    #
+    fDump = open( sFile, "w" )
+    #
+    oResult = run( tDump, stdout = fDump )
+    #
+    fDump.close()
+    #
+    return oResult.returncode, oResult.stderr
+
+
+
