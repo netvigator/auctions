6d1c29e9d2e03af802ea322de9aba0a773dbce1b
Rick Graves <gravesricharde@yahoo.com>
2021-09-12 05:54:24 +0700

    add default_acl = 'public-read'

--- custom_storages.py
+++ custom_storages.py
@@ -2,8 +2,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     bucket_name = 'auction-files'
-    location = 'static'
+    location    = 'static'
+    default_acl = 'public-read'
 
 class MediaStorage(S3Boto3Storage):
     bucket_name = 'auction-files'
-    location = 'media'
+    location    = 'media'
+    default_acl = 'public-read'
