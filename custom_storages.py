8fd41badcb851e23ff90ab7e18e0b91af1300942
Rick Graves <gravesricharde@yahoo.com>
2021-09-12 07:01:12 +0700

    put common properties in class CustomStorage, then StaticStorage & MediaStorage inherit from that, more DRY

--- custom_storages.py
+++ custom_storages.py
@@ -1,11 +1,11 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(S3Boto3Storage):
+class CustomStorage( S3Boto3Storage ):
     bucket_name = 'auction-files'
-    location    = 'static'
     default_acl = 'public-read'
 
-class MediaStorage(S3Boto3Storage):
-    bucket_name = 'auction-files'
+class StaticStorage( CustomStorage ):
+    location    = 'static'
+
+class MediaStorage( CustomStorage ):
     location    = 'media'
-    default_acl = 'public-read'
