694b8c0549d70022093fce36599b38948cafe886
Rick Graves <gravesricharde@yahoo.com>
2021-09-14 06:08:59 +0700

    implement putFile(), also put some code in functions so only run when needed

--- core/s3_utils.py
+++ core/s3_utils.py
@@ -1,5 +1,6 @@
 # helper functions for files on s3 and compatible object storage service
-
+# can draw inspiration from:
+# https://github.com/Mashoud123/Python3-DigitalOcean-Spaces-Manager-v2-Advanced/blob/master/spaces.py
 
 from django.conf    import settings
 
@@ -13,15 +14,20 @@ oSession = boto3.session.Session()
 sBucketName = settings.AWS_STORAGE_BUCKET_NAME
 
 
-client = oSession.client(
+def getClient():
+    #
+    client = oSession.client(
             's3',
-            region_name             = settings.AWS_S3_CUSTOM_DOMAIN[8:12],
-            endpoint_url            = settings.AWS_S3_CUSTOM_DOMAIN,
+            region_name             = settings.AWS_S3_ENDPOINT_URL[8:12],
+            endpoint_url            = settings.AWS_S3_ENDPOINT_URL,
             aws_access_key_id       = settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key   = settings.AWS_SECRET_ACCESS_KEY )
+    #
+    return client
 
-
-s3_paginator = boto3.client(
+def _getPaginator():
+    #
+    s3_paginator = boto3.client(
             's3',
             region_name             = settings.AWS_S3_ENDPOINT_URL[8:12],
             endpoint_url            = settings.AWS_S3_ENDPOINT_URL,
@@ -30,6 +36,8 @@ s3_paginator = boto3.client(
             config                  = Config(
                                     s3 = { 'addressing_style': 'virtual' } )
         ).get_paginator('list_objects_v2')
+    #
+    return s3_paginator
 
 def getFileNameGenerator(
         bucket_name = sBucketName,
@@ -39,6 +47,9 @@ def getFileNameGenerator(
     #
     prefix = prefix[1:] if prefix.startswith(delimiter) else prefix
     start_after = (start_after or prefix) if prefix.endswith(delimiter) else start_after
+    #
+    s3_paginator = _getPaginator()
+    #
     for page in s3_paginator.paginate(
             Bucket      = bucket_name,
             Prefix      = prefix,
@@ -56,3 +67,20 @@ def getFileList(
     return list(
             getFileNameGenerator(
                 bucket_name, prefix, delimiter, start_after ) )
+
+def putFile( sLocalFile, sUploadName ):
+    #
+    try:
+        #
+        oClient = getClient()
+        #
+        oClient.upload_file( sLocalFile, sBucketName, sUploadName )
+        #
+        message = 'success'
+        #
+    except Exception as e:
+        #
+        message = 'error: %s' % str( e )
+        #
+    return message
+
