c3225df278f87438d39b7a2f941170e000cb44cd
Rick Graves <gravesricharde@yahoo.com>
2022-03-23 03:24:11 -0700

    move database restore instructions now applicable to digital ocean data higher

--- ebayinfo/models.py
+++ ebayinfo/models.py
@@ -175,6 +175,9 @@ create database auctions ;
 GRANT ALL PRIVILEGES ON database auctions TO <user name> ;
 
 
+*** digital ocean on workstation ***
+pg_restore -h <host> -p <port> -U <user name> -c -d auctions <file_name>
+
 from terminal on remote machine:
 pg_restore -h data -p 5432 -U <user name> -c -d auctions <file_name>
 
@@ -186,8 +189,5 @@ pg_restore -p 5432 -U <user name> -c -d auctions <file_name>
 
 on webserver
 pg_restore -h localhost -p 5432 -U <user name> -c -d auctions <file_name>
-
-digital ocean
-pg_restore -h <host> -p <port> -U <user name> -c -d auctions <file_name>
 
 '''
